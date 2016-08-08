ff14251667f139d6d99c1496d8c86fffda12f8ea
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2016-08-08 13:21:41 -0600

    Model internationalization

--- laboratory/models.py
+++ laboratory/models.py
@@ -1,15 +1,16 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 
 @python_2_unicode_compatible
 class LaboratoryRoom(models.Model):
-    name = models.CharField('Name', max_length=255)
+    name = models.CharField(_('Name'), max_length=255)
 
     class Meta:
-        verbose_name = 'Laboratory Room'
-        verbose_name_plural = 'Laboratory Rooms'
+        verbose_name = _('Laboratory Room')
+        verbose_name_plural = _('Laboratory Rooms')
 
     def __str__(self):
         return '%s' % (self.name,)
@@ -20,17 +21,17 @@ class Furniture(models.Model):
     FURNITURE = 'F'
     DRAWER = 'D'
     TYPE_CHOICES = (
-        (FURNITURE, 'Furniture'),
-        (DRAWER, 'Drawer')
+        (FURNITURE, _('Furniture')),
+        (DRAWER, _('Drawer'))
     )
     labroom = models.ForeignKey('LaboratoryRoom')
-    name = models.CharField('Name', max_length=255)
-    type = models.CharField('Type', max_length=2, choices=TYPE_CHOICES)
-    dataconfig = models.TextField('Data configuration')
+    name = models.CharField(_('Name'), max_length=255)
+    type = models.CharField(_('Type'), max_length=2, choices=TYPE_CHOICES)
+    dataconfig = models.TextField(_('Data configuration'))
 
     class Meta:
-        verbose_name = 'Piece of furniture'
-        verbose_name_plural = 'Furniture'
+        verbose_name = _('Piece of furniture')
+        verbose_name_plural = _('Furniture')
 
     def __str__(self):
         return '%s' % (self.name,)
@@ -41,16 +42,16 @@ class Shelf(models.Model):
     CRATE = 'C'
     DRAWER = 'D'
     TYPE_CHOICES = (
-        (CRATE, 'Crate'),
-        (DRAWER, 'Drawer')
+        (CRATE, _('Crate')),
+        (DRAWER, _('Drawer'))
     )
     furniture = models.ForeignKey('Furniture')
-    container_shelf = models.ForeignKey('Shelf', null=True, blank=True)
-    type = models.CharField('Type', max_length=2, choices=TYPE_CHOICES)
+    container_shelf = models.ForeignKey(_('Shelf'), null=True, blank=True)
+    type = models.CharField(_('Type'), max_length=2, choices=TYPE_CHOICES)
 
     class Meta:
-        verbose_name = 'Shelf'
-        verbose_name_plural = 'Shelves'
+        verbose_name = _('Shelf')
+        verbose_name_plural = _('Shelves')
 
     def __str__(self):
         return '%s' % (self.furniture,)
@@ -69,24 +70,24 @@ class ObjectFeatures(models.Model):
     WATER_CHEMISTRY = "8"
     OTHER = "9"
     CHOICES = (
-        (GENERAL_USE, 'General use'),
-        (SECURITY_EQUIPMENT, 'Security equipment'),
-        (ANALYTIC_CHEMISTRY, 'Analytic Chemistry'),
-        (ORGANIC_CHEMISTRY, 'Organic Chemistry'),
-        (PHYSICAL_CHEMISTRY, 'Physical Chemistry'),
-        (CHEMICAL_BIOLOGICAL_PROCESS, 'Chemical and Biological process'),
-        (INDUSTRIAL_BIOTECHNOLOGY, 'Industrial Biotechnology'),
-        (BIOCHEMISTRY, 'Biochemistry'),
-        (WATER_CHEMISTRY, 'Water Chemistry'),
-        (OTHER, 'Other')
+        (GENERAL_USE, _('General use')),
+        (SECURITY_EQUIPMENT, _('Security equipment')),
+        (ANALYTIC_CHEMISTRY, _('Analytic Chemistry')),
+        (ORGANIC_CHEMISTRY, _('Organic Chemistry')),
+        (PHYSICAL_CHEMISTRY, _('Physical Chemistry')),
+        (CHEMICAL_BIOLOGICAL_PROCESS, _('Chemical and Biological process')),
+        (INDUSTRIAL_BIOTECHNOLOGY, _('Industrial Biotechnology')),
+        (BIOCHEMISTRY, _('Biochemistry')),
+        (WATER_CHEMISTRY, _('Water Chemistry')),
+        (OTHER, _('Other'))
     )
 
-    name = models.CharField('Name', max_length=2, choices=CHOICES)
-    description = models.TextField('Description')
+    name = models.CharField(_('Name'), max_length=2, choices=CHOICES)
+    description = models.TextField(_('Description'))
 
     class Meta:
-        verbose_name = 'Object feature'
-        verbose_name_plural = 'Object features'
+        verbose_name = _('Object feature')
+        verbose_name_plural = _('Object features')
 
     def __str__(self):
         return '%s' % (self.CHOICES[int(self.name)][1],)
@@ -97,20 +98,20 @@ class Object(models.Model):
     MATERIAL = '1'
     EQUIPMENT = '2'
     TYPE_CHOICES = (
-        (REACTIVE, 'Reactive'),
-        (MATERIAL, 'Material'),
-        (EQUIPMENT, 'Equipment')
+        (REACTIVE, _('Reactive')),
+        (MATERIAL, _('Material')),
+        (EQUIPMENT, _('Equipment'))
     )
     shelf = models.ForeignKey('Shelf')
-    type = models.CharField('Type', max_length=2, choices=TYPE_CHOICES)
-    code = models.CharField('Code', max_length=255)
-    description = models.TextField('Description')
-    name = models.CharField('Name', max_length=255)
+    type = models.CharField(_('Type'), max_length=2, choices=TYPE_CHOICES)
+    code = models.CharField(_('Code'), max_length=255)
+    description = models.TextField(_('Description'))
+    name = models.CharField(_('Name'), max_length=255)
     feature = models.ManyToManyField('ObjectFeatures')
 
     class Meta:
-        verbose_name = 'Object'
-        verbose_name_plural = 'Objects'
+        verbose_name = _('Object')
+        verbose_name_plural = _('Objects')
 
     def __str__(self):
         return '%s' % (self.name,)
@@ -123,19 +124,19 @@ class ShelfObject(models.Model):
     L = '3'
     ML = '4'
     CHOICES = (
-        (M, 'Meters'),
-        (MM, 'Milimeters'),
-        (CM, 'Centimeters'),
-        (L, 'Liters'),
-        (ML, 'Mililiters')
+        (M, _('Meters')),
+        (MM, _('Milimeters')),
+        (CM, _('Centimeters')),
+        (L, _('Liters')),
+        (ML, _('Mililiters'))
     )
     object = models.ForeignKey('Object')
-    quantity = models.FloatField('Material quantity')
-    measurement_unit = models.CharField('Measurement unit', max_length=2, choices=CHOICES)
+    quantity = models.FloatField(_('Material quantity'))
+    measurement_unit = models.CharField(_('Measurement unit'), max_length=2, choices=CHOICES)
 
     class Meta:
-        verbose_name = 'Shelf object'
-        verbose_name_plural = 'Shelf objects'
+        verbose_name = _('Shelf object')
+        verbose_name_plural = _('Shelf objects')
 
     def __str__(self):
         return '%s - %s %s' % (self.object, self.quantity, self.CHOICES[int(self.measurement_unit)][1])
