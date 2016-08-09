c770775d466019bb7ed58f6169f97d404429e3de
Adolfo Hernández <fofoadolfo14@gmail.com>
2016-08-09 16:07:59 -0600

    Add reportlab requirements - Edit laboratory views

--- laboratory/views.py
+++ laboratory/views.py
@@ -1,11 +1,15 @@
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.utils import timezone
 from reportlab.pdfgen import canvas
-from django.http import HttpResponse 
-from io import BytesIO
 from reportlab.lib.pagesizes import A4
+from io import BytesIO
+
+
 
 # Create your views here.
-
 def ReportBuilding(request):
     # Create the HttpResponse with PDF.
     response = HttpResponse(content_type='application/pdf')
@@ -16,10 +20,15 @@ def ReportBuilding(request):
     p = canvas.Canvas(buffer, pagesize=A4)
 
     # Draw things on the PDF. Here's where the PDF generation happens.
-    #Header
+    
+    #PDF Header
+    hora = timezone.now()
     p.setLineWidth(.3)
-    p.setFont('Helvetica', 22)
+    p.setFont('Helvetica', 18)
     p.drawString(30, 750, "Report Building")
+    p.setFont('Helvetica', 12)
+    p.drawString(460, 750, hora.strftime('%d/%m/%Y - %H:%M'))
+    p.line(30, 747, 560, 747)
 
     # Close the PDF object cleanly.
     p.showPage()
