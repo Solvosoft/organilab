367a2c7139dfca66baedb4c73c4123e4d0c82a21
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2016-08-09 10:14:18 -0600

    Missing model data generation functions

--- laboratory/create_data.py
+++ laboratory/create_data.py
@@ -1,5 +1,6 @@
 import random
-from laboratory.models import LaboratoryRoom
+from laboratory.models import LaboratoryRoom, ShelfObject
+from laboratory.models import Object
 from laboratory.models import ObjectFeatures
 from laboratory.models import Furniture
 from laboratory.models import Shelf
@@ -19,7 +20,7 @@ def create_furniture(max=10):
     for i in range(0, max):
         Furniture.objects.create(
             labroom=random_model_object(LaboratoryRoom),
-            name='Testing furniture number %s' % (i+1),
+            name='Testing furniture number %s' % (i + 1),
             type=random_choice(Furniture.TYPE_CHOICES)[0]
         )
 
@@ -31,6 +32,7 @@ def create_shelf(max=10):
             type=random_choice(Shelf.TYPE_CHOICES)[0]
         )
 
+
 def create_object_features(max=10):
     for i in range(0, max):
         ObjectFeatures.objects.create(
@@ -39,22 +41,38 @@ def create_object_features(max=10):
         )
 
 
-def create_objects():
-    pass
+def create_objects(max=10):
+    for i in range(0, max):
+        Object.objects.create(
+            shelf=random_model_object(Shelf),
+            type=random_choice(Object.TYPE_CHOICES)[0],
+            code='object_number_%d' % (i + 1),
+            description='Object description',
+            name='Object number %d' % (i + 1)
+        )
 
 
-def create_shelf_objects():
-    pass
+def create_shelf_objects(max=10):
+    for i in range(0, max):
+        ShelfObject.objects.create(
+            object=random_model_object(Object),
+            quantity=random.random() * 100,
+            measurement_unit=random_choice((ShelfObject.CHOICES))[0]
+        )
 
 
 def clean_models():
     LaboratoryRoom.objects.all().delete()
     Furniture.objects.all().delete()
     Shelf.objects.all().delete()
+    ObjectFeatures.objects.all().delete()
+    Object.objects.all().delete()
+    ShelfObject.objects.all().delete()
 
 
 def random_choice(iterable):
     return random.choice(iterable)
+
 
 def random_model_object(model):
     return model.objects.order_by('?').first()
