12f65bc1c00b5e46441bf3d8d7ef24f865c9d941
Adolfo Hernández <fofoadolfo14@gmail.com>
2016-08-16 13:38:26 -0600

    PDF page numbers

--- laboratory/views.py
+++ laboratory/views.py
@@ -3,18 +3,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.utils import timezone
-from laboratory.models import LaboratoryRoom
+from laboratory.models import LaboratoryRoom, Furniture
 from django.template.loader import get_template
 from django.template.context import Context
 from weasyprint import HTML
 
-class LaboratoryRoomListView(ListView):
-    model = LaboratoryRoom
+class miContexto(object):
     
     def get_context_data(self, **kwargs):
         contex = ListView.get_context_data(self, **kwargs)
         contex['datetime'] = timezone.now()
         return contex
+
+class LaboratoryRoomListView(miContexto, ListView):
+    model = LaboratoryRoom
+    
+class SummaryFurnitureListView(miContexto, ListView):
+    model = Furniture
+
     
 def report_building(request):
     laboratoryroom = LaboratoryRoom.objects.all()
@@ -27,6 +33,26 @@ def report_building(request):
                'request': request
                }
     
+    html = template.render(Context(context)).encode("UTF-8")
+
+    page = HTML(string=html, encoding='utf-8').write_pdf()
+    #pisaStatus = pisa.CreatePDF(html, dest=response)
+    response = HttpResponse(page, content_type='application/pdf')
+    response[
+              'Content-Disposition'] = 'attachment; filename="report_building.pdf"'
+    return response
+
+def summary_report(request):
+    furniture = Furniture.objects.all()
+
+    template = get_template('laboratory/furniture_list.html')
+    
+    context = {
+               'object_list': furniture,
+               'datetime': timezone.now(),
+               'request': request
+               }
+    
     html = template.render(
                             Context(context)).encode("UTF-8")
 
@@ -34,5 +60,5 @@ def report_building(request):
     #pisaStatus = pisa.CreatePDF(html, dest=response)
     response = HttpResponse(page, content_type='application/pdf')
     response[
-              'Content-Disposition'] = 'attachment; filename="report_building.pdf"'
+              'Content-Disposition'] = 'attachment; filename="report_summaryfurniture.pdf"'
     return response
