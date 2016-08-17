05f3962538d8d3557eee51b84c9ae043324c8272
Nashyra Lorig <nashyralb@gmail.com>
2016-08-17 10:10:51 -0600

    modified file

--- laboratory/views.py
+++ laboratory/views.py
@@ -1,3 +1,31 @@
+'''
+Created on 1/8/2016
+
+@author: nashyra
+'''
+
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django import forms
+from django.core.exceptions import ValidationError
 
 # Create your views here.
+
+class FormNums(forms.Form):
+    num4 = forms.IntegerField(widget=forms.NumberInput, help_text = "A number")
+    
+    def clean(self):
+        data = super(FormNums, self).clean()
+        try:
+            num=int(data['num4'])
+        except:
+            raise ValidationError("Int is required")
+        return data
+
+def convert_int(number):
+    result = 0
+    try:
+        result = int(number)
+    except:
+        pass
+    return result
