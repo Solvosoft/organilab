5d68af25db7029f22b453c15ee974c2655413c28
Nashyra Lorig <nashyralb@gmail.com>
2016-08-16 21:59:21 -0600

    modified file

---
+++ laboratory/urls.py
@@ -0,0 +1,19 @@
+'''
+Created on 1/8/2016
+
+@author: nashyra
+'''
+from __future__ import unicode_literals
+from django.core.urlresolvers import reverse
+from django.conf.urls import url
+from laboratory.generic import ShelfCreate, ShelfDelete, ShelfListView
+#from reporter.ajax_view import list_proveedor, ProveedorCreate
+
+urlpatterns = [
+               url(r"^create$", ShelfCreate.as_view(),
+                   name="galleta_create"),
+               url(r"^list$", ShelfListView.as_view(),
+                   name="shelf_list"),
+               url(r"^delete/(?P<pk>\d+)$", ShelfDelete.as_view(),
+                   name="galletas_confirm_delete")
+]
