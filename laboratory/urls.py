5abadb103593d2170ee78f1931034cb3568eef58
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2016-08-19 10:13:47 -0600

    Furniture create view

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -4,16 +4,16 @@ Created on 1/8/2016
 @author: nashyra
 '''
 from __future__ import unicode_literals
-from django.core.urlresolvers import reverse
 from django.conf.urls import url
 from laboratory.generic import ShelfCreate, ShelfDelete, ShelfListView
-#from laboratory.ajax_view import list_shelf, ShelvesCreate
+from laboratory.views import FurnitureCreateView
 
 urlpatterns = [
-               url(r"^create$", ShelfCreate.as_view(),
-                   name="shelf_create"),
-               url(r"^list$", ShelfListView.as_view(),
-                   name="shelf_list_view"),
-               url(r"^delete/(?P<pk>\d+)$", ShelfDelete.as_view(),
-                   name="shelf_confirm_delete")
+    url(r"^create$", ShelfCreate.as_view(),
+        name="shelf_create"),
+    url(r"^list$", ShelfListView.as_view(),
+        name="shelf_list_view"),
+    url(r"^delete/(?P<pk>\d+)$", ShelfDelete.as_view(),
+        name="shelf_confirm_delete"),
+    url(r'^furniture/create$', FurnitureCreateView.as_view())
 ]
--- laboratory/views.py
+++ laboratory/views.py
@@ -3,29 +3,11 @@ Created on 1/8/2016
 
 @author: nashyra
 '''
+from django.views.generic import CreateView
+from laboratory.models import Furniture
 
-from django.shortcuts import render
-from django.http.response import HttpResponse
-from django import forms
-from django.core.exceptions import ValidationError
 
-# Create your views here.
-
-class FormNums(forms.Form):
-    num4 = forms.IntegerField(widget=forms.NumberInput, help_text = "A number")
-    
-    def clean(self):
-        data = super(FormNums, self).clean()
-        try:
-            num=int(data['num4'])
-        except:
-            raise ValidationError("Int is required")
-        return data
-
-def convert_int(number):
-    result = 0
-    try:
-        result = int(number)
-    except:
-        pass
-    return result
+class FurnitureCreateView(CreateView):
+    model = Furniture
+    success_url = '/'
+    fields = '__all__'
