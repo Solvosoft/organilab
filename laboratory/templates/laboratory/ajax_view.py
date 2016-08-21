44fcf74234074700260d21c1cff34a2398c6c079
Natalia Parra <natyparra04@gmail.com>
2016-08-20 18:12:56 -0600

    falta recibir datos en ajax_view.py y cerrar correctamente los modales despues de agregar o eliminar

---
+++ laboratory/templates/laboratory/ajax_view.py
@@ -0,0 +1,114 @@
+'''
+Created on /8/2016
+
+@author: natalia
+'''
+from laboratory.models import Furniture, Shelf, ShelfObject
+from django.template.loader import render_to_string
+from django_ajax.decorators import ajax
+from django_ajax.mixin import AJAXMixin
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls.base import reverse_lazy
+from django.http.response import HttpResponseRedirect
+
+def list_furniture_render(request):   
+    var= request.GET.get('namelaboratoryRoom');
+    print(' imprimiendo GET NAME laboratoryRoom')
+    print(var)
+    
+    if var:
+     furnitures= Furniture.objects.filter(labroom='ap1')
+    else: 
+     furnitures= Furniture.objects.all()
+    return render_to_string(
+                    'laboratory/furniture_list.html',
+                     context={
+                       'object_list':furnitures
+                       })
+@ajax 
+def list_furniture(request):
+    print("Entro al ajax_view labory furniture")
+    return {
+           'inner-fragments':{
+                '#furnitures': list_furniture_render(request)
+                                   
+            },
+    }
+    
+    
+    
+    
+def list_shelf_render(request):   
+    var=request.GET.get("furniture")
+    print(' imprimiendo GET furniture')
+    print(var)
+    
+    if var:
+     shelf = Shelf.objects.filter(furniture=var)
+    else: 
+     shelf= Shelf.objects.all()
+    return render_to_string(
+                    'laboratory/shelf_list.html',
+                     context={
+                       'object_list':shelf
+                       })
+@ajax 
+def list_shelf(request):
+    print("Entro al ajax_view laboryyyyyyyyyyyyyyyyy shelf")
+    return {
+           'inner-fragments':{
+                '#shelf': list_shelf_render(request)
+                                   
+            },
+    }
+    
+    
+def list_shelfobject_render(request):   
+    var=request.GET.get("furniture")
+    print(' imprimiendo GET furniture')
+    print(var)
+    
+    if var:
+     shelfobject = ShelfObject.objects.filter(furniture=var)
+    else: 
+     shelfobject= ShelfObject.objects.all()
+    return render_to_string(
+                    'laboratory/shelfObject_list.html',
+                     context={
+                       'object_list':shelfobject
+                       })
+@ajax 
+def list_shelfobject(request):
+    print("Entro al ajax_view laboryyyyyyyyyyyyyyyyy shelfObject")
+    return {
+           'inner-fragments':{
+                '#shelfobject': list_shelfobject_render(request)
+                                   
+            },
+    }
+    
+class ShelfObjectCreate(AJAXMixin, CreateView):
+    model= ShelfObject
+    fields="__all__"
+    success_url= reverse_lazy('list_shelf')
+    
+    def post(self, request, *args, **kwargs):
+        response = CreateView.post(self, request, *args, **kwargs)
+        
+        if type(response) == HttpResponseRedirect:
+             return list_shelfobject_render(request)
+         
+        return response
+    
+class ShelfObjectDelete(AJAXMixin, DeleteView):
+    model= ShelfObject
+    success_url= reverse_lazy('list_shelf')
+    
+    
+    def post(self, request, *args, **kwargs):
+        response = DeleteView.post(self, request, *args, **kwargs)
+        
+        if type(response) == HttpResponseRedirect:
+             return list_shelfobject_render(request)
+         
+        return response
--- laboratory/views.py
+++ laboratory/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def vistaEdificio(request): 
+    return render(request, 'vista_edificio.html') 
