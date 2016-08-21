ec1a52eae5fe6c5ed15f92745a1aa56c393f8d9a
Mau Rodríguez <mauricio.rodriguez_mora@ucr.ac.cr>
2016-08-21 14:34:02 -0600

    Refactor the code and changed the location of ajax_view.py

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -4,22 +4,21 @@ Created on 11/8/2016
 @author: natalia
 '''
 from django.conf.urls import url
-from laboratory.generic import ObjectDeleteFromShelf, ObjectList,\
+from laboratory.generic import ObjectDeleteFromShelf, ObjectList, \
     LaboratoryRoomsList
-from laboratory.templates.laboratory.ajax_view import list_furniture, list_shelf, list_shelfobject, ShelfObjectCreate, ShelfObjectDelete
+from laboratory.ajax_view import list_furniture, list_shelf, list_shelfobject, ShelfObjectCreate, ShelfObjectDelete
 
-
-urlpatterns= [
-url(r"^delete/(?P<pk>\d+)$", ObjectDeleteFromShelf.as_view(),name="object_Delete"),
-#url(r"^list$", ObjectList.as_view(), name='object-list')
-url(r"^listLaboratoryRooms$", LaboratoryRoomsList.as_view(), name='laboratoryRooms_list')
-]
-urlpatterns +=[
-url(r"^furniture/list$", list_furniture,name= "list_furniture"),
-url(r"^shelf/list$", list_shelf,name= "list_shelf"),
-url(r"^shelfObject/list$", list_shelfobject,name= "list_shelfobject"),
-url(r"^shelfObject/create$",  ShelfObjectCreate.as_view(),name="shelfobject_create"),
-url(r"^shelfObject/delete/(?P<pk>\d+)$", ShelfObjectDelete.as_view(), name="shelfobject_delete")
-
+urlpatterns = [
+    url(r"^delete/(?P<pk>\d+)$", ObjectDeleteFromShelf.as_view(), name="object_Delete"),
+    # url(r"^list$", ObjectList.as_view(), name='object-list')
+    url(r"^listLaboratoryRooms$", LaboratoryRoomsList.as_view(), name='laboratoryRooms_list')
 ]
 
+urlpatterns += [
+    url(r"^furniture/list$", list_furniture, name="list_furniture"),
+    url(r"^shelf/list$", list_shelf, name="list_shelf"),
+    url(r"^shelfObject/list$", list_shelfobject, name="list_shelfobject"),
+    url(r"^shelfObject/create$", ShelfObjectCreate.as_view(), name="shelfobject_create"),
+    url(r"^shelfObject/delete/(?P<pk>\d+)$", ShelfObjectDelete.as_view(), name="shelfobject_delete")
+
+]
--- organilab/settings.py
+++ organilab/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '8sko4lo%ca!0e^-i%7@6hnzxmc7l0^m040n%jy99z_e8cvmta)'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'organilab.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
