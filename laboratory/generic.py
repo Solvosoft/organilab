3a9d8f531a89cf7dfbcc8b6497af5267efbf7a3f
Nashyra Lorig <nashyralb@gmail.com>
2016-08-22 09:23:52 -0600

    add the create class of object

--- laboratory/generic.py
+++ laboratory/generic.py
@@ -7,14 +7,21 @@ Created on 1/8/2016
 from __future__ import unicode_literals
 
 from django.views.generic.edit import CreateView, DeleteView
-from laboratory.models import Shelf, LaboratoryRoom, Furniture
+from laboratory.models import Shelf, LaboratoryRoom, Furniture, Object
 from django.contrib.messages.api import success
 from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse_lazy
-from django.db.models.query import QuerySet    
+from django.db.models.query import QuerySet   
+
+class ObjectCreate(CreateView):
+    model = Object
+    fields = '__all__'
+    success_url = "/"
 
 class ShelfCreate(CreateView):
     model = Shelf
+    fields = '__all__'
+    success_url = "/"
 
 class ShelfDelete(DeleteView):
     model = Shelf
