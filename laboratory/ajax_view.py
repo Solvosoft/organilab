39b061757bf10931b6068c115499914f825aa5c1
Luis Zárate <luis.zarate@solvosoft.com>
2016-08-25 16:36:10 -0600

    Update view with modal way

--- laboratory/ajax_view.py
+++ laboratory/ajax_view.py
@@ -11,106 +11,120 @@ from django.views.generic.edit import CreateView, DeleteView
 from django.urls.base import reverse_lazy
 from django.http.response import HttpResponseRedirect
 
-def list_furniture_render(request):  
-    
-    var=request.GET.get('namelaboratoryRoom','0')
-    print(' _________________   '+var+'   __________namelaboratoryRoom' )
-   
-    
+
+def get_shelves(furniture):
+    # import json
+    # dev =  json.loads(furniture.dataconfig)
+    # hacer algo para pasar de num a shelf
+    return [["", 3, ""],
+            [1, "", ""],
+            ["", 5, ""]
+            ]
+
+
+def list_furniture_render(request):
+
+    var = request.GET.get('namelaboratoryRoom', '0')
+    print(' _________________   ' + var + '   __________namelaboratoryRoom')
+
     if var:
-     furnitures= Furniture.objects.filter(labroom=var)
-    else:   
-     furnitures= Furniture.objects.all()
+        furnitures = Furniture.objects.filter(labroom=var)
+    else:
+        furnitures = Furniture.objects.all()
     return render_to_string(
-                    'laboratory/furniture_list.html',
-                     context={
-                       'object_list':furnitures
-                       })
-@ajax 
+        'laboratory/furniture_list.html',
+        context={
+            'object_list': furnitures
+        })
+
+
+@ajax
 def list_furniture(request):
    # print("Entro al ajax_view labory furniture")
     return {
-           'inner-fragments':{
-                '#furnitures': list_furniture_render(request)
-                                   
-            },
+        'inner-fragments': {
+            '#furnitures': list_furniture_render(request)
+
+        },
     }
-    
-    
-    
-    
-def list_shelf_render(request):   
-    var=request.GET.get('furniture','0')
-    print(' _________________   '+var+'   __________furniture' )
-    
-    if var:
-     shelf = Shelf.objects.filter(furniture=var)
-    else: 
-     shelf= Shelf.objects.all()
+
+
+def list_shelf_render(request):
+    var = request.GET.get('furniture', '0')
+    print(' _________________   ' + var + '   __________furniture')
+
+    furniture = Furniture.objects.filter(pk=var)
+    shelf = get_shelves(furniture)
+
     return render_to_string(
-                    'laboratory/shelf_list.html',
-                     context={
-                       'object_list':shelf
-                       })
-@ajax 
+        'laboratory/shelf_list.html',
+        context={
+            'object_list': shelf
+        })
+
+
+@ajax
 def list_shelf(request):
     print("Entro al ajax_view laboryyyyyyyyyyyyyyyyy shelf")
     return {
-           'inner-fragments':{
-                '#shelf': list_shelf_render(request)
-                                   
-            },
+        'inner-fragments': {
+            '#shelf': list_shelf_render(request)
+
+        },
     }
-    
-    
-def list_shelfobject_render(request):   
-    var=request.GET.get('shelf','0')
-    
-    print(' _________________   '+var+'   __________shelf' )
-    
+
+
+def list_shelfobject_render(request):
+    var = request.GET.get('shelf', '0')
+
+    print(' _________________   ' + var + '   __________shelf')
+
     if var:
-     shelfobject = ShelfObject.objects.filter(object=var)
-    else: 
-     shelfobject= ShelfObject.objects.all()
+        shelfobject = ShelfObject.objects.filter(object=var)
+    else:
+        shelfobject = ShelfObject.objects.all()
     return render_to_string(
-                    'laboratory/shelfObject_list.html',
-                     context={
-                       'object_list':shelfobject
-                       })
-@ajax 
+        'laboratory/shelfObject_list.html',
+        context={
+            'object_list': shelfobject
+        })
+
+
+@ajax
 def list_shelfobject(request):
     print("Entro al ajax_view laboryyyyyyyyyyyyyyyyy shelfObject")
     return {
-           'inner-fragments':{
-                '#shelfobject': list_shelfobject_render(request),
-                '#shelfposition': request.GET.get('shelf','0'),
-                '#shelfposition1': request.GET.get('shelf','0')          
-            
-            },
+        'inner-fragments': {
+            '#shelfobject': list_shelfobject_render(request),
+            '#shelfposition': request.GET.get('shelf', '0'),
+            '#shelfposition1': request.GET.get('shelf', '0')
+
+        },
     }
-    
+
+
 class ShelfObjectCreate(AJAXMixin, CreateView):
-    model= ShelfObject
-    fields="__all__"
-    success_url= reverse_lazy('list_shelf')
-    
+    model = ShelfObject
+    fields = "__all__"
+    success_url = reverse_lazy('list_shelf')
+
     def post(self, request, *args, **kwargs):
         response = CreateView.post(self, request, *args, **kwargs)
-        
+
         if type(response) == HttpResponseRedirect:
-             return list_shelfobject_render(request)
-         
+            return list_shelfobject_render(request)
+
         return response
-    
+
+
 class ShelfObjectDelete(AJAXMixin, DeleteView):
-    model= ShelfObject
-    success_url= reverse_lazy('list_shelf')
-    
-    
+    model = ShelfObject
+    success_url = reverse_lazy('list_shelf')
+
     def post(self, request, *args, **kwargs):
         response = DeleteView.post(self, request, *args, **kwargs)
-        
+
         if type(response) == HttpResponseRedirect:
-             return list_shelfobject_render(request)
-         
+            return list_shelfobject_render(request)
+
         return response
