bdd996e531484eabaa1a9b346258311f4591b623
Luis Zárate <luis.zarate@solvosoft.com>
2016-08-26 03:50:56 -0600

    Shelf table with add and delete rows/columns

--- laboratory/static/laboratory/js/furniture_table.js
+++ laboratory/static/laboratory/js/furniture_table.js
@@ -2,61 +2,38 @@ var numberCol = 2,
 	    numberRow = 2;
 
 	function addRow() {
-		var root = document.getElementById('mytab').getElementsByTagName('tbody')[0];
-		var rows = root.getElementsByTagName('tr');
-		var clone = cloneEl(rows[rows.length - 1]);
-		root.appendChild(clone);
+		sk=$("<tr> </tr>");
+		$('#mytab tr:last td').each(function(i, e){
+			sk.append(
+				"<td>"+$("#addshelfproto").html()+"</td>"
+			);
+					numberCol++;
+		});
+		
+		$('#mytab tr').last().after(sk);
 		numberRow++;
 	}
 
 	function addColumn() {
-		var rows = document.getElementById('mytab').getElementsByTagName('tr'),
-		    i = 0,
-		    r,
-		    c,
-		    clone;
-		while ( r = rows[i++]) {
-			c = r.getElementsByTagName('td');
-			clone = cloneEl(c[c.length - 1]);
-			c[0].parentNode.appendChild(clone);
-		}
-		numberCol++;
+		$('#mytab tr').each(function(i, e){
+			$(e).append(
+				"<td>"+$("#addshelfproto").html()+"</td>"
+			);
+					numberCol++;
+		});
 	}
 
-	function cloneEl(el) {
-		var clo = el.cloneNode(true);
-		return clo;
-	}
 
 	function deleteRows() {
-		if (numberRow > 1) {
-			var tbl = document.getElementById('mytab'), // table reference
-			    lastRow = tbl.rows.length - 1, // set the last row index
-			    i;
-			// delete rows with index greater then 0
-			for ( i = lastRow; i > 0; i++) {
-				tbl.deleteRow(i);
-			}
-			numberRow--;
-		}
+			$('#mytab tr').last().remove();
 	}
 
 	// delete table columns with index greater then 0
 	function deleteColumns() {
-		if (numberCol > 1) {
-			var tbl = document.getElementById('mytab'), // table reference
-			    lastCol = tbl.rows[0].cells.length - 1, // set the last column index
-			    i,
-			    j,
-			    deleteCol;
-			// delete cells with index greater then 0 (for each row)
-			for ( i = 0; i < tbl.rows.length; i++) {
-				//for ( j = lastCol; j > 0; j--) {
-				tbl.rows[i].deleteCell(lastCol);
-				//}
-			}
-			numberCol--;
-		}
+		$('#mytab tr').each(function(i, e){
+			$(e).find("td:last").remove();
+			
+		});
 
 	}
 
