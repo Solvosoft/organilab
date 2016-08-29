7a318643f1ed7d9c0699eb7ec628ad53a5c6438f
Luis Zárate <luis.zarate@solvosoft.com>
2016-08-29 15:48:57 -0600

    Shelf working

--- laboratory/static/laboratory/js/furniture_table.js
+++ laboratory/static/laboratory/js/furniture_table.js
@@ -1,42 +1,62 @@
-var numberCol = 2,
-	    numberRow = 2;
-
-	function addRow() {
-		sk=$("<tr> </tr>");
+function addRow() {
+	sk=$("<tr> </tr>");
+	if($('#mytab tr:last td').length>0){
 		$('#mytab tr:last td').each(function(i, e){
-			sk.append(
-				"<td>"+$("#addshelfproto").html()+"</td>"
-			);
-					numberCol++;
+			var inp = "<td>"+$("#addshelfproto").html()+"</td>";
+			inp=inp.replace(/ROW/g, ""+document.numberRow);
+			inp=inp.replace(/COL/g, ""+i);
+			sk.append(inp);
 		});
-		
-		$('#mytab tr').last().after(sk);
-		numberRow++;
+	} else {
+		var inp = "<td>"+$("#addshelfproto").html()+"</td>";
+		inp=inp.replace(/ROW/g, ""+document.numberRow);
+		inp=inp.replace(/COL/g, ""+document.numberCol);	
+		sk.append(inp);
+		document.numberCol++;
 	}
+	$('#mytab').append(sk);
+	document.numberRow++;
+}
 
-	function addColumn() {
+function addColumn() {
+	if($('#mytab tr').length>0){
 		$('#mytab tr').each(function(i, e){
-			$(e).append(
-				"<td>"+$("#addshelfproto").html()+"</td>"
-			);
-					numberCol++;
-		});
+			var inp = "<td>"+$("#addshelfproto").html()+"</td>";
+			inp=inp.replace(/ROW/g, ""+i);
+			inp=inp.replace(/COL/g, ""+document.numberCol);	
+			$(e).append(inp);
+		});			
+	}else{
+		var inp = "<td>"+$("#addshelfproto").html()+"</td>";
+		inp=inp.replace(/ROW/g, ""+document.numberRow);
+		inp=inp.replace(/COL/g, ""+document.numberCol);		
+		$('#mytab').append("<tr>"+inp+"</tr>");	
+		document.numberRow++;	
 	}
 
+	document.numberCol++;
+}
 
-	function deleteRows() {
-			$('#mytab tr').last().remove();
-	}
 
-	// delete table columns with index greater then 0
-	function deleteColumns() {
-		$('#mytab tr').each(function(i, e){
-			$(e).find("td:last").remove();
+function deleteRows() {
+	$('#mytab tr').last().remove();
+	document.numberRow--;
+}
+
+// delete table columns with index greater then 0
+function deleteColumns() {
+	$('#mytab tr').each(function(i, e){
+		$(e).find("td:last").remove();
 			
 		});
-
+		document.numberCol--;
 	}
 
-	function processResponse(data) {
-		$('#shelves').html(data);
-	}
+function closeModal(){
+	$("#createshelfmodal").modal("hide");
+}
+
+function processResponse(data) {
+	$("#shelfmodalbody").html(data);
+	activemodal = $("#createshelfmodal").modal('show');
+}
--- laboratory/views.py
+++ laboratory/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.utils import timezone
-from laboratory.models import LaboratoryRoom, Furniture, Object
-from django.template.loader import get_template
+from laboratory.models import LaboratoryRoom, Furniture, Object, Shelf
+from django.template.loader import get_template, render_to_string
 from django.template.context import Context
 from weasyprint import HTML
-
-from django.views.generic.edit import CreateView
+import json
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls.base import reverse_lazy
 
 
 class miContexto(object):
@@ -141,4 +142,44 @@ def index(request):
 class FurnitureCreateView(CreateView):
     model = Furniture
     success_url = '/'
-    fields = '__all__'
+    fields = ("labroom", "name", "type")
+
+    def get_success_url(self):
+
+        return reverse_lazy("laboratory:furniture_update", kwargs={'pk': self.object.pk})
+
+
+class FurnitureUpdateView(UpdateView):
+    model = Furniture
+    success_url = '/'
+    fields = ("labroom", "name", "type")
+
+    def get_context_data(self, **kwargs):
+        context = UpdateView.get_context_data(self, **kwargs)
+        context['dataconfig'] = self.build_configdata()
+        return context
+
+    def get_dataconfig(self):
+        dataconfig = self.object.dataconfig
+        if dataconfig:
+            dataconfig = json.loads(dataconfig)
+
+        for irow, row in enumerate(dataconfig):
+            for icol, col in enumerate(row):
+                if col:
+                    dataconfig[irow][icol] = Shelf.objects.filter(
+                        pk__in=col.split(","))
+        return dataconfig
+
+    def build_configdata(self):
+        dataconfig = self.get_dataconfig()
+        row = len(dataconfig)
+        if row > 0:
+            col = len(dataconfig[0])
+        else:
+            col = 0
+        return render_to_string('laboratory/dataconfig.html',
+                                {'dataconfig': dataconfig,
+                                 'obj': self.object,
+                                 'col': col,
+                                 'row': row})
