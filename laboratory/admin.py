a12e707a3d99ce4c49deba597d78821c7e359e00
Luis Zárate <luis.zarate@solvosoft.com>
2016-09-03 00:13:53 -0600

    Reservations first commit

--- laboratory/admin.py
+++ laboratory/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from laboratory import models
-from .models import LaboratoryRoom
+
+
 admin.site.register(models.LaboratoryRoom)
 admin.site.register(models.Furniture)
 admin.site.register(models.Shelf)
 admin.site.register(models.ObjectFeatures)
 admin.site.register(models.Object)
 admin.site.register(models.ShelfObject)
-
-
-
---
+++ laboratory/reservation.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+'''
+Free as freedom will be 2/9/2016
+
+@author: luisza
+'''
+
+from __future__ import unicode_literals
+from laboratory.models import ShelfObject
+from djreservation.views import ProductReservationView
+
+
+class ShelfObjectReservation(ProductReservationView):
+    base_model = ShelfObject
+    modelpk = None
+    amount_field = 'quantity'
+    extra_display_field = ['measurement_unit']
