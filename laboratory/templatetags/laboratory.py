18a23fd63e87bb8e98f65c0f92723adddd3cb2d5
Luis Zárate Montero <luis.zarate@solvosoft.com>
2017-05-04 20:08:31 -0600

    Fixed perms management

---
+++ laboratory/templatetags/laboratory.py
@@ -0,0 +1,20 @@
+'''
+Created on 4 may. 2017
+
+@author: luis
+'''
+from django import template
+from laboratory.utils import check_lab_perms
+register = template.Library()
+
+from laboratory.models import Laboratory
+
+@register.simple_tag(takes_context=True)
+def has_laboratory_perm(context, perm):
+    user = context['request'].user
+    lab_pk = context['request'].session.get('lab_pk')
+    lab = Laboratory.objects.filter(pk=lab_pk).first()
+    if lab is not None:
+        return check_lab_perms(lab, user, perm)
+    
+    return False
