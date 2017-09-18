135ad9bd3579318980ec999353470fe8bdde2f92
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2017-09-18 08:31:11 -0600

    Issue 27 18 sept

--- laboratory/forms.py
+++ laboratory/forms.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from ajax_select.fields import AutoCompleteSelectMultipleField
 from django.utils.translation import ugettext_lazy as _
 
+from django.core import validators
+
 class ObjectSearchForm(forms.Form):
     q = AutoCompleteSelectMultipleField('objects', required=False, help_text=_("Search by name, code or CAS number"))
     all_labs = forms.BooleanField(widget=forms.CheckboxInput, required=False, label="All labs")
@@ -19,6 +21,32 @@ class UserCreate(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget = forms.PasswordInput)
     password_verification = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_password_verification(self):
+        password = self.cleaned_data.get("password")
+        password_verification = self.cleaned_data.get("password_verification")
+        if password and password_verification and password != password_verification:
+            raise forms.ValidationError(
+                "Passwords don't match.",code='password_mismatch',
+            )
+        return password_verification
+
+    """def clean(self):
+        form_data = self.cleaned_data
+        if form_data['password'] != form_data['password_verification']:
+            self._errors["password"] = ["Password do not match"] # Will raise a error message
+            del form_data['password']
+        return form_data"""
+
+    def save(self):
+        username = self.cleaned_data['username']
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+
+        user = User.objects.create(username=username,first_name=first_name, last_name=last_name, email=email, password=password)
+        user.save()
 
 class UserAccessForm(forms.Form):
     access = forms.BooleanField(widget = forms.CheckboxInput(attrs={'id':'user_cb_'})) #User_checkbox_id
--- laboratory/views/access.py
+++ laboratory/views/access.py
@@ -11,7 +11,7 @@ from django.utils.decorators import method_decorator
 from laboratory.models import Laboratory
 from laboratory.forms import UserCreate, UserSearchForm, UserAccessForm
 
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, CreateView
 
 from django.utils.translation import ugettext_lazy as _
 from django.db.models.query_utils import Q
@@ -21,20 +21,39 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.shortcuts import render
+
+from django.http import HttpResponseRedirect
+
+
+global user_create_form
+user_create_form = UserCreate()
+
+def save_user(request, lab_pk):
+    if request.method == "POST":
+        user_create_form = UserCreate(request.POST)
+        if user_create_form.is_valid():
+            user_create_form.save()
+            #return redirect()
+            return HttpResponseRedirect("/")
+    else:
+        user_create_form = UserCreate()
+    return render(request, 'laboratory/user_create_form.html', {'user_create_form': user_create_form})
 
 class AccessListLabAdminsView(FormView, ListView):
     model = User
     template_name = 'laboratory/access_list.html'
     lab_pk = None
     paginate_by = 10
-    #user_form = UserCreate()
     form_class = UserSearchForm
-    second_form_class = UserCreate
-    queryset = User.objects.filter(groups__name = 'laboratory_admin')
 
     def dispatch(self, request, *args, **kwargs):
         self.lab_pk = kwargs.get('lab_pk')
         return super(AccessListLabAdminsView, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        lab = get_object_or_404(Laboratory, pk=self.lab_pk)
+        return lab.lab_admins.all()
 
     def get_success_url(self):
         return reverse('laboratory:access_list_lab_admins', kwargs={'lab_pk':self.lab_pk})
@@ -45,8 +64,7 @@ class AccessListLabAdminsView(FormView, ListView):
         context['laboratory'] = self.lab_pk
         context['role'] = 1
         context['user_search'] = self.form_class
-        context['user_create'] = self.second_form_class
-        context['user_access'] = UserAccessForm()
+        context['user_create_form'] = user_create_form
         return context
 
     def form_valid(self, form):
@@ -60,20 +78,34 @@ class AccessListLabAdminsView(FormView, ListView):
         messages.info(self.request, "User added successfully")
         return redirect(self.get_success_url())
 
+    """@classmethod
+    def unasign_access(self, request, user_id):
+        print("Entra")
+        admin_group = Group.objects.get(name='laboratory_admin')
+        laboratory = get_object_or_404(Laboratory, pk=self.lab_pk)
+
+        userobj = User.objects.get(pk=user_id)
+        #userobj.groups.remove(admin_group)
+        if laboratory.lab_admins.filter(id=user_id).exists():
+            laboratory.lab_admins.remove(user_id)
+        messages.info(self.request, "User unasigned successfully")
+        return redirect(self.get_success_url())"""
 
 class AccessListLaboratoritsView(FormView, ListView):
     model = User
     template_name = 'laboratory/access_list.html'
     lab_pk = None
     paginate_by = 10
-    user_form = UserCreate()
     form_class = UserSearchForm
-    queryset = User.objects.filter(groups__name = 'laboratory_professor')
-    #Change to laboratorist
 
     def dispatch(self, request, *args, **kwargs):
         self.lab_pk = kwargs.get('lab_pk')
         return super(AccessListLaboratoritsView, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        lab = get_object_or_404(Laboratory, pk=self.lab_pk)
+        return lab.laboratorists.all()
+        #Change to laboratorist
 
     def get_success_url(self):
         return reverse('laboratory:access_list_laboratorits', kwargs = {'lab_pk':self.lab_pk})
@@ -83,9 +115,8 @@ class AccessListLaboratoritsView(FormView, ListView):
         context['lab_pk'] = self.lab_pk
         context['laboratory'] = self.lab_pk
         context['role'] = 2
-        context['user_create'] = self.user_form
+        context['user_create_form'] = user_create_form
         context['user_search'] = self.form_class
-        context['user_access'] = UserAccessForm()
         return context
 
     def form_valid(self, form):
@@ -106,7 +137,6 @@ class AccessListStudentsView(FormView, ListView):
     paginate_by = 10
     user_form = UserCreate()
     form_class = UserSearchForm
-    queryset = User.objects.filter(groups__name = 'laboratory_student')
 
     def dispatch(self, request, *args, **kwargs):
         self.lab_pk = kwargs.get('lab_pk')
@@ -115,12 +145,16 @@ class AccessListStudentsView(FormView, ListView):
     def get_success_url(self):
         return reverse('laboratory:access_list_students', kwargs = {'lab_pk':self.lab_pk})
 
+    def get_queryset(self):
+        lab = get_object_or_404(Laboratory, pk=self.lab_pk)
+        return lab.students.all()
+
     def get_context_data(self, **kwargs):
         context = super(AccessListStudentsView, self).get_context_data(**kwargs)
         context['lab_pk'] = self.lab_pk
         context['laboratory'] = self.lab_pk
         context['role'] = 3
-        context['user_create'] = self.user_form
+        context['user_create_form'] = user_create_form
         context['user_search'] = self.form_class
         context['user_access'] = UserAccessForm()
         return context
@@ -135,3 +169,28 @@ class AccessListStudentsView(FormView, ListView):
                 laboratory.students.add(user)
         messages.info(self.request, "User added successfully")
         return redirect(self.get_success_url())
+
+
+"""class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreate
+    template_name = 'laboratory/user_create_form.html'
+    lab_pk = None
+
+    def get_context_data(self, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['lab_pk'] = self.lab_pk
+        return context
+
+    def get_success_url(self):
+        return reverse('laboratory:create_user', kwargs = {'lab_pk':self.lab_pk})
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        self.lab_pk = kwargs.get('lab_pk')
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+      self.object = form.save(commit=False)
+      self.object.save()
+      return HttpResponseRedirect(reverse('category', args=[self.object.slug]))"""
