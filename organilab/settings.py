f37d44c79f98a6f8663b7351d2c51ed8ab04f4c7
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2017-10-16 04:16:31 -0600

    Password reset

--- organilab/settings.py
+++ organilab/settings.py
@@ -139,8 +139,17 @@ LOGIN_REDIRECT_URL = reverse_lazy('laboratory:index')
 
 # Email development settings
 DEFAULT_FROM_EMAIL = "mail@example.com"
-EMAIL_HOST = "localhost"
-EMAIL_PORT = "1025"
+#EMAIL_HOST = "localhost"
+#EMAIL_PORT = "1025"
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'something@gmail.com' # a real email
+EMAIL_HOST_PASSWORD = 'a password' # the password of the real email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Celery settings
 BROKER_URL = 'redis://localhost:6379'
