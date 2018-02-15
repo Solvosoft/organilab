7ee4e18c821df63c7d23cc51f03260bd61294646
= <miguel.arguedas.mejias@gmail.com>
2018-02-15 13:45:15 -0600

    Adding multiformat report of organizations

--- laboratory/views/reports.py
+++ laboratory/views/reports.py
@@ -23,6 +23,59 @@ import django_excel
 from django.db.models.aggregates import Sum, Min
 
 
+def make_book_organization_laboratory(objects):
+    dev=[
+        [_("Name"), _("Phone"), _("Location")]
+        ]
+    for object in objects:
+        dev.append([
+            object.name,
+            object.phone_number,
+            object.location,
+            ])
+    return dev
+    
+    
+@login_required
+@user_lab_perms(perm="report")
+def report_organization_building(request, *args, **kwargs):
+    var = request.GET.get('pk')
+    if var:
+            organizations_child = OrganizationStructure.objects.get(pk=var).get_descendants(include_self=True)
+            labs=Laboratory.objects.filter(organization__in=organizations_child)
+            labs = get_object_or_404(
+                 Laboratory, pk__in=labs)
+    else:
+            labs = Laboratory.objects.all()
+    
+    fileformat = request.GET.get('format', 'pdf')
+    if fileformat in ['xls', 'xlsx', 'ods']:
+        return django_excel.make_response_from_array(
+        make_book_organization_laboratory(labs), fileformat ,file_name="Laboratories.%s"%(fileformat,))
+
+    
+    context = {
+        'object_list': labs,
+        'datetime': timezone.now(),
+        'request': request,
+        'laboratory': kwargs.get('lab_pk')
+    }
+
+    template = get_template('pdf/laboratoryroom_pdf.html')
+
+    html = template.render(
+        context=context).encode("UTF-8")
+
+    page = HTML(string=html, encoding='utf-8').write_pdf()
+
+    response = HttpResponse(page, content_type='application/pdf')
+    response[
+        'Content-Disposition'] = 'attachment; filename="report_organization_libraries.pdf"'
+    return response
+
+
+
+
 def make_book_laboratory(rooms):
     
     content={}
@@ -51,40 +104,6 @@ def make_book_laboratory(rooms):
             content[labroom.name] = labobj
   
     return content
-
-@login_required
-@user_lab_perms(perm="report")
-def report_organization_building(request, *args, **kwargs):
-    var = request.GET.get('organization')
-    print (var)
-    if var is None:
-        if 'organization' in kwargs:
-            organizations_child = OrganizationStructure.objects.get(pk=svar).get_descendants(include_self=True)
-            labs = get_object_or_404(
-                 Laboratory, organization__in=organizations_child).rooms.all()
-    else:
-            labs = Laboratory.objects.all()
-    
-    context = {
-        'object_list': labs,
-        'datetime': timezone.now(),
-        'request': request,
-        'laboratory': kwargs.get('lab_pk')
-    }
-
-    template = get_template('pdf/laboratoryroom_pdf.html')
-
-    html = template.render(
-        context=context).encode("UTF-8")
-
-    page = HTML(string=html, encoding='utf-8').write_pdf()
-
-    response = HttpResponse(page, content_type='application/pdf')
-    response[
-        'Content-Disposition'] = 'attachment; filename="report_shelf_objects.pdf"'
-    return response
-
-
 
 
 @login_required
