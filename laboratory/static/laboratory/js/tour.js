31a2a83d5ee067afba4b9e6c42a39e93844d016c
Luis Zárate Montero <luis.zarate@solvosoft.com>
2018-02-16 09:53:27 -0600

    tour

--- laboratory/static/laboratory/js/tour.js
+++ laboratory/static/laboratory/js/tour.js
@@ -7,9 +7,9 @@ $.ajax({
     url: '/_ajax/get_tour_steps',
     success: function (data){
         var tour_steps = JSON.parse(data.content);
-        console.log(tour_steps);
         var tour = new Tour({
-            steps: tour_steps
+            steps: tour_steps,
+            template: '<div class="popover" role="tooltip"> <div class="arrow"></div> <h3 class="popover-title"></h3> <div class="popover-content"></div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-default" data-role="prev">Atras</button> <button class="btn btn-sm btn-default" data-role="next">Siguiente</button> <button class="btn btn-sm btn-default" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> <button class="btn btn-sm btn-default" data-role="end">Finalizar tour</button> </div> </div>'
         });
         
         tour.init();
