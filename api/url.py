4f766f7f33a287a22a6a5bca92ff2689ca31f154
= <miguel.arguedas.mejias@gmail.com>
2018-03-15 16:05:50 -0600

    laboratory DEL,PUT,POST,GET

--- api/url.py
+++ api/url.py
@@ -15,15 +15,18 @@ app_name = 'api'
 
 # TOKEN URLs[
 urlpatterns = [url(r'^token-auth/$', views.obtain_auth_token, name='get_auth_token')
+               
 ]
 
 
 # API URLs
 room_api_urls = [
-    url(r'^$',LaboratoryRoomAPIView.as_view(),name='api_getpost_laboratoryroom'),
+        url(r'^$',LaboratoryRoomAPIView.as_view(),name='api_laboratoryroom'),
+        url(r'^(?P<pk>\d+)/$',LaboratoryRoomAPIView.as_view(),name='api_laboratoryroom_updates'),
     ]
-               
 
-urlpatterns += [url(r'^(?P<lab_pk>\d+)/rooms', include(room_api_urls)),
+
+
+urlpatterns += [url(r'^(?P<lab_pk>\d+)/rooms/', include(room_api_urls)),
                 
                 ]
