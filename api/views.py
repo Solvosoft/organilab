e6fbc4aca2b290d44c0396827ded0af6dc1dfad6
= <miguel.arguedas.mejias@gmail.com>
2018-03-15 13:55:11 -0600

    Completed user auth on get

--- api/views.py
+++ api/views.py
@@ -3,13 +3,11 @@ Created on 1/14/2018
 
 @author: migue56
 '''
-from django.shortcuts import get_object_or_404
-
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from .utils import filters_params_api, get_valid_lab
+from .utils import filters_params_api, get_valid_lab, get_response_code
 
 from .serializers import ( LaboratoryRoomSerializer,
                            FurnitureSerializer,
@@ -44,22 +42,40 @@ class LaboratoryRoomAPIView(GenericAPIView):
         """
         params = request.query_params
         user = request.user
-        lab = get_valid_lab(lab_pk,user,'laboratory.view_laboratoryroom')
+        (lab,perm) = get_valid_lab(lab_pk,user,'laboratory.view_laboratoryroom')
         
-        if lab:
-              self.queryset= lab.rooms.all()     
-        if params: # it can have filters params
-            self.queryset=filters_params_api(self.queryset,params,LaboratoryRoom)
+        if perm:
+            self.queryset= lab.rooms.all()     
+            if params: # it can have filters params
+                self.queryset=filters_params_api(self.queryset,params,LaboratoryRoom)
+                
+            queryset = self.paginate_queryset(queryset=self.queryset)
+        
+            serialiser = LaboratoryRoomSerializer(instance=queryset, many=True)
+            return self.get_paginated_response(serialiser.data)    
+        else: 
+            return get_response_code(403)
             
-        queryset = self.paginate_queryset(queryset=self.queryset)
-    
-        serialiser = LaboratoryRoomSerializer(instance=queryset, many=True)
-        return self.get_paginated_response(serialiser.data)    
-        
-        
         
            
-    
+    def put (self,request,lab_pk, *arg,**kwargs):
+        """
+        Put:
+        Use this to get your laboratoryrooms, to filter this result you can add query params to the url
+        
+        build your get whit your user token valid
+         Header: [{"key":"Authorization","value":"Token  da293ee2561211f1edf5b6722f349a2e23898cbe"}]
+         
+        """
+        user = request.user
+        (lab,perm) = get_valid_lab(lab_pk,user,'laboratory.change_laboratoryroom')
+        if perm:
+            lab_room_serializer = LaboratoryRoomSerializer(data=request.data)
+            if lab_room_serializer.is_valid():
+                lab_room_serializer.save();
+                
+            
+        
 # class FurnitureAPIView(GenericAPIView):
 #     
 #     
