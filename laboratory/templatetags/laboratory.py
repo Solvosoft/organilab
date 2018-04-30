ff0775d2d2c6bf7a9b0425c66c7d19b226fa12c0
Luis Zárate M <luis.zarate@solvosoft.com>
2018-04-30 17:56:22 -0600

    Perms manager in index and navbar

--- laboratory/templatetags/laboratory.py
+++ laboratory/templatetags/laboratory.py
@@ -28,20 +28,47 @@ def get_search_form(context):
 def has_perms(context, codename):
     if 'request' in context:
         user = context['request'].user
-        lab_pk = context['request'].session.get('lab_pk') 
-        
-        # Permit to redirect User to select form
-        if not lab_pk:
-            return True
-         
-        if user.has_perm(codename) :
+        if user.has_perm(codename) or user.is_superuser:
             return True
         else:
+            lab_pk = context['request'].resolver_match.kwargs.get('lab_pk', None)
+            
+            # Permit to redirect User to select form
+            if not lab_pk:
+                return False
             lab = get_object_or_404(Laboratory, pk=lab_pk)
             if check_lab_group_has_perm(user,lab,codename,filter_laboratorist_technician):
                 return True
     return False
 
+
+
+@register.simple_tag(takes_context=True)
+def index_permissions(context):
+    dev = {
+        'view_laboratory': has_perms(context, "laboratory.view_laboratory"), 
+        'view_procedure':   has_perms(context, "academic.view_procedure"),
+        'view_solutions': has_perms(context, "laboratory.view_solution"),
+
+        'add_laboratory':  has_perms(context, "laboratory.add_laboratory"),  
+        'manage_laboratory':  has_perms(context, "laboratory.change_laboratory"),
+        'add_furniture':  has_perms(context, "laboratory.add_furniture"),
+        'add_object':  has_perms(context, "laboratory.add_object"),
+        "add_features":  has_perms(context, "laboratory.add_objectfeatures"),
+        'view_reports':  has_perms(context, "laboratory.view_report"),
+        'do_reports': has_perms(context, "laboratory.do_report"),
+        'add_reservation': has_perms(context, "laboratory.add_reservation"),
+        }
+    
+    
+    dev['show_labview'] = dev['view_laboratory'] or dev['view_procedure'] or dev['view_solutions']
+    dev['admin_lab'] = dev['add_laboratory'] or dev['manage_laboratory'] \
+                        or dev['add_furniture'] or dev['add_object'] \
+                        or dev["add_features"]
+                        
+    dev['show_reports'] = dev['view_reports'] or dev['do_reports']
+    
+    return dev
     
     
 @register.simple_tag
