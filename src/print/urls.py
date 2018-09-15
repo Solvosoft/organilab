8cb0c9cfa5196c1c2e5dfa1de6050d3d14db92b6
luisfelipe7 <luisfelipecs97@gmail.com>
2018-09-15 00:09:53 -0600

    CRUD View print

--- src/print/urls.py
+++ src/print/urls.py
@@ -5,9 +5,12 @@ Created on 14 sep. 2018
 '''
 
 from django.conf.urls import url
-from .views import index_print
-# from msds.views import MSDSObjectCRUD
+from .views import index_print, get_list_print
+from print.views import PrintCRUD
 
-urlpatterns = [
+print = PrintCRUD()
+
+urlpatterns = print.get_urls() + [
     url(r'index_print', index_print, name='index_print'),
+    url(r'^list$', get_list_print, name="list_print"),
 ]
--- src/print/views.py
+++ src/print/views.py
@@ -10,12 +10,78 @@ from django.db.models.query_utils import Q
 from django.core.paginator import Paginator
 from django.utils.translation import ugettext as _
 from cruds_adminlte.crud import CRUDView
-# from msds.forms import FormMSDSobject, FormMSDSobjectUpdate
+from print.forms import FormPrint
 from django.urls.base import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render
+from print.models import Print
 
 
 def index_print(request):
     return render(request, 'index_print.html')
+
+
+def get_list_print(request):
+    q = request.GET.get('search[value]')
+    length = request.GET.get('length', '10')
+    pgnum = request.GET.get('start', '0')
+
+    try:
+        length = int(length)
+        pgnum = 1 + (int(pgnum) / length)
+    except:
+        length = 10
+        pgnum = 1
+
+    if q:
+        objs = Print.objects.filter(
+            Q(name__icontains=q) | Q(location__icontains=q)
+        ).order_by('name')
+    else:
+        objs = Print.objects.all().order_by('name')
+
+    recordsFiltered = objs.count()
+    p = Paginator(objs, length)
+    if pgnum > p.num_pages:
+        pgnum = 1
+    page = p.page(pgnum)
+    data = []
+    for obj in page.object_list:
+        data.append([
+            obj.id,
+            obj.name,
+            obj.email,
+            #            get_download_links(request, obj)
+        ])
+    dev = {
+        "data": data,
+        "recordsTotal": Print.objects.all().count(),
+        "recordsFiltered": recordsFiltered
+    }
+
+    draw = request.GET.get('_', '')
+    try:
+        draw = int(draw)
+        dev['draw'] = draw
+    except:
+        pass
+    return JsonResponse(dev)
+
+
+class PrintCRUD(CRUDView):
+    model = Print
+    views_available = ['create', 'delete', 'update', 'detail']
+    namespace = "print"
+    add_form = FormPrint
+
+    def get_create_view(self):
+        CreateViewClass = super(PrintCRUD, self).get_create_view()
+
+        class OCreateView(CreateViewClass):
+            def get_success_url(self):
+                url = reverse("print:index_print")
+                messages.success(self.request,
+                                 _("Your Print was register successfully"))
+                return url
+        return OCreateView
