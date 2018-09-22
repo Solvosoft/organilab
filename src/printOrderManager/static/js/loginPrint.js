a519284093e926d0dde821447fc3418d9cc10205
luisfelipe7 <luisfelipecs97@gmail.com>
2018-09-21 22:41:43 -0600

    Map in PrintRegister

--- src/printOrderManager/static/js/loginPrint.js
+++
@@ -1,9 +0,0 @@
-var hash = document.location.hash;
-if (hash) {
-    $('.nav-tabs a[href=' + hash + ']').tab('show');
-}
-
-// Change hash for page-reload
-$('.nav-tabs a').on('shown.bs.tab', function (e) {
-    window.location.hash = e.target.hash;
-});
