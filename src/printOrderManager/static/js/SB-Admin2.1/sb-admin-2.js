3e5a743f0c06e7bee5d34dd36927613860c94906
luisfelipe7 <luisfelipecs97@gmail.com>
2018-10-18 20:49:08 -0600

    SB Admin interface changed

--- src/printOrderManager/static/js/SB-Admin2.1/sb-admin-2.js
+++ src/printOrderManager/static/js/SB-Admin2.1/sb-admin-2.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
 	//side menu toggle (init)
 	if (isIE() <= 9) {
@@ -8,26 +8,26 @@ $(function() {
 		$('#sidebar').find("li.active").has("ul").children("ul").addClass("collapse in");
 		$('#sidebar').find("li").not(".active").has("ul").children("ul").addClass("collapse");
 	}
-	
+
 	//side menu toggle (setting)
-	$("#sidebar-area .dropdown-collapse").on((jQuery.support.touch ? "tap" : "click"), function(e) {
+	$("#sidebar-area .dropdown-collapse").on((jQuery.support.touch ? "tap" : "click"), function (e) {
 		e.preventDefault();
-		
+
 		if ($("body").hasClass("sidebar-closed")) {
 			return false;
 		}
-		
+
 		$(this).parent("li").toggleClass("active").children("ul").collapse("toggle");
-		
+
 		//if ($toggle) { //toggle On ・ Off
-		
+
 		$(this).parent("li").siblings().removeClass("active").children("ul.in").collapse("hide");
-		
+
 		//}
 		return false;
 	});
-			
-	
+
+
 	handleSidebarToggler();
 });
 
@@ -35,27 +35,29 @@ $(function() {
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
+$(function () {
 	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
 	if ($.cookie && $.cookie('sidebar-closed') === '1' && !$('body').hasClass("sidebar-closed") && width >= 768) {
 		$('body').addClass("sidebar-closed");
+		$('#imageTitle').addClass('imageAdmin2').removeClass('imageAdmin');
 		$('#sidebar .nav-second-level, #sidebar .nav-third-level').removeClass('collapse');
 	}
-	
-	$(window).bind("load resize", function() {
+
+	$(window).bind("load resize", function () {
 		topOffset = 50;
 		var body = $('body');
 		var sidebarMenuSubs = $('#sidebar .nav-second-level, #sidebar .nav-third-level');
-		
+
 		width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-		
+
 		if (width < 768) {
 			$('div.navbar-collapse').addClass('collapse');
 			topOffset = 100; // 2-row-menu
-			
+
 			if (body.hasClass("sidebar-closed")) {
 				body.removeClass("sidebar-closed");
+				$('#imageTitle').addClass('imageAdmin').removeClass('imageAdmin2');
 				sidebarMenuSubs.addClass('collapse');
 			}
 		} else {
@@ -78,16 +80,16 @@ $(function() {
 	});
 
 	var url = window.location;
-	var element = $('ul.nav a').filter(function() {
+	var element = $('ul.nav a').filter(function () {
 		return this.href == url;
 	}).addClass('active').parent().parent().addClass('in').parent();
 	if (element.is('li')) {
 		element.addClass('active');
 	}
-	
+
 	//ページローディング画面の非表示化
 	if ($('#pageLoading').css("display") == "block") {
-		$('#pageLoading').delay(100).css("display","none");
+		$('#pageLoading').delay(100).css("display", "none");
 	}
 
 });
@@ -95,27 +97,29 @@ $(function() {
 //Top Toggler
 var handleSidebarToggler = function () {
 	var body = $('body');
-	
+
 	// handle sidebar show/hide
 	body.on('click', '.sidebar-toggler', function (e) {
-	
+
 		var sidebarMenuSubs = $('#sidebar .nav-second-level, #sidebar .nav-third-level');
-		
+
 		//collapse("toggle") した際にheightが「0」になるため、height style削除
 		$("#sidebar-area .dropdown-collapse").parent("li").children("ul").css('height', '');
-		
+
 		$(".sidebar-search", $('.page-sidebar')).removeClass("open");
 		if (body.hasClass("sidebar-closed")) {
 			body.removeClass("sidebar-closed");
+			$('#imageTitle').addClass('imageAdmin').removeClass('imageAdmin2');
 			sidebarMenuSubs.addClass('collapse');
-			
+
 			if ($.cookie) {
 				$.cookie('sidebar-closed', '0');
 			}
 		} else {
 			body.addClass("sidebar-closed");
+			$('#imageTitle').addClass('imageAdmin2').removeClass('imageAdmin');
 			sidebarMenuSubs.removeClass('collapse');
-			
+
 			if ($.cookie) {
 				$.cookie('sidebar-closed', '1');
 			}
@@ -125,7 +129,7 @@ var handleSidebarToggler = function () {
 };
 
 //IE Checker
-var isIE = function() {
+var isIE = function () {
 	var undef,
 		v = 3,
 		div = document.createElement("div"),
@@ -137,4 +141,3 @@ var isIE = function() {
 		return v > 4 ? v : undef;
 	}
 }
-
--- src/printOrderManager/static/js/index_printManageById.js
+++ src/printOrderManager/static/js/index_printManageById.js
@@ -1,8 +1,18 @@
 // This functions is called when the document is ready
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
 
 function updateText() {
-    alert("Cambie de estado");
+    //alert("Cambie de estado");
 }
+
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
