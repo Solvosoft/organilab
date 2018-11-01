2f4eae8ea666e36ae1bad1ce59f5ec33c40c06a4
luisfelipe7 <luisfelipecs97@gmail.com>
2018-11-01 09:50:13 -0600

    Create Paper Type Form Validations

--- src/printOrderManager/static/js/createPaperType_printManageById.js
+++ src/printOrderManager/static/js/createPaperType_printManageById.js
@@ -16,10 +16,7 @@ let client = new coreapi.Client({
 })
 
 var errors = new Array();
-const object = "contact";
-const tableId = "users";
-var table = undefined;
-
+const object = "paper type";
 
 
 // Values required to register a contact
@@ -29,24 +26,15 @@ var formValues = new Array();
 
 // Define the errors validation of the form
 
-var errorsValidation = $('.createContact-card form');
+var errorsValidation = $('.createPaperType-card form');
 
 // This functions is called when the document is ready
 
 
 $(document).ready(function () {
-    /*loadTable();
     defineErrors();
-    defineValues(0, 0, "", "");
+    defineValues("",0,"","",0,0,0);
     defineValidations();
-
-    $('.tooltip-demo').tooltip({
-        selector: "[data-toggle=tooltip]",
-        container: "body"
-    })
-
-    $("[data-toggle=popover]")
-        .popover()*/
 });
 
 
@@ -56,17 +44,39 @@ function defineValidations() {
     /**
      * Custom validator for phone number
      */
-    $.validator.addMethod("phoneNumberS", function (value, element) {
-        return this.optional(element) || /^\+?1?\d{9,15}$/i.test(value);
-    }, "Phone number must be entered in the format: '+55577777777'");
 
     errorsValidation.validate({
         rules: {
-            userPhoneNumber: {
-                required: false,
+            namePaperType: {
+                required: true,
+                maxlength: 255,
+            },
+            gramsPaperType: {
+                required: true,
+                maxlength: 10,
+                number: true,
+            },
+            availablePaper: {
+                required: true,
                 maxlength: 15,
-                phoneNumberS: true,
-                //regex: "/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/",
+            },
+            descriptionPaperType: {
+                required: true,
+                maxlength: 255,
+            },
+            unitSize: {
+                required: true,
+                maxlength: 25,
+            },
+            longSize: {
+                required: true,
+                maxlength: 10,
+                number: true,
+            },
+            widthSize: {
+                required: true,
+                maxlength: 10,
+                number: true,
             },
         },
         errorElement: 'span',
@@ -86,7 +96,6 @@ function defineValidations() {
             $(element).closest('.form-group, .has-feedback').removeClass('has-error').addClass('has-success');
         }
     });
-
 }
 
 
@@ -99,13 +108,14 @@ function defineErrors() {
 
 // Define the values of the form
 
-function defineValues(printId, userId, userName, userFullName) {
-    formValues["printId"] = printId;
-    formValues["userId"] = userId;
-    formValues["userName"] = userName;
-    formValues["userFullName"] = userFullName;
-    formValues["contactPhoneNumber"] = 0;
-    formValues["contactState"] = 0;
+function defineValues(name, grams, availability, description, unitSize, longSize, widthSize) {
+    formValues["name"] = name;
+    formValues["grams"] = grams;
+    formValues["availability"] = availability;
+    formValues["description"] = description;
+    formValues["unitSize"] = unitSize;
+    formValues["longSize"] = longSize;
+    formValues["widthSize"] = widthSize;
 }
 
 // Clean the inputs of the form
@@ -125,103 +135,12 @@ function cleanForm() {
     });
 }
 
-// Load the table
-
-
-function loadTable() {
-    if (table == undefined) {
-        table = $('#' + tableId).DataTable({
-            "ajax": {
-                "url": '/printOrderManager/list_usersNotRelatedToPrint',
-                "data": { //Parameters send it in a POST
-                    "pk": printObjectId,
-                }
-            }, // Data sent with the delete request
-            "destroy": true, // Allow reload table
-            responsive: true,
-            "fixedHeader": true,
-            "language": {
-                "url": "/get_dataset_translation"
-            },
-            /*"columnDefs": [{
-                "width": "58%",
-                "targets": 2
-            }]
-            "scrollY": 200,
-            "scrollX": true*/
-        });
-    } else {
-        table.ajax.reload(null, false);
-    }
-}
-
-// Set the values to the form
-
-function defineValuesForm(printId, userId, userName, userFullName) {
-    cleanForm();
-    defineValues(printId, userId, userName, userFullName);
-    if (userFullName != " ") {
-        $('#userId').text(userName + " : " + userFullName);
-    } else {
-        $('#userId').text(userName);
-    }
-    //$('#userPhoneNumber').val('FFFFFF');
-}
 
 // Save the contact of the form
 
-function saveContact() {
+function savePaperType() {
     errorsValidation.validate(); //Validate the form
-    var idSavedContact = 0;
     if (errorsValidation.valid() == true) {
-        //Reload the values
-        formValues["contactPhoneNumber"] = $('#userPhoneNumber').val();
-        formValues["contactState"] = $('#stateContact').val();
 
-        //Save the contact with Django REST Framework
-        // Interact with the API endpoint to create a contact
-        var action = ["contacts", "create"]
-        var params = {
-            phone: formValues["contactPhoneNumber"],
-            assigned_user: formValues["userId"],
-            state: formValues["contactState"],
-        }
-        client.action(schema, action, params).then(function (result) {
-            // Return value is in 'result'
-            idSavedContact = result.id;
-
-            //Read the print object with Django REST Framework
-            // Interact with the API endpoint to read the contacts of the print
-            var action = ["printObject", "read"]
-            var params = {
-                id: formValues["printId"],
-            }
-            client.action(schema, action, params).then(function (result) {
-                // Return value is in 'result'
-                var printContacts = result.contacts;
-                printContacts.push(idSavedContact);
-
-                //Partial update the print object with Django REST Framework
-                // Interact with the API endpoint to partial update the contact
-                var action = ["printObject", "partial_update"]
-                var params = {
-                    id: formValues["printId"],
-                    contacts: printContacts,
-                }
-                client.action(schema, action, params).then(function (result) {
-                    swal("Contact added!", "The " + object + " with the user  " + formValues["userName"] + " was added successfully", "success");
-                    loadTable();
-                })
-
-            }).catch(function (error) {
-                swal("Contact not added!", errors[error.message], "error");
-                // Handle error case where eg. user provides incorrect credentials.
-            })
-
-        }).catch(function (error) {
-            swal("Contact not added!", errors[error.message], "error");
-            // Handle error case where eg. user provides incorrect credentials.
-        })
-        $("#closeModal").click(); //Close Modal
     }
 }
