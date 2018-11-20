864c90599e8afa085c35b8f6994db7cff087f85d
Luis Zárate M <luis.zarate@solvosoft.com>
2018-11-19 20:54:20 -0600

    static crawl not defined

--- src/organilab/settings.py
+++ src/organilab/settings.py
@@ -199,10 +199,6 @@ USE_TZ = True
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_CRAWL = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, "static"),
-    STATIC_CRAWL
-]
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
