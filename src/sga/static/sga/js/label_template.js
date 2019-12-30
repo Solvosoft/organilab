92a777097e66cca0d4ed23ed28cc63eeceb4cce8
stwartf64 <stewartf64@gmail.com>
2019-12-30 14:58:42 -0600

    Initial undo/redo functionality added

--- src/sga/static/sga/js/label_template.js
+++ src/sga/static/sga/js/label_template.js
@@ -1,81 +1,105 @@
 let _canvases = [];
-let state;
-let undo = [];
-let redo = [];
+
+
+class CanvasHandler
+{
+    constructor(state, canv_obj)
+    {
+        this.canv_obj = canv_obj;
+        this.state = state;
+        this.undo = [];
+        this.redo = [];
+    }
+
+}
 
 function save(index){
-    redo = [];
+    console.log("Guardando en el canvas #" + index)
+    _canvases[index].redo = [];
     $('#redo').prop('disabled', true);
-    if (state){
-        undo.push(state);
+    if (_canvases[index].state){
+        _canvases[index].undo.push(_canvases[index].state);
         $('#undo').prop('disabled', false);
     }
-    state = JSON.stringify(_canvases[index]);
-    console.log(state);
+    _canvases[index].state = JSON.stringify(_canvases[index].canv_obj);
 
 }
 function replay(playStack, saveStack, buttonsOn, buttonsOff, index){
-    saveStack.push(state);
-    state = playStack.pop();
-    var on = $(buttonsOn);
-    var off = $(buttonsOff);
+    if(saveStack =='redo'){
+        _canvases[index].redo.push(_canvases[index].state);
+        _canvases[index].state = _canvases[index].undo.pop();
+    }
+    else{
+        _canvases[index].undo.push(_canvases[index].state);
+        _canvases[index].state = _canvases[index].redo.pop();
+    }
+    let on = $(buttonsOn);
+    let off = $(buttonsOff);
     on.prop('disabled', true);
     off.prop('disabled', true);
-    console.log(_canvases[index]);
-    _canvases[index].clear();
-    _canvases[index].loadFromJSON(state, function(){
-        _canvases[index].renderAll();
+    _canvases[index].canv_obj.clear();
+    _canvases[index].canv_obj.loadFromJSON(_canvases[index].state, function(){
+        _canvases[index].canv_obj.renderAll();
         on.prop('disabled', false);
-        if (playStack.length){
-            off.prop('disabled',false);
+        if(playStack == 'undo'){
+            if (_canvases[index].undo.length){
+                off.prop('disabled',false);
+            }
         }
+        else{
+            if (_canvases[index].redo.length){
+                off.prop('disabled',false);
+            }
+        }
+
     });
-    console.log(_canvases[index]);
 
 }
 (function( ) {
     let formdata = $("#sgaform").serializeArray();
     $(".templatepreview").each(function(index, element){
         $.post(element.dataset.href,formdata,function(data, status){
-            let newcanvas = canvas_editor = new fabric.Canvas(element.id);
-            _canvases.push(newcanvas);
-            save(index);
-            newcanvas.loadFromJSON(data.object, function() {
-                newcanvas.item(0).selectable = false;
-                newcanvas['panning'] = false;
-                newcanvas['onselected'] = false;
-                newcanvas.on('mouse:wheel', function (opt) {
+            let newcanvas = new fabric.Canvas(element.id);
+            let handler = new CanvasHandler(JSON.stringify(newcanvas), newcanvas);
+            _canvases.push(handler);
+            let index_temp = _canvases.length - 1;
+            _canvases[index_temp].canv_obj.loadFromJSON(data.object, function() {
+                _canvases[index_temp].canv_obj.item(0).selectable = false;
+                _canvases[index_temp].canv_obj['panning'] = false;
+                _canvases[index_temp].canv_obj['onselected'] = false;
+                _canvases[index_temp].canv_obj.on('mouse:wheel', function (opt) {
                      let delta = opt.e.deltaY;
-                     let zoom = newcanvas.getZoom();
+                     let zoom = _canvases[index_temp].canv_obj.getZoom();
                      zoom = zoom + delta / 200;
                      if (zoom > 20) zoom = 20;
                      if (zoom < 0.01) zoom = 0.01;
-                     newcanvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+                     _canvases[index_temp].canv_obj.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
                      opt.e.preventDefault();
                      opt.e.stopPropagation();
                  });
-                newcanvas.on('mouse:up', function () {
-                     newcanvas['panning'] = false;
+                _canvases[index_temp].canv_obj.on('mouse:up', function () {
+                     _canvases[index_temp].canv_obj['panning'] = false;
                  });
-                newcanvas.on('mouse:dblclick', function () {
-                     if (!newcanvas['onselected']) {
-                         newcanvas['panning'] = true;
+                _canvases[index_temp].canv_obj.on('mouse:dblclick', function () {
+                     if (!_canvases[index_temp].canv_obj['onselected']) {
+                         _canvases[index_temp].canv_obj['panning'] = true;
                      }
                  });
-                newcanvas.on('mouse:move', function (e) {
-                     if (newcanvas['panning'] && e && e.e && !newcanvas['onselected']) {
+                _canvases[index_temp].canv_obj.on('mouse:move', function (e) {
+                     if (_canvases[index_temp].canv_obj['panning'] && e && e.e && !_canvases[index_temp].canv_obj['onselected']) {
                              var delta = new fabric.Point(e.e.movementX, e.e.movementY);
-                             newcanvas.relativePan(delta);
+                             _canvases[index_temp].canv_obj.relativePan(delta);
                      }
                  });
-                newcanvas.on('object:selected', function () {
-                     newcanvas['onselected'] = true;
+                _canvases[index_temp].canv_obj.on('object:selected', function () {
+                     _canvases[index_temp].canv_obj['onselected'] = true;
                  });
-                newcanvas.on('before:selection:cleared', function () {
-                     newcanvas['onselected'] = false;
+                _canvases[index_temp].canv_obj.on('before:selection:cleared', function () {
+                     _canvases[index_temp].canv_obj['onselected'] = false;
                  });
-                newcanvas.on('object:modified', function () {
-                     save(index);
+                _canvases[index_temp].canv_obj.on('object:modified', function () {
+                    console.log("Modificando el canvas #"+ index_temp);
+                     save(index_temp);
                  });
 
                 let canvas_container_preview = $(".canvas-container-preview");
@@ -88,9 +112,9 @@ function replay(playStack, saveStack, buttonsOn, buttonsOff, index){
                 width = 400;
                 }
          /*      newcanvas.setZoom(parseFloat(data.preview));*/
-                newcanvas.setWidth(width);
-                newcanvas.setHeight(height);
-                newcanvas.renderAll();
+                _canvases[index_temp].canv_obj.setWidth(width);
+                _canvases[index_temp].canv_obj.setHeight(height);
+                _canvases[index_temp].canv_obj.renderAll();
                 save(index);
             });
           });
@@ -98,11 +122,11 @@ function replay(playStack, saveStack, buttonsOn, buttonsOff, index){
 })();
 
 function undoFunction(index){
-    replay(undo,redo,'#redo','#undo', index - 1);
+    replay('undo','redo','#redo','#undo', index - 1);
 }
 
 function redoFunction(index){
-    replay(redo,undo,'#undo','#redo', index - 1);
+    replay('redo','undo','#undo','#redo', index - 1);
 }
 
 $(document).ready(function(){
@@ -114,9 +138,9 @@ $(document).ready(function(){
 
 function get_canvas(pk){
     for(let canvas of _canvases){
-        let id = canvas.lowerCanvasEl.id;
+        let id = canvas.canv_obj.lowerCanvasEl.id;
         if (id === "preview_" + pk.toString())
-            return canvas;
+            return canvas.canv_obj;
      }
 }
 
