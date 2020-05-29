56147a7d1962b77758ac30315434825488f70c51
stwartf64 <stewartf64@gmail.com>
2020-05-29 14:36:37 -0600

    Autocomplete working and bugfixing

--- src/laboratory/sustance/forms.py
+++ src/laboratory/sustance/forms.py
@@ -1,22 +1,25 @@
 
 from django import forms
 
-from laboratory.models import Object, SustanceCharacteristics
+from laboratory.models import Object, SustanceCharacteristics, Laboratory, ObjectFeatures
 
 
 class SustanceObjectForm(forms.ModelForm):
-
+    features = forms.CharField()
+    laboratory = forms.CharField()
     class Meta:
         model = Object
         fields = [
             'name', 'synonym',
             'code', 'is_public',
-            'description', 'features',
-            'model', 'serie', 'plaque',
-            'laboratory'
+            'description',
+            'model', 'serie', 'plaque'
         ]
+        exclude = ['laboratory', 'features']
 
 class SustanceCharacteristicsForm(forms.ModelForm):
+    white_organ = forms.CharField()
+    h_code = forms.CharField()
     class Meta:
         model = SustanceCharacteristics
-        exclude = ['obj']
+        exclude = ['obj', 'white_organ', 'h_code']
--- src/laboratory/sustance/views.py
+++ src/laboratory/sustance/views.py
@@ -1,15 +1,18 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
+import json
 from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DeleteView
 from django_datatables_view.base_datatable_view import BaseDatatableView
 
-from laboratory.models import Object
+from laboratory.models import Object, ObjectFeatures, Laboratory, Catalog
 from laboratory.sustance.forms import SustanceObjectForm, SustanceCharacteristicsForm
 from django.utils.translation import ugettext_lazy as _
 
+from sga.models import DangerIndication
 
 
 def create_edit_sustance(request, pk=None):
@@ -25,7 +28,6 @@ def create_edit_sustance(request, pk=None):
         filesdata = request.FILES
     objform = SustanceObjectForm(postdata, instance=instance)
     suschacform = SustanceCharacteristicsForm(postdata, files=filesdata, instance=suscharobj)
-
     if request.method == 'POST':
         if objform.is_valid() and suschacform.is_valid():
             obj = objform.save(commit=False)
@@ -36,6 +38,9 @@ def create_edit_sustance(request, pk=None):
             suscharinst.save()
             messages.success(request, _("Sustance saved successfully"))
             return redirect(reverse('laboratory:sustance_list'))
+        else:
+            messages.warning(request, _("Pending information in form"))
+
 
     return render(request, 'laboratory/sustance/sustance_form.html', {
         'objform': objform,
@@ -103,3 +108,86 @@ class SustanceListJson(BaseDatatableView, UserPassesTestMixin):
                 delete
             ])
         return json_data
+
+
+"""
+    Ajax views for autocomplete fields
+"""
+
+
+def search_autocomplete_sustance_features(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '')
+        # Contains the typed characters, is valid since the first character
+        # Search Parameter: Object Feature Name
+        if any(c.isalpha() for c in q):
+            search_qs = ObjectFeatures.objects.filter(name__icontains=q)
+        results = []
+        for r in search_qs:
+            results.append({'label': r.name, 'value': r.pk})
+        if not results:
+            results.append('No results')
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
+
+
+def search_autocomplete_sustance_laboratory(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '')
+        # Contains the typed characters, is valid since the first character
+        # Search Parameter: Laboratory name and user in laboratorists list
+        if any(c.isalpha() for c in q):
+            search_qs = Laboratory.objects.filter(name__icontains=q, laboratorists__in=[request.user])
+        results = []
+        for r in search_qs:
+            results.append({'label': r.name, 'value': r.pk})
+        if not results:
+            results.append('No results')
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
+
+
+def search_autocomplete_sustance_white_organ(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '')
+        # Contains the typed characters, is valid since the first character
+        # Search Parameter: White Organ Description
+        if any(c.isalpha() for c in q):
+            search_qs = Catalog.objects.filter(key="white_organ", description__icontains=q)
+        results = []
+        for r in search_qs:
+            results.append({'label': r.description, 'value': r.pk})
+        if not results:
+            results.append('No results')
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
+
+
+def search_autocomplete_sustance_danger_indication(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '')
+        # Contains the typed characters, is valid since the first character
+        # Search Parameter: Danger Indication Code
+        if any(c.isalpha() for c in q):
+            search_qs = DangerIndication.objects.filter(code__icontains=q)
+        else:
+            search_qs = DangerIndication.objects.filter(code__icontains=q)
+        results = []
+        for r in search_qs:
+            results.append({'label': r.code + ": " + r.description, 'value': r.pk})
+        if not results:
+            results.append('No results')
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
