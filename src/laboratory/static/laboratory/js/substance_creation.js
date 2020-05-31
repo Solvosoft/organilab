c195ce069f05852ec7c6e6a02ab086b80c38c214
Luis Zárate M <luis.zarate@solvosoft.com>
2020-05-31 17:26:35 -0600

    repair pull requests

--- src/laboratory/static/laboratory/js/substance_creation.js
+++ src/laboratory/static/laboratory/js/substance_creation.js
@@ -4,57 +4,20 @@ $(document).ready(function () {
     $('select[name="laboratory"]').select2();
     $('select[name="h_code"]').select2();
     $('select[name="white_organ"]').select2();
+    $('select[name="iarc"]').select2();
+    $('select[name="imdg"]').select2();
 
-    $('#id_features').attr("aria-describedby", "featuresHelp");
-    $('#id_laboratory').attr("aria-describedby", "laboratoryHelp")
-    $('#id_white_organ').attr("aria-describedby", "white_organHelp");
-    $('#id_h_code').attr("aria-describedby", "h_codeHelp")
-    $("#id_features").autocomplete({
-        source: "search_autocomplete_sustance_features",
-        //Start predicting at character #1
-        minLength: 1,
-        //Show important substance information and save id
-        select: function (event, ui) {
-            $('#features_id').val(ui.item.value); // save selected id to hidden input
-            $('#features_id').data("name", ui.item.label); // save the selected text to hidden input
-            $('#id_features').val(ui.item.label); // display the selected text
-            return false;
-        }
-    });
-    $("#id_laboratory").autocomplete({
-        source: "search_autocomplete_sustance_laboratory",
-        //Start predicting at character #1
-        minLength: 1,
-        //Show important substance information and save id
-        select: function (event, ui) {
-            $('#laboratory_id').val(ui.item.value); // save selected id to hidden input
-            $('#laboratory_id').data("name", ui.item.label); // save the selected text to hidden input
-            $('#id_laboratory').val(ui.item.label); // display the selected text
-            return false;
-        }
-    });
-    $("#id_white_organ").autocomplete({
-        source: "search_autocomplete_sustance_white_organ",
-        //Start predicting at character #1
-        minLength: 1,
-        //Show important substance information and save id
-        select: function (event, ui) {
-            $('#white_organ_id').val(ui.item.value); // save selected id to hidden input
-            $('#white_organ_id').data("name", ui.item.label); // save the selected text to hidden input
-            $('#id_white_organ').val(ui.item.label); // display the selected text
-            return false;
-        }
-    });
-    $("#id_h_code").autocomplete({
-        source: "search_autocomplete_sustance_danger_indication",
-        //Start predicting at character #1
-        minLength: 1,
-        //Show important substance information and save id
-        select: function (event, ui) {
-            $('#h_code_id').val(ui.item.value); // save selected id to hidden input
-            $('#h_code_id').data("name", ui.item.label); // save the selected text to hidden input
-            $('#id_h_code').val(ui.item.label); // display the selected text
-            return false;
-        }
-    });
+    function hide_show_precursor_type(){
+         var checkBox=document.getElementById("id_is_precursor");
+         if (checkBox.checked == true){
+            $("#id_precursor_type").closest('.form-group').show()
+         }else{
+            $("#id_precursor_type").closest('.form-group').hide()
+         }
+    }
+
+    $("input[name='is_precursor']").change(hide_show_precursor_type);
+    hide_show_precursor_type();
+
+
 });
