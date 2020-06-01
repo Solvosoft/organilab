3b670036ec14656fe3467a7756025e2660ac4aa0
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-05-31 21:45:34 -0600

    Add test post create furniture view, update docstring

--- src/laboratory/tests/test_furniture_views.py
+++ src/laboratory/tests/test_furniture_views.py
@@ -17,22 +17,36 @@ class FurnitureViewTestCase(TestCase):
         """tests that users without permissions can't access furniture report view"""
         lab = Laboratory.objects.filter(name="Laboratory 5").first()
         user = User.objects.filter(username="est_1").first()
-        kwargs = {"lab_pk": lab.id}
+        kwargs = {"lab_pk": lab.id }
         request = RequestFactory().get(reverse("laboratory:reports_furniture_detail", kwargs=kwargs))
         request.user = user 
         response = FurnitureReportView.as_view()(request, **kwargs)
         self.assertEqual(response.status_code, 403)
 
-    def test_furniture_add_view_permissions(self):
-        """tests that users withou permissions can't create a furniture"""
+    def test_furniture_create_view_permissions(self):
+        """tests that users without permissions can't create a furniture"""
         lab = Laboratory.objects.filter(name="Laboratory 5").first()
         user = User.objects.filter(username="est_1").first()
         room = LaboratoryRoom.objects.create(name="test_room")
         lab.rooms.add(room)
-        kwargs = {"lab_pk": lab.id, "labroom": room.id }
+        kwargs = { "lab_pk": lab.id, "labroom": room.id }
         request = RequestFactory().get(reverse("laboratory:furniture_create", kwargs=kwargs))
         request.user = user 
         response = FurnitureCreateView.as_view()(request, **kwargs)
-        self.assertEqual(response.status_code, 403)   
+        self.assertEqual(response.status_code, 403)
+    
+    def test_furniture_create_view_post_permissions(self):
+        """tests that submitting a form without permissions won't create a furniture"""
+        lab = Laboratory.objects.filter(name="Laboratory 5").first()
+        user = User.objects.filter(username="est_1").first()
+        room = LaboratoryRoom.objects.create(name="test_room")
+        lab.rooms.add(room)
+        kwargs = { "lab_pk": lab.id, "labroom": room.id, "name": "test_furniture", "type": "F" }
+        request = RequestFactory().post(reverse("laboratory:furniture_create", kwargs=kwargs))
+        request.user = user 
+        response = FurnitureCreateView.as_view()(request, **kwargs)
+        self.assertEqual(response.status_code, 403)
+    
+    
 
     
