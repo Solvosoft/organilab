4da69d93f52e5f88496f88c765ee903f7c2d9718
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2020-06-01 13:16:10 -0600

    test_admin_has_authorization_to_see_lab_room_building_report

--- src/laboratory/test/test_report_view.py
+++ src/laboratory/test/test_report_view.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
 from laboratory.test.utils import OrganizationalStructureDataMixin
-from laboratory.views.reports import report_organization_building
+from laboratory.views.reports import report_organization_building, report_labroom_building, report_shelf_objects
 
 
 class ReportViewTestCase(OrganizationalStructureDataMixin, TestCase):
-    # laboratory.do_report
-    #
+    """
+        test for report view
+    """
 
     # laboratory.do_report
     # /lab/55/organizations/reports/list >> listview
@@ -14,26 +15,52 @@ class ReportViewTestCase(OrganizationalStructureDataMixin, TestCase):
     def setUp(self):
         super(ReportViewTestCase, self).setUp()
 
-    def test_admin_has_autorization_to_see_organization_report(self):
-        """
-            'laboratory.do_report' permission is required to reach this resource
-        """
+    def check_simple_view_if_admin_has_permissions(self, function=None, resource=None, permission=None, pk=None):
 
-        pk = self.root_organization.pk
-        request = self.factory.get(f"/lab/{pk}/organizations/reports/organization")
+        request = self.factory.get(resource)
         request.user = self.user_admin
-        self.assertTrue(self.user_admin.has_perm('laboratory.do_report'))
-        response = report_organization_building(request, lab_pk=pk)
+        self.assertTrue(self.user_admin.has_perm(permission))
+        response = function(request, lab_pk=pk)
         self.assertEqual(response.status_code, 200)
 
         # laboratorist must not have this permission
         request.user = self.user_laboratoris
-        self.assertFalse(self.user_laboratoris.has_perm('laboratory.do_report'))
-        response = report_organization_building(request, lab_pk=pk)
+        self.assertFalse(self.user_laboratoris.has_perm(permission))
+        response = function(request, lab_pk=pk)
         self.assertNotEqual(response.status_code, 200)
 
         # student neither
         request.user = self.user_student
-        self.assertFalse(self.user_student.has_perm('laboratory.do_report'))
-        response = report_organization_building(request, lab_pk=pk)
+        self.assertFalse(self.user_student.has_perm(permission))
+        response = function(request, lab_pk=pk)
         self.assertNotEqual(response.status_code, 200)
+
+    def test_admin_has_authorization_to_see_organization_report(self):
+        """
+            'laboratory.do_report' permission is required to reach this resource
+        """
+        pk = self.root_organization.pk
+        path = f"/lab/{pk}/organizations/reports/organization"
+
+        self.check_simple_view_if_admin_has_permissions(
+            function=report_organization_building,
+            resource=path,
+            permission='laboratory.do_report',
+            pk=pk
+        )
+
+    def test_admin_has_authorization_to_see_lab_room_building_report(self):
+        """
+            'laboratory.do_report' permission is required to reach this resource
+        """
+        pk = self.root_organization.pk
+        path = f"lab/{pk}/reports/laboratory"
+
+        self.check_simple_view_if_admin_has_permissions(
+            function=report_labroom_building,
+            resource=path,
+            permission='laboratory.do_report',
+            pk=pk
+        )
+
+
