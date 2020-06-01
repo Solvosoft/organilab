c826c9e6a0c7da0c220095060c9e541a0aefc3f5
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2020-06-01 13:17:05 -0600

    test_admin_has_authorization_to_see_shelf_objects_report

--- src/laboratory/test/test_report_view.py
+++ src/laboratory/test/test_report_view.py
@@ -35,7 +35,8 @@ class ReportViewTestCase(OrganizationalStructureDataMixin, TestCase):
         response = function(request, lab_pk=pk)
         self.assertNotEqual(response.status_code, 200)
 
-    def test_admin_has_authorization_to_see_organization_report(self):
+
+    def test_admin_has_autorization_to_see_organization_report(self):
         """
             'laboratory.do_report' permission is required to reach this resource
         """
@@ -58,6 +59,20 @@ class ReportViewTestCase(OrganizationalStructureDataMixin, TestCase):
 
         self.check_simple_view_if_admin_has_permissions(
             function=report_labroom_building,
+            resource=path,
+            permission='laboratory.do_report',
+            pk=pk
+        )
+
+    def test_admin_has_authorization_to_see_shelf_objects_report(self):
+        """
+            'laboratory.do_report' permission is required to reach this resource
+        """
+        pk = self.root_organization.pk
+        path = f"lab/{pk}/reports/shelf_objects$"
+
+        self.check_simple_view_if_admin_has_permissions(
+            function=report_shelf_objects,
             resource=path,
             permission='laboratory.do_report',
             pk=pk
