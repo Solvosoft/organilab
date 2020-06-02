73a71af5aaf4bcb2b78725c4bb054f706add93cd
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2020-06-01 22:25:19 -0600

    first test case fixed and also the restricted cases were added

--- src/laboratory/test/test_organization_report_view.py
+++ src/laboratory/test/test_organization_report_view.py
@@ -17,31 +17,40 @@ class OrganizationReportViewTestCase(OrganizationalStructureDataMixin,TestCase):
         super(OrganizationReportViewTestCase, self).setUp()
         self.factory = RequestFactory()
 
-    def test_user_admin_can_see_organization_report(self):
+    def test_group_admin_can_see_organization_report(self):
         """
             'laboratory.view_report' permission is required to see this report
-            The laboratory administrator's group (GROUP_ADMIN_PK) has this permission.
+            The laboratory administrator's group (GROUP_ADMIN) has this permission.
         """
-        # base case: a principal technician in charge of laboratory 1
+        # case A: principal technician in charge of laboratory 1
+        lab_pk = self.lab6.id
+        request = self.factory.get(f"/lab/{lab_pk}/organizations/reports/list")
+        request.user = self.uschi1
+        self.assertTrue(self.uroot.has_perm('laboratory.view_report'))
+        response = OrganizationReportView.as_view()(request, lab_pk=lab_pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(PrincipalTechnician.objects.get(
+            credentials__id=self.uschi1.pk, assigned__id=lab_pk))  # one lab can be manage for many principals
+
+
+    def test_redirect_unathorization_groups(self):
+        """
+            'laboratory.view_report' permission belongs to GROUP_ADMIN.
+            For groups Laboratorist and student the resource must be restricted.
+        """
+        # laboratorist must not have this permission
         pk = self.lab1.id
         request = self.factory.get(f"/lab/{pk}/organizations/reports/list")
-        request.user = self.uroot
-        self.assertTrue(self.uroot.has_perm('laboratory.view_report'))
+        request.user = self.user_lab_1
+        self.assertFalse(self.user_lab_1.has_perm('laboratory.view_report'))
         response = OrganizationReportView.as_view()(request, lab_pk=pk)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(pk, PrincipalTechnician.objects.get(pk=self.uroot.pk).assigned.pk) #principal manage many labs
-
-        # laboratorist must not have this permission
-        #request.user = self.user_laboratoris
-        #self.assertFalse(self.user_laboratoris.has_perm('laboratory.view_report'))
-        #response = OrganizationReportView.as_view()(request, lab_pk=pk)
-        #self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response.url, reverse("permission_denied"))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("permission_denied"))
 
         # student neither
-        #request.user = self.user_student
-        #self.assertFalse(self.user_student.has_perm('laboratory.view_report'))
-        #response = OrganizationReportView.as_view()(request, lab_pk=pk)
-        #self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response.url, reverse("permission_denied"))
+        request.user = self.user_est_1
+        self.assertFalse(self.user_est_1.has_perm('laboratory.view_report'))
+        response = OrganizationReportView.as_view()(request, lab_pk=pk)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("permission_denied"))
 
