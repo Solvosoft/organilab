6bc6349c125a02067ae6d35d9e9ee168fe654b56
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-06-02 00:48:20 -0600

    Add get update view test with admin user

--- src/laboratory/tests/test_furniture_views.py
+++ src/laboratory/tests/test_furniture_views.py
@@ -97,3 +97,11 @@ class FurnitureViewTestCase(TestCase):
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
     
+    def test_furniture_update_view_get_admin(self):
+        """tests that admin user can get to the create furniture view"""
+        kwargs = { "lab_pk": self.lab.id, "pk": self.furniture.id }
+        url = reverse("laboratory:furniture_update", kwargs=kwargs)
+        self.client.force_login(self.admin)  
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
