a2bb8e4239f5a1e5554ac5b8c299b8918c4fa923
Julio Ordonez <julio.ordonez.v@gmail.com>
2020-06-01 21:08:41 -0600

    Refactor test code

--- src/laboratory/tests/test_furniture_views.py
+++ src/laboratory/tests/test_furniture_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.urls import reverse
+from django.utils.http import urlencode
 from django.test import TestCase, RequestFactory
 from demoQA.management import commands
 from .utils import TestUtil
@@ -51,10 +52,10 @@ class FurnitureViewTestCase(TestCase):
         room = LaboratoryRoom.objects.create(name="test_room")
         lab.rooms.add(room)
         kwargs = { "lab_pk": lab.id, "labroom": room.id }
-        request = RequestFactory().post(reverse("laboratory:furniture_create", kwargs=kwargs))
-        kwargs.update({"name": "test_furniture", "type": "F"})
-        request.user = user 
-        response = FurnitureCreateView.as_view()(request, **kwargs)
+        url = reverse("laboratory:furniture_create", kwargs=kwargs)
+        data = urlencode({"name": "test_furniture", "type": "F"})
+        self.client.force_login(user) 
+        response = self.client.post(url, data, content_type="application/x-www-form-urlencoded", follow=True)
         self.assertEqual(response.status_code, 403)
     
     def test_furniture_update_view_get_permissions(self):
@@ -80,8 +81,9 @@ class FurnitureViewTestCase(TestCase):
         catalog = Catalog.objects.filter(key="furniture_type").first()
         furniture = Furniture.objects.create(labroom=room, name="test_furniture", type=catalog)
         kwargs = { "lab_pk": lab.id, "pk": furniture.id }
-        request = RequestFactory().post(reverse("laboratory:furniture_update", kwargs=kwargs))
-        request.user = user 
-        response = FurnitureUpdateView.as_view()(request, **kwargs)
-        self.assertEqual(response.status_code, 403)
-    
+        url = reverse("laboratory:furniture_update", kwargs=kwargs)
+        self.client.force_login(user)
+        data = urlencode({"name": "modified"})
+        response = self.client.post(url, data,  content_type="application/x-www-form-urlencoded", follow=True)
+        self.assertEqual(response.status_code, 200)
+
