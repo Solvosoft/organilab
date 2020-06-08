a405c0448c459d5a186a931054edfc36eff3d952
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2020-06-08 08:13:39 -0600

    test_permissions

--- src/laboratory/test/test_shelf_object_view.py
+++ src/laboratory/test/test_shelf_object_view.py
@@ -1,10 +1,37 @@
-from unittest.case import TestCase
+from django.test import TestCase
 
-from laboratory.test.utils import OrganizationalStructureDataMixin
+from laboratory.models import Object
+from laboratory.test.utils import OrganizationalStructureDataMixin as OrganizationalStructureData
 
 
 class ShelfObjectViewTestCases(TestCase):
 
     def setUp(self):
-        infrastructure = OrganizationalStructureDataMixin()
-        infrastructure.setUp()
+        self.infrastructure = OrganizationalStructureData()
+        self.infrastructure.setUp()
+
+        self.student_user = self.infrastructure.usch4
+        self.laboratorist_user = self.infrastructure.usch6_i1
+        self.professor_user = self.infrastructure.uschi1
+        self.root_user = self.infrastructure.uroot
+
+    def test_permissions(self):
+        """
+            The lab's group is the only one that has this three permissions:
+            Laboratory: add_shelfobject, change_shelfobject, delete_shelfobject
+        """
+        # student
+        self.assertTrue(self.student_user.has_perm("laboratory.add_shelfobject"))
+        self.assertTrue(self.student_user.has_perm("laboratory.change_shelfobject"))
+        self.assertTrue(self.student_user.has_perm("laboratory.delete_shelfobject"))
+
+        # laboratoris
+        self.assertTrue(self.laboratorist_user.has_perm("laboratory.add_shelfobject"))
+        self.assertTrue(self.laboratorist_user.has_perm("laboratory.change_shelfobject"))
+        self.assertTrue(self.laboratorist_user.has_perm("laboratory.delete_shelfobject"))
+
+        # professor
+        self.assertTrue(self.professor_user.has_perm("laboratory.add_shelfobject"))
+        self.assertTrue(self.professor_user.has_perm("laboratory.change_shelfobject"))
+        self.assertTrue(self.professor_user.has_perm("laboratory.delete_shelfobject"))
+
