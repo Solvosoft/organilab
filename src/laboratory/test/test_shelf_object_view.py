019d6fad4119e86fe0f79494c5a69aa2415318e5
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2020-06-08 08:29:39 -0600

    lab's group can access shelf-object creationg form

--- src/laboratory/test/test_shelf_object_view.py
+++ src/laboratory/test/test_shelf_object_view.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-from laboratory.models import Object
+from laboratory.models import LaboratoryRoom, Furniture, Catalog
 from laboratory.test.utils import OrganizationalStructureDataMixin as OrganizationalStructureData
 
 
@@ -9,6 +10,17 @@ class ShelfObjectViewTestCases(TestCase):
     def setUp(self):
         self.infrastructure = OrganizationalStructureData()
         self.infrastructure.setUp()
+
+        self.room_lab6 = LaboratoryRoom.objects.create(name="Laboratorio alfa")
+
+        self.infrastructure.lab6.rooms.add(self.room_lab6)
+        self.infrastructure.lab6.save()
+
+        self.furniture_lab6 = Furniture.objects.create(
+            labroom=self.room_lab6,
+            name="Mueble X",
+            type=Catalog.objects.filter(key="furniture_type", description="Estante").first()
+        )
 
         self.student_user = self.infrastructure.usch4
         self.laboratorist_user = self.infrastructure.usch6_i1
@@ -35,3 +47,13 @@ class ShelfObjectViewTestCases(TestCase):
         self.assertTrue(self.professor_user.has_perm("laboratory.change_shelfobject"))
         self.assertTrue(self.professor_user.has_perm("laboratory.delete_shelfobject"))
 
+    def test_get_shelf_object_form_lab_group_allowed(self):
+        """
+            laboratory's group must be the only one
+            who can access this creation form
+        """
+        url = reverse("laboratory:shelfobject_create", kwargs={"lab_pk": self.infrastructure.lab6.pk})
+        self.client.force_login(self.laboratorist_user)
+        response = self.client.get(url, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "form")
