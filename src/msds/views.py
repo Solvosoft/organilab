68d4a1a71987860fd4409697bb966093b0a6a7b6
Luis Zárate M <luis.zarate@solvosoft.com>
2020-06-10 20:07:37 -0600

    fixed msds errors

--- src/msds/views.py
+++ src/msds/views.py
@@ -1,10 +1,14 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.http.response import JsonResponse
+
+from djgentelella.cruds.base import CRUDView
 from msds.models import MSDSObject, OrganilabNode, RegulationDocument
 from django.db.models.query_utils import Q
 from django.core.paginator import Paginator
 from django.utils.translation import ugettext as _
-#from msds.forms import FormMSDSobject, FormMSDSobjectUpdate
+from msds.forms import FormMSDSobject, FormMSDSobjectUpdate
 from django.urls.base import reverse
 from django.shortcuts import render
 import zipfile
@@ -80,7 +84,7 @@ def get_list_msds(request):
         pass
     return JsonResponse(dev)
 
-"""
+
 class MSDSObjectCRUD(CRUDView):
     model = MSDSObject
     views_available = ['create', 'update', 'detail', 'delete']
@@ -138,7 +142,7 @@ class MSDSObjectCRUD(CRUDView):
                 return query
         return ODeleteView
 
-"""
+
 def organilab_tree_frame(request):  
     context = {}
     context['sections'] = OrganilabNode.objects.all()
