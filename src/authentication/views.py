abdb6489a690e9971016d9c596c8a70a3644d7ae
Luis Zárate M <luis.zarate@solvosoft.com>
2020-06-22 20:09:40 -0600

    new Login and request demo

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,28 +1,15 @@
-from django.shortcuts import render, redirect
-from django.utils.translation import ugettext_lazy as _
-
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
-from django.contrib.auth.models import User, Group
-from django import forms
-from django.core.exceptions import ValidationError
-from laboratory.models import OrganizationStructure, Profile
-from constance import config
-from snowpenguin.django.recaptcha2.fields import ReCaptchaField
-from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
-from django.contrib.auth import authenticate, login
-from django.urls.base import reverse_lazy, reverse
-from django.views.decorators.http import require_POST
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
-from django.views.generic.base import TemplateView
-from django.contrib import messages
-from django.views.generic.edit import CreateView
-from authentication.models import FeedbackEntry
 from async_notifications.utils import send_email_from_template
 from django.conf import settings
-from djgentelella.forms.forms import CustomForm
-from djgentelella.widgets import core as genwidgets
+from django.contrib import messages
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.urls.base import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+
+from authentication.forms import DemoRequestForm
+from authentication.models import FeedbackEntry, DemoRequest
 
 
 class PermissionDeniedView(TemplateView):
@@ -62,86 +49,40 @@ class FeedbackView(CreateView):
 
         return dev
 
+def request_demo_done(request):
+    return render(request, 'registration/request_demo_done.html')
 
-def get_organization_admin_group():
-    return Group.objects.get(pk=config.GROUP_ADMIN_PK)
+from async_notifications.register import update_template_context
+context = [
+    ('data.name', 'name'),
+    ('data.business_email', 'Business email'),
+    ('data.company_name', 'Company name'),
+    ('data.country', 'Country'),
+    ('data.phone_number', 'Phone number'),
+]
+update_template_context("Request demo",  "New demo request", context,
+                        message="""User information:<br>
+                        {{data.name}}<br>
+                        {{data.business_email}}<br>
+                        {{data.company_name}}<br>
+                        {{data.country}}<br>
+                        {{data.phone_number}}
+                        """)
+class RequestDemoView(CreateView):
+    model = DemoRequest
+    form_class = DemoRequestForm
+    template_name = 'registration/request_a_demo.html'
+    success_url = reverse_lazy('index')
 
-
-class SignUpForm(CustomForm, UserCreationForm):
-    ROLES = (
-        (1, _('Organization administrator')),
-        (2, _('Student'))
-    )
-    role = forms.ChoiceField(choices=ROLES, widget=genwidgets.RadioSelect())
-    organization_name = forms.CharField(
-        max_length=120, required=False, help_text=_('Your organization name'))
-
-    captcha = ReCaptchaField(widget=ReCaptchaWidget())
-
-
-    def clean(self):
-        dev = super(SignUpForm, self).clean()
-        if self.cleaned_data['role'] == '1':
-            if not self.cleaned_data['organization_name']:
-                raise ValidationError(
-                    _('Organization name is required when role Organization administrator is selected'))
-
-        return dev
-
-    def save(self, commit=True):
-        instance = UserCreationForm.save(self, commit=commit)
-        group = get_organization_admin_group()
-        org = OrganizationStructure(
-            name=self.cleaned_data['organization_name'],
-            group=group
-        )
-        org.save()
-        pt = Profile(user=instance,
-                     phone_number="8888-8888",
-                     id_card="0-0000-0000",
-                     organization=org
-                     )
-        pt.save()
-
-        instance.groups.add(group)
-        return instance
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name',
-                  'email', 'password1', 'password2', )
-
-
-@require_POST
-def signup(request):
-    form_login = AuthenticationForm()
-    form = SignUpForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        raw_password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=raw_password)
-        login(request, user)
-        send_email_from_template("new user", user.email,
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        send_email_from_template("Request demo",
+                                 settings.DEFAULT_FROM_EMAIL,
                                  context={
-                                     'user': user,
-                                     'role': form.cleaned_data['role'],
-                                     'organization': form.cleaned_data['organization_name']
+                                     'data': form.cleaned_data
                                  },
                                  enqueued=True,
-                                 user=user,
+                                 user=None,
                                  upfile=None)
-        return redirect(reverse_lazy('index'))
-    # else:
-    #    form = SignUpForm()
-    return render(request, 'registration/login.html',
-                  {'form_signup': form,
-                   'form': form_login})
-
-
-class OrgLoginView(LoginView):
-
-    def get_context_data(self, **kwargs):
-        self.context = LoginView.get_context_data(self, **kwargs)
-        self.context['form_signup'] = SignUpForm()
-        return self.context
+        messages.success(self.request, _('We will contact you soon'))
+        return response
