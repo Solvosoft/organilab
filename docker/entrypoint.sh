224895a2eba20e922f6c347ba0595b04901a2ed2
Luis Zárate M <luis.zarate@solvosoft.com>
2020-06-24 13:11:18 -0600

    fixed some configurations

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-chwon -R organilab:organilab /run /organilab
-runuser -u organilab python manage.py migrate --settings=organilab.settings
+runuser -u organilab -c "mkdir -p /run/logs/"
+chown -R organilab:organilab /run /organilab
+runuser -u organilab -c "python manage.py migrate"
 
-if [ -z $DEVELOPMENT ]; then
-  runuser -u organilab  celery worker -A organilab -l info -b &
-  runuser -u organilab python manage.py runserver --settings=organilab.settings 0.0.0.0:80
-else
-  if [ -z $HOSTNAME ]; then
-    sed -i 's/server_name organilab.org www.organilab.org;/server_name $HOSTNAME www.$HOSTNAME;/g'  /etc/nginx/sites-available/default
+if [ -z "$DEVELOPMENT" ]; then
+  if [ ! -z "$FVAHOSTNAME" ]; then
+    sed -i 's/server_name organilab.org www.organilab.org;/server_name $FVAHOSTNAME www.$FVAHOSTNAME;/g'  /etc/nginx/sites-available/default
   fi
   supervisord -n
+else
+  runuser -u organilab  -c "celery worker -A organilab -l info -b" &
+  runuser -u organilab -c "python manage.py runserver 0.0.0.0:80"
 fi
 
