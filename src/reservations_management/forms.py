0b0e5fb2dcfccee9ad70e56148832f4d0b942daf
roberto <robertovargasrojas97@gmail.com>
2020-10-05 17:27:43 -0600

    Template for reservarions list was created, has pagination and a dropdown to select a reservation staus and show the reservations with the selected option

---
+++ src/reservations_management/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from djgentelella.forms.forms import GTForm
+from djgentelella.widgets import core as genwidgets
+from .models import Reservations
+
+
+class ReservationsForm(GTForm, ModelForm):
+    class Meta:
+        model=Reservations
+        fields='__all__'
+        widgets={
+            'user': genwidgets.Select(),
+            'status': genwidgets.Select(),
+            'comments': genwidgets.Textarea(),
+            'is_massive': genwidgets.CheckboxInput()
+        }
