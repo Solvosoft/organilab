0a1f9d1a4ffed31a30060f6041dc77ce380c7e2f
roberto <robertovargasrojas97@gmail.com>
2020-10-05 17:24:34 -0600

    R-2) View and url for reservations list was created (just an logged administrative user can access)

--- src/reservations_management/views.py
+++ src/reservations_management/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from laboratory.decorators import user_group_perms
+
+from .models import Reservations
+from .forms import ReservationsForm
 
 # Create your views here.
+
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(user_group_perms(perm='laboratory.add_objectfeatures'), name='dispatch')
+class ReservationsListView(ListView):
+    model = Reservations
+    paginate_by = 10  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ReservationsForm()
+        return context
