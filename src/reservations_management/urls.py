232901c5c43f5cfc935076c31c150fa000b7a18b
roberto <robertovargasrojas97@gmail.com>
2020-10-06 16:04:40 -0600

    R-5) View created and url registered for manage reservation page

--- src/reservations_management/urls.py
+++ src/reservations_management/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from .views import ReservationsListView
+from .views import (
+    ReservationsListView,
+    ManageReservationView
+    )
 
 urlpatterns = [
-    url(r'reservations/', ReservationsListView.as_view(), name='reservartions_list'),
+    url(r'reservations/(?P<pk>\d+)/manage$', ManageReservationView.as_view(), name='manage_reservation'),
+    url(r'reservations/', ReservationsListView.as_view(), name='reservartions_list')
 ]
--- src/reservations_management/views.py
+++ src/reservations_management/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, FormView
+
 
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from laboratory.decorators import user_group_perms
 
 from .models import Reservations
+from .forms import ReservationsForm
 
 # Create your views here.
 
-@method_decorator(login_required, name='dispatch')
+
 @method_decorator(user_group_perms(perm='laboratory.add_objectfeatures'), name='dispatch')
-class ReservationsListView(ListView):
+class ReservationsListView(LoginRequiredMixin, ListView):
     model = Reservations
-    paginate_by = 10 
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['reservations'] = Reservations.objects.filter(laboratory__profile__user_id=self.request.user.id)
+        context['reservations'] = Reservations.objects.filter(
+            laboratory__profile__user_id=self.request.user.id)
+        return context
+
+
+@method_decorator(user_group_perms(perm='laboratory.add_objectfeatures'), name='dispatch')
+class ManageReservationView(LoginRequiredMixin, FormView):
+    template_name = 'reservations_management/manage_reservation.html'
+    form_class = ReservationsForm
+    fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reservation = Reservations.objects.get(pk=self.kwargs['pk'])
+        context['form'] = ReservationsForm(instance=reservation)
         return context
