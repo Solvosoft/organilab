9858ecf5c61a4a649a3d2118fdc870c61bef7c50
roberto <robertovargasrojas97@gmail.com>
2020-10-07 17:39:28 -0600

    R-7, R-8 : Products list created and showed correctly, product status can change now through an API

---
+++ src/reservations_management/api/views.py
@@ -0,0 +1,40 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
+from reservations_management.models import ReservedProducts
+from reservations_management.api.serializers import ReservedProductSerializer
+
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import TokenAuthentication
+
+
+class ApiReservedProductsCRUD(APIView):
+    # authentication_classes = [
+    #     TokenAuthentication
+    # ]
+    # permission_classes = [
+    #     IsAuthenticated
+    # ]
+
+    def get_object(self, pk):
+        try:
+            return ReservedProducts.objects.get(pk=pk)
+        except ReservedProducts.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        reserved_product = self.get_object(pk)
+        serializer = ReservedProductSerializer(reserved_product)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        reserved_product = self.get_object(pk)
+        serializer = ReservedProductSerializer(
+            reserved_product, data=request.data
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
