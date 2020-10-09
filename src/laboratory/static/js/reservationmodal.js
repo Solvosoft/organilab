39cdb784d9d2ba0afddc8459168606211d1cd742
Jason Zamora <jasonzatre@hotmail.com>
2020-10-09 09:33:58 -0600

    Functionality of modal ajax before changes in models.py

--- src/laboratory/static/js/reservationmodal.js
+++ src/laboratory/static/js/reservationmodal.js
@@ -4,11 +4,22 @@ function reserve_product(pk){
     shelf_object_id=pk;
 }
 
+function add_form_data(form){
+    const STATUS_SOLICITED = 3
+    form.append(
+        `<p>
+            <input type="hidden" name="shelf_object" id="id_shelf_object" value="${shelf_object_id}">
+        </p>
+        <p>
+            <input type="hidden" name="status" id="id_status" value="${STATUS_SOLICITED}">
+        </p>`
+    );
+}
+
 function add_reservation() {
     form_modal = $('#modal_reservation_form');
+    add_form_data(form_modal);
     data = form_modal.serialize();
-    console.log(data)
-    data["shelf_object"] = shelf_object_id;
     console.log(data)
     $.ajax({
         url: document.api_modal,
