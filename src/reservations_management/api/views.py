437e8a22466f0ed94e50fe15c0638b8bbb4814a7
roberto <robertovargasrojas97@gmail.com>
2020-10-14 17:07:10 -0600

    R-9 )Celery task to decrease product stock in a specific date is done

--- src/reservations_management/api/views.py
+++ src/reservations_management/api/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 
 from reservations_management.models import ReservedProducts
 from reservations_management.api.serializers import ReservedProductSerializer
-# from reservations_management.functions import add_decrease_stock_task
+from reservations_management.functions import add_decrease_stock_task
 
 # from rest_framework.permissions import IsAuthenticated
 # from rest_framework.authentication import TokenAuthentication
@@ -36,9 +36,8 @@ class ApiReservedProductsCRUD(APIView):
             reserved_product, data=request.data
         )
         if serializer.is_valid():
-            if(reserved_product.status == 1):
-                pass
-                # add_decrease_stock_task(reserved_product)
+            if(serializer.initial_data['status'] == '1'):
+                add_decrease_stock_task(reserved_product)
                 
             serializer.save()
             return Response(serializer.data)
