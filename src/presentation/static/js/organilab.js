bd240c3d2e6f35faa5620f73f384c0ad7de95b82
allexiusw <william.al1379@gmail.com>
2020-12-16 15:50:46 -0600

    Validate if CKEditor is defined or not to apply configurations

--- src/presentation/static/js/organilab.js
+++ src/presentation/static/js/organilab.js
@@ -5,26 +5,27 @@ $(document).ready(function(){
     e.preventDefault();
   });
 
-  CKEDITOR.on('dialogDefinition', function(ev) {
-    // Take the dialog name and its definition from the event data.
-    var dialogName = ev.data.name;
-    var dialogDefinition = ev.data.definition;
-
-    // Check if the definition is from the dialog we're
-    // interested in (the 'link' dialog).
-    if (dialogName == 'link') {
-      // Remove the 'Upload' and 'Advanced' tabs from the 'Link' dialog.
-      // dialogDefinition.removeContents('upload');
-      // dialogDefinition.removeContents('advanced');
-
-      // Get a reference to the 'Link Info' tab.
-      var infoTab = dialogDefinition.getContents('info');
-
-      // Get a reference to the "Target" tab and set default to '_blank'
-      var targetTab = dialogDefinition.getContents('target');
-      var targetField = targetTab.get('linkTargetType');
-      targetField['default'] = '_blank';
-    }
-  });
-
+  if (typeof CKEDITOR !== 'undefined') {
+    CKEDITOR.on('dialogDefinition', function(ev) {
+      // Take the dialog name and its definition from the event data.
+      var dialogName = ev.data.name;
+      var dialogDefinition = ev.data.definition;
+  
+      // Check if the definition is from the dialog we're
+      // interested in (the 'link' dialog).
+      if (dialogName == 'link') {
+        // Remove the 'Upload' and 'Advanced' tabs from the 'Link' dialog.
+        // dialogDefinition.removeContents('upload');
+        // dialogDefinition.removeContents('advanced');
+  
+        // Get a reference to the 'Link Info' tab.
+        var infoTab = dialogDefinition.getContents('info');
+  
+        // Get a reference to the "Target" tab and set default to '_blank'
+        var targetTab = dialogDefinition.getContents('target');
+        var targetField = targetTab.get('linkTargetType');
+        targetField['default'] = '_blank';
+      }
+    });  
+  }
 });
