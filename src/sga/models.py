d8d8906777a9700b008fef56dd2645e4253d573d
allexiusw <william.al1379@gmail.com>
2020-12-15 18:18:08 -0600

    Update donations model

--- src/sga/models.py
+++ src/sga/models.py
@@ -246,6 +246,7 @@ class Donation(models.Model):
     details = models.TextField(max_length=255, verbose_name=_("Details"))
     is_donator = models.BooleanField(default=True, verbose_name=_("Add me to the donators list"))
     is_paid = models.BooleanField(default=False, verbose_name=_("Is paid?"))
+    donation_date = models.DateTimeField(auto_now_add=True, verbose_name=_('Donation date'))
 
     class Meta:
         verbose_name = _("Donation")
