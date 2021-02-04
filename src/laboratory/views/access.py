7445930d0c38938679055933382e096707e742f2
William Alexander <william.al1379@gmail.com>
2021-02-04 17:22:47 -0600

    Update access_list management

--- src/laboratory/views/access.py
+++ src/laboratory/views/access.py
@@ -1,9 +1,10 @@
 # encoding: utf-8
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.shortcuts import render
 from laboratory.forms import OrganizationUserManagementForm, SearchUserForm
-from laboratory.models import OrganizationStructure, OrganizationUserManagement
+from laboratory.models import Laboratory, OrganizationStructure, OrganizationUserManagement, Profile
 from laboratory.decorators import has_lab_assigned
 
 
@@ -11,54 +12,31 @@ from laboratory.decorators import has_lab_assigned
 @permission_required(['laboratory.view_organizationusermanagement', 'laboratory.add_organizationusermanagement'])
 def access_management(request):
     context = {}
-    parent = None
-
-    if request.method == 'POST':
-        user = request.user
-        form = OrganizationUserManagementForm(request.POST)
-        if form.is_valid():
-            pk = int(request.POST["pk"])
-
-            if pk > 0:
-                parent = OrganizationStructure.objects.filter(pk=pk).first()
-
-            orga = OrganizationStructure(name=form.cleaned_data['name'], parent=parent)
-            orga.save()
-            orga_user = OrganizationUserManagement(group=form.cleaned_data['group'], organization=orga)
-            orga_user.save()
-            orga_user.users.add(user)
-            return redirect('laboratory:access_list')
-    else:
-        form = OrganizationUserManagementForm()
-
-    context['form'] = form
+    context['labs'] = request.user.profile.laboratories.all()
     return render(request, 'laboratory/access_management.html', context=context)
 
 
 @has_lab_assigned(lab_pk='pk')
 @permission_required('laboratory.view_organizationusermanagement')
 def users_management(request, pk):
-
-    context = {}
-    orga_user_managament = OrganizationUserManagement.objects.filter(organization__pk=pk).first()
-    users_organization = orga_user_managament.users.all()
-    users_pk = [user.pk for user in users_organization]
-
     if request.method == 'POST':
-        form = SearchUserForm(request.POST, users_list=users_pk)
+        users_list = Profile.objects.filter(laboratories__pk=pk).all()
+        form = SearchUserForm(request.POST, users_list=users_list)
         if form.is_valid():
-            user = form.cleaned_data['user']
-            orga_user_managament.users.add(user)
-            return redirect('laboratory:users_management', pk=pk)
-    else:
-        form = SearchUserForm(users_list=users_pk)
+            user = User.objects.get(username=form.cleaned_data['user'])
+            lab = Laboratory.objects.get(pk=pk)
+            if not hasattr(user, 'profile'):
+                profile = Profile(user=user)
+                profile.save()
+            user.profile.laboratories.add(lab)
+        return redirect('laboratory:users_management', pk=pk)
 
-    context['form'] = form
-
-    if orga_user_managament:
-        context['users_list'] = users_organization
-        context['organization'] = orga_user_managament.organization
-
+    users_pk = User.objects.filter(profile__laboratories__pk=pk).values_list('pk', flat=True)
+    context = {
+        'users_list': Profile.objects.filter(laboratories__pk=pk).all(),
+        'organization': Laboratory.objects.get(pk=pk),
+        'form': SearchUserForm(users_list=users_pk)
+    }
     return render(request, 'laboratory/users_management.html', context=context)
 
 
