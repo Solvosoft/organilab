a08cefd8400682d014451dafe975d9f178ea03d1
Kejebo <kenjen041@gmail.com>
2021-03-09 11:14:51 -0600

    update views to use middleware when need add substances

--- src/laboratory/middleware.py
+++ src/laboratory/middleware.py
@@ -14,21 +14,25 @@ class ProfileMiddleware:
     def process_view(self,request, view_func, view_args, view_kwargs):
 
         if 'lab_pk' in view_kwargs:
+            if view_kwargs['lab_pk'] is None:
+                 profile_in = ProfilePermission.objects.filter(profile=request.user.profile,
+                                                         laboratories_id=request.POST.get('lab_pk')).first()
+            else:
+                 profile_in = ProfilePermission.objects.filter(profile=request.user.profile,
+                                                         laboratories_id=view_kwargs['lab_pk']).first()
 
-            profile_in=ProfilePermission.objects.filter(profile=request.user.profile,
-                                                     laboratories_id=view_kwargs['lab_pk']).first()
             if profile_in:
 
                 roles = profile_in.rol.all()
                 user_permissions=[]
 
                 for rol in roles:
-                  user_permissions+=list(rol.permissions.values_list('content_type__app_label', 'codename').order_by())
+                  user_permissions += list(rol.permissions.values_list('content_type__app_label', 'codename').order_by())
 
-                user_permissions+=list(request.user.user_permissions.values_list('content_type__app_label', 'codename').order_by())
+                user_permissions += list(request.user.user_permissions.values_list('content_type__app_label', 'codename').order_by())
 
                 if user_permissions:
-                    request.user._perm_cache={"%s.%s" % (ct, name) for ct, name in user_permissions}
+                    request.user._perm_cache = {"%s.%s" % (ct, name) for ct, name in user_permissions}
 
-                print(request.user._perm_cache)
 
+            print(request.user.has_perm('laboratory.view_object'))
