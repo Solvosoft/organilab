5c60031b64d12db3df6aef3b8f2abd7e46fa6e54
Kejebo <kenjen041@gmail.com>
2021-05-04 16:54:20 -0600

    Add the procedure CRUD

--- src/academic/forms.py
+++ src/academic/forms.py
@@ -2,7 +2,11 @@ from ckeditor.widgets import CKEditorWidget
 from django import forms
 from .models import ProcedureStep, Procedure
 from django.conf import settings
-
+from djgentelella.forms.forms import GTForm
+from djgentelella.widgets import core as genwidgets
+from laboratory.models import Object,Catalog,Shelf
+from django.utils.translation import ugettext_lazy as _
+from organilab.settings import DATETIME_INPUT_FORMATS
 
 class ProcedureForm(forms.ModelForm):
     class Meta:
@@ -10,12 +14,38 @@ class ProcedureForm(forms.ModelForm):
         fields = ['title', 'description']
         widgets = {
             'description': CKEditorWidget(attrs={'lang': settings.LANGUAGE_CODE }),
+            'title':genwidgets.TextInput()
         }
 
-class ProcedureStepForm(forms.ModelForm):
+class ProcedureStepForm(forms.ModelForm,GTForm):
     class Meta:
         model = ProcedureStep
         fields = ['title', 'description']
         widgets = {
             'description': CKEditorWidget(attrs={'lang': settings.LANGUAGE_CODE }),
+            'title': genwidgets.TextInput
         }
+
+class ObservationForm(GTForm, forms.Form):
+    description = forms.CharField(widget=genwidgets.Textarea(), label= _("Description"))
+
+
+class ObjectForm(GTForm, forms.Form):
+    object = forms.ModelChoiceField(widget=genwidgets.Select(), queryset=Object.objects.all(), label= _('Object'), required=True)
+    quantity = forms.CharField(widget=genwidgets.TextInput(), max_length=20, label= _('Amount'), required=True)
+    unit = forms.ModelChoiceField(widget=genwidgets.Select(), queryset=Catalog.objects.filter(key='units'), label=_('Unit'), required=True)
+
+class StepForm(GTForm, forms.Form):
+    title = forms.CharField(widget=genwidgets.TextInput, label=_('title'))
+    description = forms.CharField(widget=CKEditorWidget(attrs={'lang': settings.LANGUAGE_CODE }),
+                                  label=_('Description'))
+class ReservationForm(GTForm, forms.Form):
+    shelf = forms.ModelChoiceField(widget=genwidgets.Select, queryset=Shelf.objects.all(), label=_("Shelf"),
+                                   required=True)
+    initial_date = forms.DateTimeField(widget=genwidgets.DateInput, input_formats=DATETIME_INPUT_FORMATS, required=False)
+    final_date = forms.DateTimeField(widget=genwidgets.DateInput, input_formats=DATETIME_INPUT_FORMATS, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(ReservationForm, self).__init__(*args, **kwargs)
+        shelf = Shelf.objects.filter(furniture__labroom__laboratory__id=int(60))
+        self.fields['shelf'].queryset = shelf
--- src/academic/views.py
+++ src/academic/views.py
@@ -1,70 +1,184 @@
 from django.shortcuts import get_object_or_404, redirect
-from djgentelella.cruds.base import CRUDView
-from djgentelella.cruds.inline_crud import InlineAjaxCRUD
-from academic.models import Procedure, ProcedureStep, ProcedureRequiredObject,\
-    ProcedureObservations
+from academic.models import Procedure, ProcedureStep, ProcedureRequiredObject,ProcedureObservations
 from django.urls.base import reverse_lazy
+from django.urls import reverse
 from laboratory.decorators import has_lab_assigned
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import permission_required
-from academic.forms import ProcedureForm, ProcedureStepForm
-
-
-
-# Default behaivor of CRUDView is check permissions in each request
-class ProcedureView(CRUDView):
-    model = Procedure
-    template_father = 'base.html'
-    add_form = ProcedureForm
-    update_form = ProcedureForm
-    
-
-class ProcedureRequiredObjectView(InlineAjaxCRUD):    
-    base_model = ProcedureStep
-    model = ProcedureRequiredObject
-    inline_field = 'step'
-    title = 'Objects required'
-    fields = ['object', 'quantity', 'measurement_unit']
-    list_fields = ['object', 'quantity', 'measurement_unit']
-
-
-class ProcedureObservationsView(InlineAjaxCRUD):    
-    base_model = ProcedureStep
-    model = ProcedureObservations
-    inline_field = 'step'
-    title = 'Observation'
-    fields=['description']
-    list_fields = ['description']
-
-
-# Default behaivor of CRUDView is check permissions in each request
-class StepsView(CRUDView):
-    model = ProcedureStep
-    inlines=[ProcedureRequiredObjectView, ProcedureObservationsView ]
-    list_fields = ['title']
-    fields=['title', 'description']
-    views_available = ['detail','update', 'delete']
-    template_father = "base.html"
-    add_form = ProcedureStepForm
-    update_form = ProcedureStepForm
-
-    def get_update_view(self):
-        crudU= CRUDView.get_update_view(self)
-        class CSV(crudU):
-            def get_success_url(self):
-                return reverse_lazy('academic_procedure_list')
-        return CSV
-    
-    def get_delete_view(self):
-        Dview= CRUDView.get_delete_view(self)
-        class CSV(Dview):
-            def get_success_url(self):
-                return reverse_lazy('academic_procedure_list')
-        return CSV
+from academic.forms import ProcedureForm, ProcedureStepForm,ObjectForm,ObservationForm,StepForm,ReservationForm
+from django.views.generic import ListView, CreateView, UpdateView,FormView
+from django.shortcuts import redirect, render
+from django.http import JsonResponse
+from laboratory.models import Object, Catalog
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core import serializers
 
 
 @permission_required('academic.add_procedurestep')
-def add_steps_wrapper(request, pk):
+def add_steps_wrapper(request, pk,lab_pk=None):
     procedure = get_object_or_404(Procedure, pk=pk)
     procstep = ProcedureStep.objects.create(procedure=procedure)
-    return redirect('academic_procedurestep_update', pk=procstep.pk)
+    return redirect(reverse('update_step', kwargs={'pk':procstep.pk, 'lab_pk': lab_pk}))
+
+
+
+class ProcedureListView(ListView):
+    model = Procedure
+    queryset = Procedure.objects.all()
+    template_name = 'academic/list.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context= super(ProcedureListView, self).get_context_data()
+        context['lab'] = self.kwargs['pk']
+        context['reservation_form']=ReservationForm
+        return context
+
+
+class ProcedureCreateView(CreateView):
+    model = Procedure
+    form_class=ProcedureForm
+    template_name = 'academic/procedure_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProcedureCreateView, self).get_context_data()
+        context['lab_pk']=self.kwargs['lab_pk']
+        return context
+
+    def get_success_url(self, **kwargs):
+        lab_pk = self.kwargs['lab_pk']
+        success_url = reverse_lazy('procedure_list', kwargs={'pk': lab_pk})
+        return success_url
+
+class ProcedureUpdateView(UpdateView):
+    model = Procedure
+    form_class= ProcedureForm
+    template_name = 'academic/procedure_create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProcedureUpdateView, self).get_context_data()
+        context['lab_pk'] = self.kwargs['lab_pk']
+        return context
+
+    def get_success_url(self, **kwargs):
+        lab_pk = self.kwargs['lab_pk']
+        success_url = reverse_lazy('procedure_list', kwargs={'pk': lab_pk})
+        return success_url
+
+def procedureStepDetail(request, pk, lab_pk):
+    steps = Procedure.objects.filter(pk=pk).first()
+    return render(request,'academic/detail.html', {'object':steps,'procedure':pk, 'lab_pk':lab_pk,'reservation_form':ReservationForm})
+
+
+class ProcedureStepCreateView(FormView):
+    form_class = StepForm
+    template_name = 'academic/procedure_steps.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProcedureStepCreateView, self).get_context_data()
+        context['object_form']=ObjectForm
+        context['observation_form']=ObservationForm
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        procedure = get_object_or_404(Procedure, pk=int(self.kwargs['pk']))
+        step = ProcedureStep.objects.create(procedure=procedure,title=form.cleaned_data['title'],
+                                         description=form.cleaned_data['description'])
+        step.save()
+
+        return response
+
+    def get_success_url(self):
+        lab_pk = self.kwargs['lab_pk']
+        success_url = reverse_lazy('procedure_list', kwargs={'pk': lab_pk})
+        return success_url
+
+class ProcedureStepUpdateView(UpdateView):
+    model = ProcedureStep
+    form_class = ProcedureStepForm
+    template_name = 'academic/procedure_steps.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProcedureStepUpdateView, self).get_context_data()
+        context['object_form'] = ObjectForm
+        context['observation_form'] = ObservationForm
+        context['step'] = ProcedureStep.objects.get(pk=int(self.kwargs['pk']))
+        context['lab_pk']=self.kwargs['lab_pk']
+        return context
+
+    def get_success_url(self, **kwargs):
+        lab_pk = self.kwargs['lab_pk']
+        success_url = reverse_lazy('procedure_list', kwargs={'pk': lab_pk})
+        return success_url
+
+
+def save_object(request, pk):
+    step = get_object_or_404(ProcedureStep, pk=pk)
+    unit = get_object_or_404(Catalog, pk=int(request.POST['unit']))
+    obj = get_object_or_404(Object, pk=int(request.POST['object']))
+
+    objects = ProcedureRequiredObject.objects.create(step=step, object=obj,
+                                          quantity=request.POST['quantity'],
+                                          measurement_unit=unit)
+    objects.save()
+    return JsonResponse({'data': get_objects(pk)})
+
+
+def delete_step(request):
+    step = ProcedureStep.objects.get(pk=int(request.POST['pk']))
+    step.delete()
+    return JsonResponse({'data': True})
+
+
+def remove_object(request,pk):
+    obj = ProcedureRequiredObject.objects.get(pk=int(request.POST['pk']))
+    obj.delete()
+
+    return JsonResponse({'data': get_objects(pk)})
+
+
+def get_objects(pk):
+    objects_list = ProcedureRequiredObject.objects.filter(step__id=pk)
+    aux = []
+    for data in objects_list:
+        aux.append({'obj': str(data.object), 'unit': str(data.measurement_unit), 'id': data.pk,'amount': data.quantity})
+    result = json.dumps(aux)
+    return result
+
+
+def save_observation(request, pk):
+
+    step = get_object_or_404(ProcedureStep, pk=pk)
+
+    objects = ProcedureObservations.objects.create(step=step, description=request.POST['description'])
+    objects.save()
+
+    return JsonResponse({'data': get_observations(pk)})
+
+
+def get_observations(pk):
+    obsevations = ProcedureObservations.objects.filter(step__id=pk)
+    aux = []
+    for data in obsevations:
+        aux.append({'description': data.description, 'id': data.pk})
+    result = json.dumps(aux)
+    return result
+
+
+def remove_observation(request,pk):
+    obj = ProcedureObservations.objects.get(pk=int(request.POST['pk']))
+    obj.delete()
+
+    return JsonResponse({'data': get_observations(pk)})
+
+
+def get_procedure(request):
+    procedure= get_object_or_404(Procedure, pk=int(request.POST['pk']))
+    return JsonResponse({'data': {'title': procedure.title, 'pk': procedure.pk}})
+
+def delete_procedure(request):
+    procedure = get_object_or_404(Procedure, pk=int(request.POST['pk']))
+    procedure.delete()
+    return JsonResponse({'data': True})
