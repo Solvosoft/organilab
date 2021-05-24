87df2c1601c8cecb7cbfe2d4705d54a88ef0dfd9
Kejebo <kenjen041@gmail.com>
2021-05-24 15:38:38 -0600

    Moved the send_email function.

--- src/laboratory/signals.py
+++ src/laboratory/signals.py
@@ -16,42 +16,6 @@ def notify_shelf_object_reach_limit(sender, **kwargs):
         send_email_to_ptech_limitobjs(instance)
 
 
-def get_emails(emails,blocked_emails):
-    return [x for x in emails if x not in blocked_emails]
-
-
-def send_email_limitobjs(lab, shelfobjects, enqueued=True):
-    allowed_emails = []
-    if len(shelfobjects)>0:
-        for shelfobject in shelfobjects:
-            blocked = BlockedListNotification.objects.filter(
-                laboratory=lab, object=shelfobject.object)
-            blocked_emails = [x for x in blocked.values_list('user__email', flat=True)]
-            ptech = Profile.objects.filter(laboratories__in=[lab])
-            emails = [x for x in ptech.values_list('user__email', flat=True)]
-            allowed_emails.extend([x for x in emails if x not in blocked_emails])
-
-        emails = list(set(allowed_emails))
-        context = {
-            'laboratory': lab,
-            'shelf_object': shelfobjects,
-        }
-        schema = 'https'
-        if settings.DEBUG:
-                schema = 'http'
-
-
-        url = f"/lab/{lab.pk}/blocknotifications/"
-        domain = Site.objects.get_current().domain
-        context['blockurl'] = f"{schema}://{domain}{url}"
-        context['domain'] = domain
-        send_email_from_template("Shelf object in limit",
-                                     emails,
-                                     context=context,
-                                     enqueued=enqueued,
-                                     user=None,
-                                     upfile=None)
-
 
 def send_email_to_ptech_limitobjs(shelf_object, enqueued=True):
     labroom = shelf_object.shelf.furniture.labroom
