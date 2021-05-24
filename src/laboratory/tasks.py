40bc1daea4a1c5fda92f1e0f99854b78830af651
Kejebo <kenjen041@gmail.com>
2021-05-24 10:27:44 -0600

    updated the notifications when the object in the limit quantity.

--- src/laboratory/tasks.py
+++ src/laboratory/tasks.py
@@ -2,30 +2,39 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import Celery
 from laboratory.models import ShelfObject, Laboratory,PrecursorReport
-from laboratory.signals import send_email_to_ptech_limitobjs
+from laboratory.signals import send_email_to_ptech_limitobjs, send_email_limitobjs
 
 from django.db.models.expressions import F
 from datetime import date
 
 app = Celery()
 
+def get_limited_shelf_objects(lab):
+    object_list = []
 
-@app.task
+    for rooms in lab.rooms.all():
+        for furniture in rooms.furniture_set.all():
+            for obj in furniture.get_limited_shelf_objects():
+                    object_list.append(obj)
+
+    return object_list
+
+
+#@app.task
 def notify_about_product_limit_reach():
-    limited_shelf_objects = get_limited_shelf_objects()
-
-    for shelf_object in limited_shelf_objects:
-        send_email_to_ptech_limitobjs(shelf_object, enqueued=False)
-
+    labs = Laboratory.objects.all()
+    object_list=[]
+    for lab in labs:
+        for shelfobjects in get_limited_shelf_objects(lab):
+                object_list.append(shelfobjects)
+        send_email_limitobjs(lab,object_list, enqueued=False)
+        object_list.clear()
 
 @app.on_after_configure.connect
 def setup_daily_tasks(sender, **kwargs):
     sender.add_periodic_task(
         2, notify_about_product_limit_reach.s(), name='notify')
 
-
-def get_limited_shelf_objects():
-    return ShelfObject.objects.filter(quantity__lte=F('limit_quantity'))
 
 @app.task()
 def create_precursor_reports():
