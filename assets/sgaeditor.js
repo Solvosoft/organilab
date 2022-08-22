8f7c8869dda7895a11276c3a05c5a88010a7c474
Luis Zárate M <luis.zarate@solvosoft.com>
2022-08-21 20:50:29 -0600

    SVGEditor

---
+++ assets/sgaeditor.js
@@ -0,0 +1,22 @@
+import Editor from 'svgedit/dist/editor/Editor';
+
+const svgEditor = new Editor(document.getElementById('canvas_editor'))
+svgEditor.init()
+svgEditor.setConfig({
+          allowInitialUserOverride: true,
+          imgPath: '/static/editor/images',
+          extensions: [],
+          noDefaultExtensions: true,
+          userExtensions: [/* { pathName: './react-extensions/react-test/dist/react-test.js' } */]
+        })
+// Variable XDOMAIN below is created by Rollup for the Xdomain build (see rollup.config.js)
+/* globals XDOMAIN */
+try { // try clause to avoid js to complain if XDOMAIN undefined
+          if (XDOMAIN) {
+            svgEditor.setConfig({
+              canvasName: 'xdomain', // Namespace this
+              allowedOrigins: [ '*' ]
+            })
+    console.info('xdomain config activated')
+  }
+} catch (error) { /* empty fn */ }
