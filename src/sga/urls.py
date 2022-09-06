1f93d6ba1cbebfe17c7d466497377161936050b9
Luis Zárate M <luis.zarate@solvosoft.com>
2022-09-05 20:43:33 -0600

    Add sustance type

--- src/sga/urls.py
+++ src/sga/urls.py
@@ -5,6 +5,8 @@
 """
 
 # Import functions of another modules
+from django.urls import path
+
 from .views import index_sga, label_creator, get_sga_editor_options, information_creator, template, editor, \
     render_pdf_view
 
@@ -18,23 +20,22 @@ app_name = 'sga'
 # Views
 urlpatterns = [
     # sga/index_sga/
-    url(r'index_sga', index_sga, name='index_sga'),
+    path('index_sga', index_sga, name='index_sga'),
     # sga/label_information/
-    url(r'information', information_creator, name='information'),
+    path('information', information_creator, name='information'),
     # sga/label_template/
-    url(r'template', template, name='template'),
-    url(r'sga_editor_options', get_sga_editor_options, name='get_sga_editor_options'),
-    url(r'show_editor_preview/(?P<pk>\d+)$', views.show_editor_preview, name='show_editor_preview'),
+    path('editor_edit/<str:sustancetype>', template, name='template'),
+    path('sga_editor_options', get_sga_editor_options, name='get_sga_editor_options'),
+    path('show_editor_preview/<int:pk>', views.show_editor_preview, name='show_editor_preview'),
     # sga/label_creator/
-    url(r'label_creator/(?P<step>\d)?$', label_creator, name='label_creator'),
+    path('label_creator/<int:pk>', label_creator, name='label_creator'),
     # Django Ajax Selects
     url(r'^ajax_select/', include(ajax_select_urls)),
-    # sga/auto_complete_sustance/
-    url(r'search_autocomplete_sustance', views.search_autocomplete_sustance, name='search_autocomplete_sustance'),
-    url(r'label_information', views.label_information, name='label_information'),
-    url(r'label_template', views.label_template, name='label_template'),
-    # sga/label_editor/
-    url(r'label_editor', views.label_editor, name='label_editor'),
+    path('search_autocomplete_sustance', views.search_autocomplete_sustance, name='search_autocomplete_sustance'),
+    path('label_information', views.label_information, name='label_information'),
+    path('label_template', views.label_template, name='label_template'),
+
+    path('label_editor', views.label_editor, name='label_editor'),
     # sga/editor
     url(r'editor', editor, name='editor'),
     url(r'recipient_size/(?P<is_template>\d+)/(?P<pk>\d+)?$', views.get_recipient_size, name='get_recipient_size'),
