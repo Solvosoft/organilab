e8643042203b21d355c29300ffe20d58622c9e5e
Luis Zárate M <luis.zarate@solvosoft.com>
2022-09-12 23:07:22 -0600

    Multiline not complete

--- src/sga/static/userextensions/text-multiline/text-multiline.js
+++ src/sga/static/userextensions/text-multiline/text-multiline.js
@@ -25,11 +25,61 @@ export default {
       referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
     }
 
+    const editText = (event)=>{
+
+         let selects = svgCanvas.getSelectedElements()
+         if(selects.length==1 && selects[0].attributes.class.value=="textelementforeign"){
+             tinymce.get("wyswygmodaltextarea").setContent(selects[0].innerHTML);
+             $('#wyswygmodal').modal('show');
+         }
+    }
     const addText = (event)=>{
-        alert(svgEditor.generateSVGfromTextArea(document.getElementById("wyswygmodaltextarea")))
+        let content = tinymce.get("wyswygmodaltextarea").getContent()
+        let selects = svgCanvas.getSelectedElements()
+        if(selects.length==1 && selects[0].attributes.class.value=="textelementforeign"){
+            selects[0].innerHTML=content
+        }else{
+
+
+        const textclass="textelementforeign"
+        const curStyle = svgCanvas.getStyle()
+        const curShape = svgCanvas.addSVGElementsFromJson({
+                  element: 'foreignObject',
+                  curStyles: true,
+                  attr: {
+                    x:100,
+                    y:100,
+                    height: 100,
+                    width: 100,
+                    class: textclass,
+                    id: svgCanvas.getNextId(),
+                    opacity: curStyle.opacity
+
+                  }
+              })
+               curShape.innerHTML=content
+            curShape.addEventListener("dblclick", editText);
+            svgCanvas.setSelectedElements(0, curShape)
+            svgCanvas.setCurrentMode('select')
+            svgEditor.leftPanel.updateLeftPanel('tool_select')
+            }
+            $('#wyswygmodal').modal('hide');
     }
 
     document.getElementById("btnsave-multiline").addEventListener("click", addText);
+
+    const addStyleText  = (elements) => {
+        elements.forEach((htmlelement)=>{
+
+            if(htmlelement.attributes.hasOwnProperty('data-mce-style') ){
+               htmlelement.style.cssText = htmlelement.attributes['data-mce-style'].value
+            }
+            if(htmlelement.children !== undefined){
+                    addStyleText(Array.from(htmlelement.children))
+            }
+
+        })
+    }
 
     return {
            name: svgEditor.i18next.t(`${name}:name`),
@@ -47,8 +97,7 @@ export default {
         })
       },
       mouseDown () {
-        if (svgCanvas.getMode() === modeId) {
-          svgEditor.setPanning(true)
+       if (svgCanvas.getMode() === modeId) {
           return {
             started: true
           }
@@ -57,7 +106,6 @@ export default {
       },
       mouseUp () {
         if (svgCanvas.getMode() === modeId) {
-          svgEditor.setPanning(false)
           return {
             keep: false,
             element: null
