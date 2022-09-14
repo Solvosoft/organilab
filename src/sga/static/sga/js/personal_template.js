65bd26fabee5cf7565bb75a2ec7b673d01030d97
Marcela González Zúñiga <marcegz31@gmail.com>
2022-09-14 10:49:29 -0600

    Cleaning delete sga label process

--- src/sga/static/sga/js/personal_template.js
+++ src/sga/static/sga/js/personal_template.js
@@ -1,73 +1,45 @@
 $( document ).ready(function() {
-$('#table_template_list').DataTable();
-
+    $('#table_template_list').DataTable();
 });
-const sendrequest=(element=>{
 
-let pk=element.getAttribute('data-id');
-     $.ajax({
-        url: 'sga/getData/',
-        type:'GET',
-        data: {pk},
-        headers: {'X-CSRFToken': getCookie('csrftoken') },
-        datatype:'json',
-        success: function (response) {
-        data=JSON.parse(response);
-        create_table(data);
-      }
-        });
 
-});
 function delete_template(element){
-const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
 
-swalWithBootstrapButtons.fire({
-  title: 'Esta seguro de eliminar la plantilla?',
-  text: "Estas a tiempo de revertir esta acción!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Si',
-  cancelButtonText: 'No',
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) {
-    swalWithBootstrapButtons.fire(
-      'Eliminado!',
-      'La plantilla a sido eliminada.',
-      'success'
-    )
-        //sendrequest(element);
+    swalWithBootstrapButtons.fire({
+        title: 'Esta seguro de eliminar la plantilla?',
+        text: "Estas a tiempo de revertir esta acción!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            var id =element.getAttribute('data-id');
+            var url = document.url_delete_sgalabel;
 
-  } else if (
-    result.dismiss === Swal.DismissReason.cancel
-  ) {
-    swalWithBootstrapButtons.fire(
-      'Cancelado',
-      'El archivo no fue eliminado :)',
-      'error'
-    )
-  }
-})
-}
+            if(id){
+                url = url.replace('0', id);
+            }
+            window.location = url;
 
-function create_table(data){
-      let tbody=document.querySelector('#template_list');
-        tbody.innerHTML='';
-        data.forEach((item,i)=>{
-         tbody.innerHTML+=`<tr>
-                <td>${item['fields']['name']}</td>
-                <td><a class="btn btn-md btn-info" href="/sga/get_pdf/${item.pk}"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
-                    <a class="deleted btn btn-md btn-danger" onclick="delete_template(this)" data-id=${item['pk']}><i class="fa fa-trash"></i></a></td>
-            </tr>`
-
-      });
-
+    } else if (
+        result.dismiss === Swal.DismissReason.cancel
+    ){
+        swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'El archivo no fue eliminado :)',
+            'error'
+        )
+    }
+    })
 }
 
 
