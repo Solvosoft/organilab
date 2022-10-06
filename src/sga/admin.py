d8f7a79b7e83bee5c53ebd8fa1dfe7c4e9b5dbe9
Kendrick <kenjen041@gmail.com>
2022-10-06 17:58:18 -0600

    Adding security leaf model in admin

--- src/sga/admin.py
+++ src/sga/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from sga.models import TemplateSGA, PersonalTemplateSGA
+from sga.models import TemplateSGA, PersonalTemplateSGA, SecurityLeaf
 from .models import DangerIndication, BuilderInformation, Substance, Label, \
     WarningClass, Component, WarningWord, Pictogram, PrudenceAdvice, \
     RecipientSize, Donation
@@ -66,3 +66,4 @@ admin.site.register(Label, AdminLabels)
 admin.site.register(TemplateSGA)
 admin.site.register(Donation, DonationAdmin)
 admin.site.register(PersonalTemplateSGA)
+admin.site.register(SecurityLeaf)
