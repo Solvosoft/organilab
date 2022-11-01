d93aa4ed16bbcf236381e02405bad74f1ac57e4e
Marcela González Zúñiga <marcegz31@gmail.com>
2022-11-01 14:47:47 -0600

    Users add function by organization

--- src/auth_and_perms/views/organizationstructure.py
+++ src/auth_and_perms/views/organizationstructure.py
@@ -1,11 +1,12 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from auth_and_perms.models import ProfilePermission, Rol, Profile
-from laboratory.models import OrganizationStructure
-from django.conf import settings
-
+from auth_and_perms.forms import AddUserForm
+from auth_and_perms.models import ProfilePermission, Rol
+from laboratory.models import OrganizationStructure, OrganizationUserManagement
+from django.utils.translation import gettext_lazy as _
 
 def getLevelClass(level):
     color = {
@@ -52,7 +53,9 @@ def organization_manage_view(request):
 
     for node in parents:
         getTree(node, nodes, level=0)
-    context={'nodes': nodes }
+    context={'nodes': nodes,
+             'adduserform': AddUserForm()
+             }
     return render(request, 'auth_and_perms/list_organizations.html', context)
 
 def manage_user_rolpermission(profilepermission, rol):
@@ -79,3 +82,25 @@ def update_user_rol(request, user_pk, rol_pk):
         manage_user_rolpermission(profilepermission, rol)
         response['result'] = 'ok'
     return JsonResponse(response)
+
+
+
+def add_users_organization(request, pk):
+
+    organizationstructure = get_object_or_404(OrganizationStructure, pk=pk)
+    orgusersmanagement = OrganizationUserManagement.objects.filter(organization=organizationstructure)
+
+    if request.method == 'POST':
+        if orgusersmanagement.exists():
+            form = AddUserForm(request.POST, instance=orgusersmanagement.first())
+            if form.is_valid():
+                form.save(commit=False)
+                form.save_m2m()
+                messages.success(request, _("Element saved successfully"))
+                return redirect('auth_and_perms:organizationManager')
+        messages.error(request, _("Organization doesn't exists"))
+    return redirect('auth_and_perms:organizationManager')
+
+
+
+
