52d2b659f59880a6012beda208e78b9f9a943586
Luis Zárate Montero <luis.zarate@solvosoft.com>
2022-11-10 22:56:16 -0600

    Protect media files

---
+++ src/auth_and_perms/views/__init__.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.http import HttpResponseForbidden
+
+def media_access(request, path):
+    """
+    When trying to access :
+    myproject.com/media/uploads/passport.png
+
+    If access is authorized, the request will be redirected to
+    myproject.com/protected/media/uploads/passport.png
+
+    This special URL will be handle by nginx we the help of X-Accel
+    """
+    user = request.user
+    if user.is_authenticated():
+        response = HttpResponse()
+        # Content-type will be detected by nginx
+        del response['Content-Type']
+        response['X-Accel-Redirect'] = '/protected/' + path
+        return response
+    return HttpResponseForbidden('Not authorized to access this media.')
