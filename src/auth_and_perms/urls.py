9842835396c51ae9eedd8e6e106aef4989601306
Luis Zárate Montero <luis.zarate@solvosoft.com>
2022-11-11 23:33:14 -0600

    New registration method

--- src/auth_and_perms/urls.py
+++ src/auth_and_perms/urls.py
@@ -5,6 +5,8 @@ from auth_and_perms.views import organizationstructure as orgstruct
 
 from rest_framework.routers import SimpleRouter
 
+from auth_and_perms.views import user_org_creation
+
 routes = SimpleRouter()
 
 routes.register('rol', RolAPI, 'api-rol' )
@@ -14,6 +16,9 @@ app_name='auth_and_perms'
 
 urlpatterns = [
     path('api/', include(routes.urls)),
+    path('organization/registration',  user_org_creation.register_user_to_platform, name='register_user_to_platform'),
+    path('organization/totp/<int:pk>/',  user_org_creation.create_profile_otp, name='user_org_creation_totp'),
+    path('totp/img/<int:pk>/',  user_org_creation.show_QR_img, name='show_qr_img'),
     path('organization/manage/',  orgstruct.organization_manage_view, name='organizationManager'),
     path('organization/manage/addusersorganization/<int:pk>/',  orgstruct.add_users_organization, name='addusersorganization'),
     path('organization/manage/users/add/<int:pk>/', orgstruct.AddUser.as_view(), name="add_user"),
