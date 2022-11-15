d9364a72a36f29a026dbbb459481ce0a3016bb00
Marcela González Zúñiga <marcegz31@gmail.com>
2022-11-15 14:52:34 -0600

    Rol base select filter was updated from utils file

--- src/auth_and_perms/utils.py
+++ src/auth_and_perms/utils.py
@@ -4,4 +4,4 @@ from laboratory.models import OrganizationStructure
 
 def get_roles_by_user(user):
     orgs = OrganizationStructure.os_manager.filter_user(user).values_list('pk', flat=True)
-    return Rol.objects.filter(organizationstructure__in=orgs)
+    return Rol.objects.filter(organizationstructure__in=orgs).distinct()
