54c925d9b1a3cf06a616ccc18635d10401468285
Luis Zárate M <luis.zarate@solvosoft.com>
2022-11-21 23:14:49 -0600

    Fixed import data groups

--- src/laboratory/migrations/0056_remove_organizationusermanagement_group.py
+++ src/laboratory/migrations/0056_remove_organizationusermanagement_group.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='organizationusermanagement',
-            name='group',
-        ),
+#        migrations.RemoveField(
+#            model_name='organizationusermanagement',
+#            name='group',
+#        ),
     ]
