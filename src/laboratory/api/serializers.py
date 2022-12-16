83cdc1619bf8a7c1b87c120e28bed9c991835f39
Luis Zárate Montero <luis.zarate@solvosoft.com>
2022-12-16 16:01:16 -0600

    First commit for adding protocols

--- src/laboratory/api/serializers.py
+++ src/laboratory/api/serializers.py
@@ -1,9 +1,15 @@
+from django.urls import reverse
 from rest_framework import serializers
 
 from laboratory.models import CommentInform
 from reservations_management.models import ReservedProducts, Reservations
 from organilab.settings import DATETIME_INPUT_FORMATS
+from laboratory.models import Protocol
+from django.utils.translation import gettext_lazy as _
 
+from django_filters import DateFromToRangeFilter, DateTimeFromToRangeFilter
+from djgentelella.fields.drfdatetime import DateRangeTextWidget, DateTimeRangeTextWidget
+from django_filters import FilterSet
 
 class ReservedProductsSerializer(serializers.ModelSerializer):
     initial_date = serializers.DateTimeField(input_formats=DATETIME_INPUT_FORMATS, required=False)
@@ -29,3 +35,58 @@ class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommentInform
         fields = '__all__'
+
+
+
+
+class ProtocolFilterSet(FilterSet):
+
+    class Meta:
+        model = Protocol
+        fields = {}
+
+
+class ProtocolSerializer(serializers.ModelSerializer):
+    action = serializers.SerializerMethodField()
+    file = serializers.SerializerMethodField()
+
+    def get_file(self, obj):
+        if not obj:
+            return {
+                'url': '#',
+                'display_name': _("File not found")
+            }
+
+        return {
+            'url': obj.file.url,
+            'display_name': _("Download")
+        }
+
+    def get_action(self, obj):
+        user = self.context['request'].user
+        btn = ''
+        if user.has_perm('laboratory.change_protocol'):
+            btn += "<a href=\"%s\" class='btn btn-outline-warning btn-sm'><i class='fa fa-edit' aria-hidden='true'></i> Edit</a>"%(
+                reverse('laboratory:protocol_update', args=(obj.laboratory.pk, obj.pk))
+            )
+        if user.has_perm('laboratory.delete_protocol'):
+            btn += "<a href=\"%s\" class='btn btn-outline-danger btn-sm'><i class='fa fa-trash' aria-hidden='true'></i> Delete</a>"%(
+                reverse('laboratory:protocol_delete', args=(obj.laboratory.pk, obj.pk))
+            )
+
+        return btn
+
+    class Meta:
+        model = Protocol
+        fields = ['name', 'short_description', 'file', 'action']
+
+
+
+class ProtocolDataTableSerializer(serializers.Serializer):
+    data = serializers.ListField(child=ProtocolSerializer(), required=True)
+    draw = serializers.IntegerField(required=True)
+    recordsFiltered = serializers.IntegerField(required=True)
+    recordsTotal = serializers.IntegerField(required=True)
+
+
+
---
+++ src/laboratory/protocol/views.py
@@ -0,0 +1,59 @@
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from rest_framework.reverse import reverse_lazy
+
+from laboratory.models import Protocol, Laboratory, Object
+from laboratory.protocol.forms import ProtocolForm
+from laboratory.views.djgeneric import CreateView, DeleteView, UpdateView
+
+@permission_required('laboratory.view_protocol')
+def protocol_list(request, *args, **kwargs ):
+    context={
+        'lab_pk': kwargs['lab_pk']
+    }
+    return render(request, 'laboratory/protocol/protocol_list.html', context=context)
+
+@method_decorator(permission_required('laboratory.add_protocol'), name='dispatch')
+class ProtocolCreateView(CreateView):
+    model = Protocol
+    template_name = 'laboratory/protocol/create.html'
+    form_class = ProtocolForm
+
+
+    def get_success_url(self):
+        return reverse_lazy('laboratory:protocol_list', args=(self.lab,))
+
+    def get_context_data(self, **kwargs):
+        context = super(ProtocolCreateView, self).get_context_data(**kwargs)
+        context['lab_pk'] = self.lab
+        return context
+
+    def form_valid(self, form):
+        protocol = form.save(commit=False)
+        laboratory = get_object_or_404(Laboratory, pk = self.lab)
+        protocol.laboratory = laboratory
+        protocol.upload_by=self.request.user
+        protocol.save()
+        return super(ProtocolCreateView,self).form_valid(form)
+
+
+class ProtocolUpdateView(UpdateView):
+    model = Protocol
+    success_url = '/'
+    form_class = ProtocolForm
+    template_name = 'laboratory/protocol/update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('laboratory:protocol_list', args=(self.lab,))
+
+
+class ProtocolDeleteView(DeleteView):
+    model = Protocol
+    template_name = 'laboratory/protocol/delete.html'
+    success_url = ''
+
+    def get_success_url(self):
+        return reverse_lazy('laboratory:protocol_list', args=(self.lab,))
