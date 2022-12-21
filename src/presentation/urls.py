3c40671224523476d890e49498faedabded08ffb
Marcela González Zúñiga <marcegz31@gmail.com>
2022-12-21 15:47:20 -0600

    Updating MSDS CRUD view

--- src/presentation/urls.py
+++ src/presentation/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import index_tutorial
 
 urlpatterns = [
-    path('tutorial', index_tutorial, name='tutorials')
+    path('tutorial/<int:org_pk>', index_tutorial, name='tutorials')
 ]
