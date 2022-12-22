7f569cde1283c1b5f164b101e100af72c7026d87
Luis Zárate Montero <luis.zarate@solvosoft.com>
2022-12-22 12:44:17 -0600

    fixed logentry passing org_pk

--- src/laboratory/views/logentry.py
+++ src/laboratory/views/logentry.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def get_logentry_from_organization(request, pk):
-    return render(request, 'laboratory/logentry_list.html', context={'organization': pk})
+def get_logentry_from_organization(request, org_pk):
+    return render(request, 'laboratory/logentry_list.html', context={'org_pk': org_pk})
