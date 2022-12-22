46499a58b5d8212a19b43ee4e1c72fd05970249d
Luis Zárate Montero <luis.zarate@solvosoft.com>
2022-12-22 15:20:45 -0600

    Update migrations

--- src/presentation/urls.py
+++ src/presentation/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index_organilab, name='index'),
     path('tutorial/<int:org_pk>', views.index_tutorial, name='tutorials'),
-    path('donate$', views.donate, name='donate'),
-    path('donate_success$', views.donate_success, name='donate_success'),
+    path('donate', views.donate, name='donate'),
+    path('donate_success', views.donate_success, name='donate_success'),
 ]
