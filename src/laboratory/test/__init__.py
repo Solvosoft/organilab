f954cbe8cee13a9d1c5b6523d115187ac16962bb
Marcela González Zúñiga <marcegz31@gmail.com>
2023-01-31 13:24:58 -0600

    Updating and sorting laboratory test

---
+++ src/laboratory/test/__init__.py
@@ -0,0 +1,4 @@
+from laboratory.test.inform_tests import *
+from laboratory.test.laboratory_tests import *
+from laboratory.test.protocol_tests import *
+from laboratory.test.provider_tests import *
