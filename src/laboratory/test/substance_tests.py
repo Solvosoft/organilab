aac77f6a2122182cb9d98739e7dac49eb0585fbe
Marcela González Zúñiga <marcegz31@gmail.com>
2023-02-02 17:17:21 -0600

    Updating substance tests file

---
+++ src/laboratory/test/substance_tests.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from laboratory.test.utils import BaseSetUpTest
+
+
+class SubstanceViewTest(BaseSetUpTest):
+
+    def test_get_substance_list(self):
+        url = reverse("laboratory:sustance_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_substance(self):
+        url = reverse("laboratory:sustance_delete", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": 1})
+        response = self.client.post(url)
+        success_url = reverse("laboratory:sustance_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, success_url)
