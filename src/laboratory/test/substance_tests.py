06922a38e3dd16c3ecfe065dcf457f56f4dc147d
Marcela González Zúñiga <marcegz31@gmail.com>
2023-02-06 16:51:12 -0600

    Organization tests were included, also updating laboratory and substance test

--- src/laboratory/test/substance_tests.py
+++ src/laboratory/test/substance_tests.py
@@ -10,7 +10,7 @@ class SubstanceViewTest(BaseSetUpTest):
         self.assertEqual(response.status_code, 200)
 
     def test_delete_substance(self):
-        url = reverse("laboratory:sustance_delete", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": 1})
+        url = reverse("laboratory:sustance_delete", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": 2})
         response = self.client.post(url)
         success_url = reverse("laboratory:sustance_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         self.assertEqual(response.status_code, 302)
