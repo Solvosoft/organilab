b99a3c22fce9824e0ae328880448bd2aebc40afe
Kendrick <kenjen041@gmail.com>
2023-02-23 12:21:19 -0600

    added the progress bar of laboratories

--- src/laboratory/templatetags/shelf_objects.py
+++ src/laboratory/templatetags/shelf_objects.py
@@ -14,3 +14,16 @@ def get_shelfs(lab):
 def get_shelf_objects(shelf):
     objects = ShelfObject.objects.filter(shelf=shelf)
     return objects
+
+@register.simple_tag
+def get_lab_porcentage(lab):
+    objects = Shelf.objects.filter(furniture__labroom__laboratory=lab, discard=True,furniture__dataconfig__isnull=False).distinct()
+    amount=objects.count()
+    porcentage = 0
+    for obj in objects:
+        porcentage+=obj.get_refuse_porcentage()
+    try:
+        porcentage = (porcentage / amount)
+    except ZeroDivisionError:
+        porcentage = 0
+    return porcentage
