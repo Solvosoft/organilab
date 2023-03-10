8d529bc03934f34e7bf0c0a759b66e3b9fe9a0b7
Marcela González Zúñiga <marcegz31@gmail.com>
2023-03-10 15:13:41 -0600

    Checking protocol view tests

--- src/laboratory/tests/test_protocol.py
+++ src/laboratory/tests/test_protocol.py
@@ -2,16 +2,20 @@ from django.urls import reverse
 
 from laboratory.models import Protocol
 from laboratory.tests.utils import BaseLaboratorySetUpTest
+import json
 
 class ProtocolViewTest(BaseLaboratorySetUpTest):
 
-    def test_get_protocol_list(self):
+    def test_protocol_list(self):
+        #This view is just a container(table), protocol table loads by "laboratory:api-protocol-list"
         url = reverse("laboratory:protocol_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name="laboratory/protocol/protocol_list.html")
 
     def test_update_protocol(self):
-        url = reverse("laboratory:protocol_update", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": 1})
+        protocol = Protocol.objects.get(name="Lavado de manos")
+        url = reverse("laboratory:protocol_update", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": protocol.pk})
 
         # Checking by method get if initial data protocol exists
         response_get = self.client.get(url)
@@ -42,13 +46,16 @@ class ProtocolViewTest(BaseLaboratorySetUpTest):
         self.assertIn("Manejo de desechos", list(Protocol.objects.values_list("name", flat=True)))
 
     def test_delete_protocol(self):
-        url = reverse("laboratory:protocol_delete", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": 1})
+        protocol = Protocol.objects.get(name="Manipulación de instrumentos de laboratorio")
+        url = reverse("laboratory:protocol_delete", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": protocol.pk})
         response = self.client.post(url)
         success_url = reverse("laboratory:protocol_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, success_url)
+        self.assertNotIn("Manipulación de instrumentos de laboratorio", list(Protocol.objects.values_list("name", flat=True)))
 
     def test_api_protocol_list(self):
         url = reverse("laboratory:api-protocol-list")
-        response = self.client.get(url)
+        response = self.client.get(url, data={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         self.assertEqual(response.status_code, 200)
+        self.assertContains(json.loads(response.content)['data'], "Control de plagas")
