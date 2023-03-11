15c90f8f0b974b48d1da09ff54610ac31ea52a07
Luis Zárate Montero <luis.zarate@solvosoft.com>
2023-03-10 20:11:33 -0600

    allow superuser see all functionalities

--- src/auth_and_perms/templatetags/user_rol_tags.py
+++ src/auth_and_perms/templatetags/user_rol_tags.py
@@ -103,6 +103,8 @@ def get_organization_table(org):
 
 @register.simple_tag(takes_context=True)
 def has_perm_in_org(context, org_pk,  permission):
+    if context['request'].user.is_superuser:
+        return True
     app_label, codename = permission.split(".")
     profile_in = ProfilePermission.objects.filter(profile=context['request'].user.profile,
                                                   content_type__app_label='laboratory',
