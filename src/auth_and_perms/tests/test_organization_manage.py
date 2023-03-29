7ec87182b23b595557b9a2dab7a45135368a0f2d
Marcela González Zúñiga <marcegz31@gmail.com>
2023-03-29 17:21:32 -0600

    Pruebas de creación de perfiles de permisos por organización y laboratorio

--- src/auth_and_perms/tests/test_organization_manage.py
+++ src/auth_and_perms/tests/test_organization_manage.py
@@ -9,24 +9,34 @@ from laboratory.tests.utils import BaseOrganizatonManageSetUpTest
 
 class ActionRolViewTest(BaseOrganizatonManageSetUpTest):
 
+    def setUp(self):
+        super().setUp()
+        self.base_data = {
+            'rols': [self.role_manage_lab.pk],
+            'as_role': True,
+            'as_user': False,         # REMOVE
+            'as_conttentype': False,  # REMOVE
+            'contenttypeobj': {
+                'model': 'laboratory',
+                'appname': 'laboratory'
+            },
+        }
+
     def test_check_user1_append_rol_to_profile(self):
         """
         Usuario 1 agregando 1 rol(Gestión lab) en una org de la cual NO es miembro, CASO NO PERMITIDO DEBERIA FALLAR
         """
 
         url = reverse("auth_and_perms:api-rolbyorg-detail", kwargs={"pk": self.org2.pk, })
-        self.base_data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab3_org2.pk})
-        self.base_data.update({
-            'mergeaction': 'append',
-            'profile': self.profile2_org2.pk,
-        })
-        response = self.client1_org1.put(url, data=json.dumps(self.base_data), content_type='application/json')
+        data = self.base_data
+        data.update({'mergeaction': 'append', 'profile': self.profile2_org2.pk})
+        data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab3_org1.pk})
+        response = self.client1_org1.put(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile2_org2,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['contenttypeobj']['objectid']).first()
+                                              object_id=data['contenttypeobj']['objectid']).first()
         self.assertEqual(response.status_code, 403)
         self.assertFalse(self.role_manage_lab in pp.rol.all())
-        self.assertFalse(self.org2 in self.user1_org_list)
 
     def test_check_user2_append_rol_to_profile(self):
         """
@@ -34,18 +44,15 @@ class ActionRolViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-rolbyorg-detail", kwargs={"pk": self.org2.pk, })
-        self.base_data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab3_org2.pk})
-        self.base_data.update({
-            'mergeaction': 'append',
-            'profile': self.profile2_org2.pk,
-        })
-        response = self.client2_org2.put(url, data=json.dumps(self.base_data), content_type='application/json')
+        data = self.base_data
+        data.update({'mergeaction': 'append', 'profile': self.profile2_org2.pk})
+        data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab3_org1.pk})
+        response = self.client2_org2.put(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile2_org2,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['contenttypeobj']['objectid']).first()
+                                              object_id=data['contenttypeobj']['objectid']).first()
         self.assertEqual(response.status_code, 200)
         self.assertTrue(self.role_manage_lab in pp.rol.all())
-        self.assertTrue(self.org2 in self.user2_org_list)
 
     def test_check_user1_sustract_rol_to_profile(self):
         """
@@ -53,18 +60,15 @@ class ActionRolViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-rolbyorg-detail", kwargs={"pk": self.org2.pk, })
-        self.base_data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab4_org2.pk})
-        self.base_data.update({
-            'mergeaction': 'sustract',
-            'profile': self.profile2_org2.pk,
-        })
-        response = self.client1_org1.put(url, data=json.dumps(self.base_data), content_type='application/json')
+        data = self.base_data
+        data.update({'mergeaction': 'sustract', 'profile': self.profile2_org2.pk})
+        data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab4_org2.pk})
+        response = self.client1_org1.put(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile2_org2,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['contenttypeobj']['objectid']).first()
+                                              object_id=data['contenttypeobj']['objectid']).first()
         self.assertEqual(response.status_code, 403)
         self.assertFalse(not self.role_manage_lab in pp.rol.all())
-        self.assertFalse(self.org2 in self.user1_org_list)
 
     def test_check_user2_sustract_rol_to_profile(self):
         """
@@ -72,18 +76,15 @@ class ActionRolViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-rolbyorg-detail", kwargs={"pk": self.org2.pk, })
-        self.base_data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab4_org2.pk})
-        self.base_data.update({
-            'mergeaction': 'sustract',
-            'profile': self.profile2_org2.pk,
-        })
-        response = self.client2_org2.put(url, data=json.dumps(self.base_data), content_type='application/json')
+        data = self.base_data
+        data.update({'mergeaction': 'sustract', 'profile': self.profile2_org2.pk})
+        data['contenttypeobj'].update({'org': self.org2.pk, 'objectid': self.lab4_org2.pk})
+        response = self.client2_org2.put(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile2_org2,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['contenttypeobj']['objectid']).first()
+                                              object_id=data['contenttypeobj']['objectid']).first()
         self.assertEqual(response.status_code, 200)
         self.assertTrue(not self.role_manage_lab in pp.rol.all())
-        self.assertTrue(self.org2 in self.user2_org_list)
 
     def test_check_user2_full_rol_to_profile(self):
         """
@@ -91,19 +92,16 @@ class ActionRolViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-rolbyorg-detail", kwargs={"pk": self.org1.pk, })
-        self.base_data['contenttypeobj'].update({'org': self.org1.pk, 'objectid': self.lab1_org1.pk})
-        self.base_data.update({
-            'mergeaction': 'full',
-            'profile': self.profile1_org1.pk,
-        })
-        response = self.client2_org2.put(url, data=json.dumps(self.base_data), content_type='application/json')
+        data = self.base_data
+        data.update({'mergeaction': 'full', 'profile': self.profile1_org1.pk})
+        data['contenttypeobj'].update({'org': self.org1.pk, 'objectid': self.lab1_org1.pk})
+        response = self.client2_org2.put(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile1_org1,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['contenttypeobj']['objectid']).first()
+                                              object_id=data['contenttypeobj']['objectid']).first()
         self.assertEqual(response.status_code, 403)
-        self.assertNotEqual(pp.rol.all().count(), len(self.base_data['rols']))
+        self.assertNotEqual(pp.rol.all().count(), len(data['rols']))
         self.assertFalse(self.role_manage_lab in pp.rol.all())
-        self.assertTrue(self.org1 in self.user2_org_list)
 
     def test_check_user1_full_rol_to_profile(self):
         """
@@ -111,28 +109,26 @@ class ActionRolViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-rolbyorg-detail", kwargs={"pk": self.org1.pk, })
-        self.base_data['contenttypeobj'].update({'org': self.org1.pk, 'objectid': self.lab1_org1.pk})
-        self.base_data.update({
-            'mergeaction': 'full',
-            'profile': self.profile1_org1.pk,
-        })
-        response = self.client1_org1.put(url, data=json.dumps(self.base_data), content_type='application/json')
+        data = self.base_data
+        data.update({'mergeaction': 'full', 'profile': self.profile1_org1.pk})
+        data['contenttypeobj'].update({'org': self.org1.pk, 'objectid': self.lab1_org1.pk})
+        response = self.client1_org1.put(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile1_org1,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['contenttypeobj']['objectid']).first()
+                                              object_id=data['contenttypeobj']['objectid']).first()
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(pp.rol.all().count(), len(self.base_data['rols']))
+        self.assertEqual(pp.rol.all().count(), len(data['rols']))
         self.assertTrue(self.role_manage_lab in pp.rol.all())
-        self.assertTrue(self.org1 in self.user1_org_list)
 
-
-class ProfilePermissionLabViewTest(BaseOrganizatonManageSetUpTest):
+class DeleteProfilePermissionLabViewTest(BaseOrganizatonManageSetUpTest):
 
     def setUp(self):
         super().setUp()
+        self.lab = self.lab1_org1
+        self.org = self.lab.organization
         self.base_data = {
-            'app_label': 'laboratory',
-            'model': 'laboratory',
+            'app_label': self.lab._meta.app_label,
+            'model': self.lab._meta.model_name,
             'disable_user': False
         }
 
@@ -142,18 +138,18 @@ class ProfilePermissionLabViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-deluserorgcontt-list")
-        self.base_data.update({
-            'object_id': self.lab1_org1.pk,
+        data = self.base_data
+        data.update({
+            'object_id': self.lab.pk,
             'profile': self.profile3_org1.pk,
-            'organization': self.org1.pk,
+            'organization': self.org.pk,
         })
-        response = self.client1_org1.delete(url, data=json.dumps(self.base_data), content_type='application/json')
+        response = self.client1_org1.delete(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile3_org1,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['object_id'])
+                                              object_id=data['object_id'])
         self.assertEqual(response.status_code, 200)
         self.assertFalse(pp.exists())
-        self.assertTrue(self.org1 in self.user1_org_list)
 
     def test_user2_delete_profilepermissionslab1profile3(self):
         """
@@ -161,20 +157,61 @@ class ProfilePermissionLabViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-deluserorgcontt-list")
-        self.base_data.update({
-            'object_id': self.lab1_org1.pk,
+        data = self.base_data
+        data.update({
+            'object_id': self.lab.pk,
             'profile': self.profile3_org1.pk,
-            'organization': self.org1.pk,
+            'organization': self.org.pk,
         })
-        response = self.client2_org2.delete(url, data=json.dumps(self.base_data), content_type='application/json')
+        response = self.client2_org2.delete(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile3_org1,
                                               content_type=self.lab_contenttype,
-                                              object_id=self.base_data['object_id'])
+                                              object_id=data['object_id'])
         self.assertEqual(response.status_code, 403)
         self.assertTrue(pp.exists())
-        self.assertFalse(self.org1 in self.user2_org_list)
 
-class ProfilePermissionOrgViewTest(BaseOrganizatonManageSetUpTest):
+class CreateProfilePermissionLabViewTest(BaseOrganizatonManageSetUpTest):
+
+    def setUp(self):
+        super().setUp()
+        self.lab = self.lab2_org2
+        self.org = self.lab.organization
+        self.base_data = {
+            'typeofcontenttype': 'laboratory',
+            'user': self.user4_org2.pk,
+            'laboratory': self.lab.pk,
+            'organization': self.org.pk
+        }
+
+    def test_user1_create_profilepermissionslab2user4(self):
+        """
+        Usuario 1 crea profilepermission lab 2 perfil 4 en una org de la cual NO es miembro, CASO NO PERMITIDO DEBERIA FALLAR
+        """
+
+        url = reverse("auth_and_perms:api-relusertocontenttype-list")
+        data = self.base_data
+        response = self.client1_org1.post(url, data=json.dumps(data), content_type='application/json')
+        pp = ProfilePermission.objects.filter(profile=self.profile4_org2,
+                                              content_type=self.lab_contenttype,
+                                              object_id=data['laboratory'])
+        self.assertEqual(response.status_code, 403)
+        self.assertFalse(pp.exists())
+
+    def test_user2_create_profilepermissionslab2user4(self):
+        """
+        Usuario 1 crea profilepermission lab 2 perfil 4 en una org de la cual SI es miembro, CASO PERMITIDO
+        """
+
+        url = reverse("auth_and_perms:api-relusertocontenttype-list")
+        data = self.base_data
+        response = self.client2_org2.post(url, data=json.dumps(data), content_type='application/json')
+        pp = ProfilePermission.objects.filter(profile=self.profile4_org2,
+                                              content_type=self.lab_contenttype,
+                                              object_id=data['laboratory'])
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(pp.exists())
+
+class DeleteProfilePermissionOrgViewTest(BaseOrganizatonManageSetUpTest):
 
     def setUp(self):
         super().setUp()
@@ -183,9 +220,6 @@ class ProfilePermissionOrgViewTest(BaseOrganizatonManageSetUpTest):
             'model': 'organizationstructure',
             'disable_user': False
         }
-        self.org_contenttype = ContentType.objects.filter(
-            app_label=self.base_data['app_label'],
-            model=self.base_data['model']).first()
 
     def test_user1_delete_profilepermissionsorg1profile3(self):
         """
@@ -193,18 +227,18 @@ class ProfilePermissionOrgViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-deluserorgcontt-list")
-        self.base_data.update({
+        data = self.base_data
+        data.update({
             'object_id': self.org1.pk,
             'profile': self.profile3_org1.pk,
             'organization': self.org1.pk,
         })
-        response = self.client1_org1.delete(url, data=json.dumps(self.base_data), content_type='application/json')
+        response = self.client1_org1.delete(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile3_org1,
                                               content_type=self.org_contenttype,
-                                              object_id=self.base_data['object_id'])
+                                              object_id=data['object_id'])
         self.assertEqual(response.status_code, 200)
         self.assertFalse(pp.exists())
-        self.assertTrue(self.org1 in self.user1_org_list)
 
     def test_user2_delete_profilepermissionsorg1profile3(self):
         """
@@ -212,15 +246,54 @@ class ProfilePermissionOrgViewTest(BaseOrganizatonManageSetUpTest):
         """
 
         url = reverse("auth_and_perms:api-deluserorgcontt-list")
-        self.base_data.update({
+        data = self.base_data
+        data.update({
             'object_id': self.org1.pk,
             'profile': self.profile3_org1.pk,
             'organization': self.org1.pk,
         })
-        response = self.client2_org2.delete(url, data=json.dumps(self.base_data), content_type='application/json')
+        response = self.client2_org2.delete(url, data=json.dumps(data), content_type='application/json')
         pp = ProfilePermission.objects.filter(profile=self.profile3_org1,
                                               content_type=self.org_contenttype,
-                                              object_id=self.base_data['object_id'])
+                                              object_id=data['object_id'])
         self.assertEqual(response.status_code, 403)
         self.assertTrue(pp.exists())
-        self.assertFalse(self.org1 in self.user2_org_list)
+
+class CreateProfilePermissionOrgViewTest(BaseOrganizatonManageSetUpTest):
+
+    def setUp(self):
+        super().setUp()
+        self.org = self.org2
+        self.base_data = {
+            'typeofcontenttype': 'organization',
+            'user': self.user4_org2.pk,
+            'organization': self.org.pk
+        }
+
+    def test_user1_create_profilepermissionsorg2user4(self):
+        """
+        Usuario 1 crea profilepermission org 2 perfil 4 en una org de la cual NO es miembro, CASO NO PERMITIDO DEBERIA FALLAR
+        """
+
+        url = reverse("auth_and_perms:api-relusertocontenttype-list")
+        data = self.base_data
+        response = self.client1_org1.post(url, data=json.dumps(data), content_type='application/json')
+        pp = ProfilePermission.objects.filter(profile=self.profile4_org2,
+                                              content_type=self.org_contenttype,
+                                              object_id=data['organization'])
+        self.assertEqual(response.status_code, 403)
+        self.assertFalse(pp.exists())
+
+    def test_user2_create_profilepermissionsorg2user4(self):
+        """
+        Usuario 2 crea profilepermission org 2 perfil 4 en una org de la cual SI es miembro, CASO PERMITIDO
+        """
+
+        url = reverse("auth_and_perms:api-relusertocontenttype-list")
+        data = self.base_data
+        response = self.client2_org2.post(url, data=json.dumps(data), content_type='application/json')
+        pp = ProfilePermission.objects.filter(profile=self.profile4_org2,
+                                              content_type=self.org_contenttype,
+                                              object_id=data['organization'])
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(pp.exists())
--- src/laboratory/tests/utils.py
+++ src/laboratory/tests/utils.py
@@ -57,42 +57,39 @@ class BaseOrganizatonManageSetUpTest(BaseSetUpTest):
 
     def setUp(self):
         super().setUp()
-        #ORG - USER - PROFILE
+
+        #ORG
         self.org1 = OrganizationStructure.objects.filter(name="Organization 1").first()
         self.org2 = OrganizationStructure.objects.filter(name="Organization 2").first()
+
+        #USER
         self.user1_org1 = get_user_model().objects.filter(username="user1org1").first()
         self.user2_org2 = get_user_model().objects.filter(username="user2org2").first()
         self.user3_org1 = get_user_model().objects.filter(username="user3org1").first()
+        self.user4_org2 = get_user_model().objects.filter(username="user4org2").first()
+
+        #PROFILE
         self.profile1_org1 = self.user1_org1.profile
         self.profile2_org2 = self.user2_org2.profile
         self.profile3_org1 = self.user3_org1.profile
+        self.profile4_org2 = self.user4_org2.profile
 
         #ORGS BY USER
-        self.user1_org_list = OrganizationStructure.os_manager.filter_user_org(self.user1_org1).distinct()
-        self.user2_org_list = OrganizationStructure.os_manager.filter_user_org(self.user2_org2).distinct()
-        self.user3_org_list = OrganizationStructure.os_manager.filter_user_org(self.user3_org1).distinct()
+        self.user1_org1_list = OrganizationStructure.os_manager.filter_user_org(self.user1_org1).distinct()
+        self.user2_org2_list = OrganizationStructure.os_manager.filter_user_org(self.user2_org2).distinct()
+        self.user3_org1_list = OrganizationStructure.os_manager.filter_user_org(self.user3_org1).distinct()
+        self.user4_org2_list = OrganizationStructure.os_manager.filter_user_org(self.user4_org2).distinct()
 
         #LABS
         self.lab1_org1 = Laboratory.objects.get(name="Lab 1")
-        self.lab2_org1 = Laboratory.objects.get(name="Lab 2")
-        self.lab3_org2 = Laboratory.objects.get(name="Lab 3")
+        self.lab2_org2 = Laboratory.objects.get(name="Lab 2")
+        self.lab3_org1 = Laboratory.objects.get(name="Lab 3")
         self.lab4_org2 = Laboratory.objects.get(name="Lab 4")
 
         #INITIAL DATA
         self.role_manage_lab = Rol.objects.get(name="Gestión Laboratorio")
-        self.base_data = {
-            'rols': [self.role_manage_lab.pk],
-            'as_role': True,
-            'as_user': False,         # REMOVE
-            'as_conttentype': False,  # REMOVE
-            'contenttypeobj': {
-                'model': 'laboratory',
-                'appname': 'laboratory'
-            },
-        }
-        self.lab_contenttype = ContentType.objects.filter(
-            app_label=self.base_data['contenttypeobj']['appname'],
-            model=self.base_data['contenttypeobj']['model']).first()
+        self.lab_contenttype = ContentType.objects.filter(app_label='laboratory', model='laboratory').first()
+        self.org_contenttype = ContentType.objects.filter(app_label='laboratory', model='organizationstructure').first()
 
         #CLIENT - LOGIN
         self.client1_org1 = self.client
