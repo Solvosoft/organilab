ff79b5677125a693876e34853c828abb843e3dc5
Kendrick <kenjen041@gmail.com>
2023-04-03 16:54:54 -0600

    fixing routes and filter

--- src/report/forms.py
+++ src/report/forms.py
@@ -33,7 +33,7 @@ class ValidateReportForm(ReportBase):
         organization = self.cleaned_data['organization']
         all_labs_org = self.cleaned_data['all_labs_org']
         laboratory = self.cleaned_data['laboratory']
-
         if all_labs_org:
             laboratory = get_laboratories_from_organization(organization)
-        return laboratory
+
+        return list(laboratory.values_list('pk',flat=True))
