f293f19ebfa96868183118c37c367c69c12a2b45
Kendrick <kenjen041@gmail.com>
2023-04-04 17:04:27 -0600

    changes format to make limit objects and objects reports

--- src/laboratory/register.py
+++ src/laboratory/register.py
@@ -1,8 +1,18 @@
 REPORT_FORMS ={
     'reactive_precursor': {
-        'task':'laboratory.tasks.reactive_precursor',
+        'task':'laboratory.tasks.report_reactive_precursor',
         'form':'report.forms.ValidateReportForm',
         'title':'List of reactive precursors',
-    }
+    },
+    'report_objects': {
+        'task':'laboratory.tasks.report_objects',
+        'form':'report.forms.ReportObjectForm',
+        'title':'List of objects',
+    },
+    'report_limit_objects': {
+        'task': 'laboratory.tasks.report_limit_objects',
+        'form': 'report.forms.ValidateReportForm',
+        'title': 'Limited shelf objects',
+    },
 
 }
