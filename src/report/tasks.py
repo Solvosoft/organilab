1e0ca0d496c1fd3c0b107f8053bdcf284f05bbdb
Kendrick <kenjen041@gmail.com>
2023-04-05 12:32:39 -0600

    move the tasks from laboratory to reports module

--- src/report/tasks.py
+++ src/report/tasks.py
@@ -3,7 +3,11 @@ import importlib
 
 from laboratory.models import TaskReport
 from report.views.lab_room import lab_room_html, lab_room_pdf
-from report.views.objects import report_objectlogchange_html, report_objectlogchange_pdf, report_objectlogchange_doc
+from report.views.objects import report_objectlogchange_html, report_objectlogchange_pdf, report_objectlogchange_doc\
+    ,report_reactive_precursor_html,report_reactive_precursor_pdf,report_reactive_precursor_doc\
+    ,report_objects_html, report_object_doc, report_objects_pdf\
+    ,report_limit_object_html, report_limit_object_pdf,report_limit_object_doc
+
 
 app = importlib.import_module(settings.CELERY_MODULE).app
 
@@ -24,3 +28,32 @@ def object_log_change_report(pk):
         report_objectlogchange_pdf(report)
     else:
         report_objectlogchange_doc(report)
+
+@app.task()
+def report_reactive_precursor(pk):
+    report = TaskReport.objects.filter(pk=pk).first()
+    if report.file_type=='html':
+        report_reactive_precursor_html(report)
+    elif report.file_type=='pdf':
+        report_reactive_precursor_pdf(report)
+    else:
+        report_reactive_precursor_doc(report)
+
+@app.task()
+def report_objects(pk):
+    report = TaskReport.objects.filter(pk=pk).first()
+    if report.file_type=='html':
+        report_objects_html(report)
+    elif report.file_type == 'pdf':
+        report_objects_pdf(report)
+    else:
+        report_object_doc(report)
+@app.task()
+def report_limit_objects(pk):
+    report = TaskReport.objects.filter(pk=pk).first()
+    if report.file_type=='html':
+        report_limit_object_html(report)
+    elif report.file_type == 'pdf':
+        report_limit_object_pdf(report)
+    else:
+        report_limit_object_doc(report)
