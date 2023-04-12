86f7f41f2f3802c3f5fd025800e9503a312b6d33
Marcela González Zúñiga <marcegz31@gmail.com>
2023-04-12 09:58:28 -0600

    Incluyendo reporte en formatos xls, xlsx, ods

--- src/report/views/lab_room.py
+++ src/report/views/lab_room.py
@@ -1,10 +1,11 @@
+from io import BytesIO
+
 from django.core.files.base import ContentFile
 from django.template.loader import render_to_string
-from io import BytesIO
 from django.utils.translation import gettext as _
 from weasyprint import HTML
 
-from laboratory.models import Laboratory, Furniture
+from laboratory.report_utils import ExcelGraphBuilder
 from report.utils import update_table_report
 from report.views.base import get_furniture_queryset_by_filters
 
@@ -37,5 +38,39 @@ def lab_room_pdf(report):
     file.seek(0)
     content = ContentFile(file.getvalue(), name=file_name)
     report.file = content
+    report.save()
+    file.close()
+
+
+def lab_room_doc(report):
+    furniture_list = get_furniture_queryset_by_filters(report)
+    content = []
+    builder = ExcelGraphBuilder()
+    content.append([_("Code"), _("Object"), _("Quantity"), _("Laboratory"), _("Laboratory Room"), _("Furniture"), _("Shelf")])
+
+    for furniture in furniture_list:
+        for shelfobject in furniture.get_objects():
+            shelf_unit = shelfobject.get_measurement_unit_display()
+            furniture = shelfobject.shelf.furniture
+            content.append([
+                shelfobject.object.code,
+                shelfobject.object.name,
+                f'{shelfobject.quantity} {shelf_unit}',
+                shelfobject.in_where_laboratory.name,
+                furniture.labroom.name,
+                furniture.name,
+                shelfobject.shelf.name
+                            ])
+
+
+
+    builder.add_table(content, report.data['title'])
+    file=builder.save()
+    report_name = report.data['name'] if report.data['name'] else 'report'
+    file_name = f'{report_name}.{report.file_type}'
+    file.seek(0)
+    content = ContentFile(file.getvalue(), name=file_name)
+    report.file = content
+    report.status = _('Generated')
     report.save()
     file.close()
