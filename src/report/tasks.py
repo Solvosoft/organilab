77e727b9d2a02330a7bf3938a0e3c88e744631b5
Marcela González Zúñiga <marcegz31@gmail.com>
2023-04-13 16:16:47 -0600

    Try except generic task

--- src/report/tasks.py
+++ src/report/tasks.py
@@ -3,10 +3,14 @@ import importlib
 from django.conf import settings
 
 from report.views.base import build_report
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext as _
 app = importlib.import_module(settings.CELERY_MODULE).app
 
 
 @app.task()
 def task_report(pk, absolute_uri):
-    build_report(pk, absolute_uri)
+    try:
+        build_report(pk, absolute_uri)
+    except Exception:
+        raise ObjectDoesNotExist(_("Object doesn't exists"))
