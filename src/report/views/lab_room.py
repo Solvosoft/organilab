180c8ac114b648cbb90c98e87b5c8d42a56cfe70
Marcela González Zúñiga <marcegz31@gmail.com>
2023-04-14 18:21:02 -0600

    Updating html function with json dataset columns in furniture and lab room report

--- src/report/views/lab_room.py
+++ src/report/views/lab_room.py
@@ -1,51 +1,41 @@
-from io import BytesIO
-
 from django.core.files.base import ContentFile
-from django.template.loader import render_to_string
 from django.utils.translation import gettext as _
-from weasyprint import HTML
 
 from laboratory.report_utils import ExcelGraphBuilder
-from report.utils import update_table_report
+from report.utils import set_format_table_columns
 from report.views.base import get_furniture_queryset_by_filters
 
-
-def lab_room_html(report):
+def get_dataset(report):
+    dataset = []
     furniture_list = get_furniture_queryset_by_filters(report)
-    rows = ""
-    col_list = [_("Code"), _("Object"), _("Quantity"), _("Laboratory"), _("Laboratory Room"), _("Furniture"), _("Shelf")]
     for furniture in furniture_list:
         for shelfobject in furniture.get_objects():
             shelf_unit = shelfobject.get_measurement_unit_display()
             furniture = shelfobject.shelf.furniture
-            rows += f'<tr><td>{shelfobject.object.code}</td><td>{shelfobject.object.name}</td>' \
-                     f'<td>{shelfobject.quantity} {shelf_unit}</td><td>{shelfobject.in_where_laboratory.name}</td>' \
-                     f'<td>{furniture.labroom.name}</td><td>{furniture.name}</td><td>{shelfobject.shelf.name}</td></tr>'
-    update_table_report(report, col_list, rows)
+            dataset.append([
+                shelfobject.object.code, shelfobject.object.name, f'{shelfobject.quantity} {shelf_unit}',
+                shelfobject.in_where_laboratory.name, furniture.labroom.name, furniture.name, shelfobject.shelf.name
+            ])
+    return dataset
+
+def lab_room_html(report):
+    columns_fields = [
+        {'name': 'code', 'title': _("Code")}, {'name': 'object', 'title': _("Object")},
+        {'name': 'quantity', 'title': _("Quantity")}, {'name': 'laboratory', 'title': _("Laboratory")},
+        {'name': 'laboratory_room', 'title': _("Laboratory Room")}, {'name': 'furniture', 'title': _("Furniture")},
+        {'name': 'shelf', 'title': _("Shelf")}
+    ]
+    report.table_content = {
+        'columns': set_format_table_columns(columns_fields),
+        'dataset': get_dataset(report)
+    }
+    report.save()
 
 
 def lab_room_doc(report):
-    furniture_list = get_furniture_queryset_by_filters(report)
-    content = []
     builder = ExcelGraphBuilder()
-    content.append([_("Code"), _("Object"), _("Quantity"), _("Laboratory"), _("Laboratory Room"), _("Furniture"), _("Shelf")])
-
-    for furniture in furniture_list:
-        for shelfobject in furniture.get_objects():
-            shelf_unit = shelfobject.get_measurement_unit_display()
-            furniture = shelfobject.shelf.furniture
-            content.append([
-                shelfobject.object.code,
-                shelfobject.object.name,
-                f'{shelfobject.quantity} {shelf_unit}',
-                shelfobject.in_where_laboratory.name,
-                furniture.labroom.name,
-                furniture.name,
-                shelfobject.shelf.name
-                            ])
-
-
-
+    content = [[_("Code"), _("Object"), _("Quantity"), _("Laboratory"), _("Laboratory Room"), _("Furniture"), _("Shelf")]]
+    content = content + get_dataset(report)
     builder.add_table(content, report.data['title'])
     file=builder.save()
     report_name = report.data['name'] if report.data['name'] else 'report'
@@ -53,6 +43,5 @@ def lab_room_doc(report):
     file.seek(0)
     content = ContentFile(file.getvalue(), name=file_name)
     report.file = content
-    report.status = _('Generated')
     report.save()
     file.close()
