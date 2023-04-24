a1facc21ee7cb17294762b6ef3ee56b4ce5b0715
Kendrick <kenjen041@gmail.com>
2023-04-24 13:24:38 -0600

    add the document report status

--- src/report/models.py
+++ src/report/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from laboratory.models import TaskReport
+
+
 # Create your models here.
+
+class DocumentReportStatus(models.Model):
+    report=models.ForeignKey(TaskReport, on_delete=models.CASCADE)
+    report_time = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=512)
+
+    def __str__(self):
+        return f"{self.report_time} {self.description}"
