c8dfa996b3b067e4a665a8235eb80499ad98d383
Marcela González Zúñiga <marcegz31@gmail.com>
2023-04-25 16:15:15 -0600

    Actualizando e importando gtselects de reportes

--- src/report/gtselects.py
+++ src/report/gtselects.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.http import Http404
 from djgentelella.groute import register_lookups
 from djgentelella.views.select2autocomplete import BaseSelect2View, GPaginator
@@ -6,75 +7,15 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
-from laboratory.models import LaboratoryRoom, OrganizationStructure, Furniture, Laboratory
+from laboratory.models import LaboratoryRoom, OrganizationStructure, Laboratory, Furniture
 from laboratory.utils import get_laboratories_from_organization, get_laboratories_from_organization_profile, \
     get_users_from_organization
-from report.forms import RelOrganizationForm, RelLaboratoryForm, RelOrganizationLaboratoryForm, OrganizationForm
-from django.contrib.auth.models import User
+from report.forms import RelOrganizationForm, RelOrganizationLaboratoryForm, OrganizationForm
+
 
 class GPaginatorMoreElements(GPaginator):
-    page_size = 50
+    page_size = 100
 
-@register_lookups(prefix="labroombase", basename="labroombase")
-class LabRoomModelLookups(generics.RetrieveAPIView, BaseSelect2View):
-    model = LaboratoryRoom
-    fields = ['name']
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-    pagination_class = GPaginatorMoreElements
-    organization, laboratory, all_labs_org = None, None, None
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        result = queryset.none()
-
-        if self.laboratory:
-            result = queryset.filter(laboratory=self.laboratory[0])
-
-        if self.all_labs_org is not None:
-            if self.all_labs_org:
-                labs = get_laboratories_from_organization(self.organization.pk)
-                result = queryset.filter(laboratory__in=labs)
-        return result
-
-    def list(self, request, *args, **kwargs):
-        if self.organization is None:
-            form = RelOrganizationForm(self.request.GET)
-            if form.is_valid():
-                self.organization = get_object_or_404(OrganizationStructure, pk=form.cleaned_data['organization'])
-                self.laboratory = form.cleaned_data['laboratory']
-                self.all_labs_org = form.cleaned_data['all_labs_org']
-        if self.organization is None:
-            raise Http404("Organization not found")
-        return super().list(request, *args, **kwargs)
-
-
-@register_lookups(prefix="furniturebase", basename="furniturebase")
-class FurnitureModelLookups(generics.RetrieveAPIView, BaseSelect2View):
-    model = Furniture
-    fields = ['name']
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-    pagination_class = GPaginatorMoreElements
-    laboratory = None
-
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        result = queryset.none()
-        if self.laboratory:
-            lab_room = LaboratoryRoom.objects.filter(laboratory__in=self.laboratory)
-            result = queryset.filter(labroom__in=lab_room)
-        return result
-
-    def list(self, request, *args, **kwargs):
-        if self.laboratory is None:
-            form = RelLaboratoryForm(self.request.GET)
-            if form.is_valid():
-                self.laboratory = form.cleaned_data['laboratory']
-        if self.laboratory is None:
-            raise Http404("Laboratory not found")
-        return super().list(request, *args, **kwargs)
 
 @register_lookups(prefix="laboratorybase", basename="laboratorybase")
 class LaboratoryModelLookups(BaseSelect2View):
@@ -132,3 +73,52 @@ class UserModelLookups(BaseSelect2View):
         if self.organization is None:
             raise Http404("Organization not found")
         return super().list(request, *args, **kwargs)
+
+
+@register_lookups(prefix="lab_room", basename="lab_room")
+class LabRoomLookup(generics.RetrieveAPIView, BaseSelect2View):
+    model = LaboratoryRoom
+    fields = ['name']
+    ordering = ['name']
+    pagination_class = GPaginatorMoreElements
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    organization, laboratory, lab_room, all_labs_org = None, None, None, False
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        result = queryset.none()
+        if self.organization:
+            if self.all_labs_org:
+                labs_by_org = get_laboratories_from_organization(self.organization)
+                result = LaboratoryRoom.objects.filter(laboratory__in=labs_by_org)
+            else:
+                if self.laboratory:
+                    result = queryset.filter(laboratory=self.laboratory)
+
+            if self.lab_room:
+                id_list = list(self.lab_room.values_list('pk', flat=True))
+                self.selected = list(map(str, id_list))
+        return result
+
+    def list(self, request, *args, **kwargs):
+        if self.organization is None:
+            form = RelOrganizationForm(request.GET)
+            if form.is_valid():
+                self.organization = form.cleaned_data['organization']
+                self.laboratory = form.cleaned_data['laboratory']
+                self.all_labs_org = form.cleaned_data['all_labs_org']
+                self.lab_room = form.cleaned_data['lab_room']
+        if self.organization is None:
+            raise Http404("Organization not found")
+        return super().list(request, *args, **kwargs)
+
+@register_lookups(prefix="furniture", basename="furniture")
+class FurnitureLookup(generics.RetrieveAPIView, BaseSelect2View):
+    model = Furniture
+    fields = ['name']
+    ref_field = 'labroom'
+    ordering = ['name']
+    pagination_class = GPaginatorMoreElements
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
