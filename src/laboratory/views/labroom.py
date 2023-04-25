d2f63d87192ab6b24a13a8477dc9f819f143597f
Marcela González Zúñiga <marcegz31@gmail.com>
2023-04-25 16:16:50 -0600

    Personalizando selección de filtros y actualización de campos en formulario

--- src/laboratory/views/labroom.py
+++ src/laboratory/views/labroom.py
@@ -139,15 +139,18 @@ class LaboratoryRoomReportView(ListView):
                         self).get_context_data(**kwargs)
         lab_obj = get_object_or_404(Laboratory, pk=self.lab)
         title = _("Objects by Laboratory Room Report")
-        context['title_view'] = title
-        context['report_name'] = 'report_laboratory_room'
-        context['report_urlnames'] = ['reports_laboratory', 'report_building']
-        context['form'] = LaboratoryRoomReportForm(initial={
-            'name': title +' '+ now().strftime("%x").replace('/', '-'),
-            'title': title,
-            'organization': self.org,
+        context.update({
+            'title_view': title,
             'report_name': 'report_laboratory_room',
-            'laboratory': lab_obj,
+            'report_urlnames': ['reports_laboratory', 'report_building'],
+            'form': LaboratoryRoomReportForm(initial={
+                'name': title +' '+ now().strftime("%x").replace('/', '-'),
+                'title': title,
+                'organization': self.org,
+                'report_name': 'report_laboratory_room',
+                'laboratory': lab_obj,
+                'all_labs_org': False
+            })
         })
         return context
 
