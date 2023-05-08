6b948e6b78ba9ae75df07edec0a4ae86ed944a0a
Kendrick <kenjen041@gmail.com>
2023-05-08 11:03:17 -0600

    Validate media access to download reports depend the user

--- src/auth_and_perms/views/__init__.py
+++ src/auth_and_perms/views/__init__.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.http import HttpResponseForbidden
+from report.models import TaskReport
 
 def media_access(request, path):
     """
@@ -12,10 +13,23 @@ def media_access(request, path):
     This special URL will be handle by nginx we the help of X-Accel
     """
     user = request.user
+
+    real_path=path
+
+    report=TaskReport.objects.filter(creator=user, file_type__isnull=False, file__isnull=False, file__icontains=path).exclude(file_type="html").first()
+
     if user.is_authenticated:
         response = HttpResponse()
         # Content-type will be detected by nginx
-        del response['Content-Type']
-        response['X-Accel-Redirect'] = '/protected/' + path
-        return response
+        if path.startswith("report"):
+            if report:
+                del response['Content-Type']
+                response['X-Accel-Redirect'] = '/protected/' + path
+                return response
+            else:
+                return HttpResponseForbidden('Not authorized to access this media.')
+        else:
+            del response['Content-Type']
+            response['X-Accel-Redirect'] = '/protected/' + path
+            return response
     return HttpResponseForbidden('Not authorized to access this media.')
