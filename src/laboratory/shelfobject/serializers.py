43f6bdef91ec87e987c7db73351c60ba0cc0ba8d
Kendrick <kenjen041@gmail.com>
2023-05-20 13:34:58 -0600

    Agregada api de update_status

--- src/laboratory/shelfobject/serializers.py
+++ src/laboratory/shelfobject/serializers.py
@@ -431,3 +431,15 @@ class TransferObjectDenySerializer(serializers.Serializer):
                          f'!= laboratory_id ({self.context.get("laboratory_id")})')
             raise serializers.ValidationError(_("Transfer was not sent to the laboratory."))
         return attr
+
+class UpdateShelfObjectStatusSerializer(serializers.Serializer):
+    shelfobject = serializers.PrimaryKeyRelatedField(many=False, queryset=ShelfObject.objects.using(settings.READONLY_DATABASE), required=True)
+    status = serializers.PrimaryKeyRelatedField(many=False, queryset=Catalog.objects.filter(key='shelfobject_status'), required=True)
+
+    def validate_shelfobject(self,value):
+        attr = super().validate(value)
+        if attr.in_where_laboratory_id != self.context.get('laboratory_id'):
+            logger.debug(f'UpdateShelfObjectStatusSerializer --> attr.in_where_laboratory_id ({attr.in_where_laboratory_id}) '
+                         f'!= laboratory_id ({self.context.get("laboratory_id")})')
+            raise serializers.ValidationError(_("Object does not exist in the laboratory."))
+        return attr
