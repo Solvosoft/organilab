8ea97abc84cfff08b3265cbbdd8bfb64e1f7fb56
Daniel Loaiza <danieloaiza@live.com>
2023-05-23 10:28:05 -0600

    Code Refactoring for Detail Modal Creation

--- src/laboratory/static/laboratory/js/detail_modal_creation.js
+++ src/laboratory/static/laboratory/js/detail_modal_creation.js
@@ -16,12 +16,42 @@ function configure_modal(data){
     if(data['qr'] !== undefined){
         insert_qr(data['qr'], data['url'])
     }
-    insert_object_data(data.object, tbody_instance)
-    load_features(data.object['object_features'], tbody_instance)
+    let object_titles = {
+        'code': gettext('Code'),
+        'name': gettext('Name'),
+        'synonym': gettext('Synonym'),
+        'description': gettext('Description'),
+        'model': gettext('Model'),
+        'serie': gettext('Serie'),
+        'plaque': gettext('Plaque')
+    }
+    insert_substance_data(data.object.object_inst, tbody_instance, object_titles)
+    insert_substance_data(data.object, tbody_instance, {'unit': gettext('Unit')})
+    append_data_lists(data.object, {'object_features': gettext('Features')}, tbody_instance)
     if (data.object['substance_characteristics']){
         manage_substance_characteristics(data.object['substance_characteristics'], tbody_instance)
     }
 }
+
+/*
+Method that inserts the QR Code in the modal, if there is one available
+I: qr (b64 image), url(string)
+O: None
+*/
+function insert_qr(qr, url){
+    let qr_container = `<div class="form-group row qr_img">
+        <img src="data:image/svg+xml;base64,${qr}" alt="${gettext('Download QR')}">
+        </div>
+        <div class="form-group row justify-content-center">
+            <div class="col-5 text-center" style="margin-bottom: 5">
+                <a href="${url}" class="btn btn-success">
+                <i class="fa fa-download" ></i>
+                ${gettext('Download QR')}</a>
+            </div>
+        </div>`
+        $('#shelfobject_detail_modal_body').prepend(qr_container)
+}
+
 /*
 Method that creates the substance characteristics
 I: data (JSON), inst(Instance of HTML to append to)
@@ -29,9 +59,15 @@ O: None
 */
 function manage_substance_characteristics(data, inst){
     let characteristics = define_usable_keys_detail(data)
+    let characteristics_lists = {
+        'white_organ': gettext('White Organs'),
+        'h_code': gettext('H Codes'),
+        'ue_code': gettext('UE Codes'),
+        'nfpa': gettext('NFPA Codes')
+    }
     data = change_boolean_to_affirmation(data)
     insert_substance_data(data, inst, characteristics)
-    append_characteristics_lists(data, inst)
+    append_data_lists(data, characteristics_lists, inst)
     if (data['security_sheet']){
         let security_html = `<tr><td class="shelfobject_titles">${gettext('Security Sheet')}</td>
             <td><a href="${data['security_sheet']}" target="_blank"> ${gettext('Download')}</a></td></tr>`
@@ -68,28 +104,6 @@ function define_usable_keys_detail(data){
 }
 
 /*
-Method that creates the iterable data if there is data to show
-I: data (JSON), inst(Instance of HTML to append to)
-O: None
-*/
-
-function append_characteristics_lists(data, inst){
-    let characteristics_lists = {
-        'white_organ': gettext('White Organs'),
-        'h_code': gettext('H Codes'),
-        'ue_code': gettext('UE Codes'),
-        'nfpa': gettext('NFPA Codes')
-    }
-    titles = Object.keys(characteristics_lists)
-    titles.forEach( (title) => {
-        if (data[title] > 0){
-            let temp_html = load_array_data(data[title], characteristics_lists[title])
-            inst.append(temp_html)
-        }
-    })
-}
-
-/*
 Method that changes the boolean value of some data to 'Yes' or 'No'
 I: data (JSON)
 O: data (JSON)
@@ -105,65 +119,18 @@ function change_boolean_to_affirmation(data){
 }
 
 /*
-Method that creates the features if there is any to show
+Method that creates the iterable data if there is data to show
 I: data (JSON), inst(Instance of HTML to append to)
 O: None
 */
-function load_features(features, inst){
-    let html_object = ""
-    if (features.length > 0){
-        for (feature of features){
-            html_object += `<li> ${feature.name}</li>`
-        }
-        let features_html = `<tr><td class="shelfobject_titles">${gettext('Features')}</td><td>
-            <ul class="shelfobjectfeatures shelfobject_list_elem">${html_object}</ul></td></tr>`
-        inst.append(features_html)
-    }
-}
-
-/*
-Method that inserts the QR Code in the modal, if there is one available
-I: qr (b64 image), url(string)
-O: None
-*/
-function insert_qr(qr, url){
-    let qr_container = `<div class="form-group row qr_img">
-        <img src="data:image/svg+xml;base64,${qr}" alt="${gettext('Download QR')}">
-        </div>
-        <div class="form-group row justify-content-center">
-            <div class="col-5 text-center" style="margin-bottom: 5">
-                <a href="${url}" class="btn btn-success">
-                <i class="fa fa-download" ></i>
-                ${gettext('Download QR')}</a>
-            </div>
-        </div>`
-        $('#shelfobject_detail_modal_body').prepend(qr_container)
-}
-
-/*
-Method that inserts all the characteristics inside characteristics which contains the usable keys
-I: data (JSON), inst(Instance of HTML to append to)
-O: None
-*/
-function insert_object_data(data, inst){
-    let html_object = ''
-    let object_titles = {
-        'code': gettext('Code'),
-        'name': gettext('Name'),
-        'synonym': gettext('Synonym'),
-        'description': gettext('Description'),
-        'unit': gettext('Unit'),
-        'model': gettext('Model'),
-        'serie': gettext('Serie'),
-        'plaque': gettext('Plaque')
-    }
-    titles = Object.keys(object_titles)
+function append_data_lists(data, data_lists, inst, nested_key){
+    console.log(data_lists)
+    titles = Object.keys(data_lists)
     titles.forEach( (title) => {
-        let value = title == 'unit' ? data[title] : data.object_inst[title]
-        html_object += `<tr><td class="shelfobject_titles">${object_titles[title]}</td>
-        <td> ${value ? value : ""} </td></tr>`
+        if (data[title].length > 0){
+            load_array_data(data[title], data_lists[title], inst, nested_key)
+        }
     })
-    inst.prepend(html_object)
 }
 
 /*
@@ -190,11 +157,17 @@ Method that inserts all the substance characteristics that are arrays
 I: array_list (Array), title(String)
 O: None
 */
-function load_array_data(array_list, title){
+function load_array_data(array_list, title, inst){
     let html_object = ""
-    array_list.forEach((item) => html_object += `<li> ${item}</li>`)
+    let nested_key = Object.keys(array_list[0])[0]
+    array_list.forEach((item) => {
+        let value = item
+        if (nested_key){
+            value = value[nested_key]
+        }
+        html_object += `<li>${value}</li>`
+    })
     let html_section = `<tr><td class="shelfobject_titles">${title}</td><td>
         <ul class="shelfobject_list_elem">${html_object}</ul></td></tr>`
-    return html_section
-
+    inst.append(html_section)
 }
