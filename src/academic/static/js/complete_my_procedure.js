e7090750118cef7608277f360f56aa7ef85aa320
AlpGregory <gregory.alce2001@gmail.com>
2023-05-24 18:37:36 -0600

    Refactoring code

--- src/academic/static/js/complete_my_procedure.js
+++ src/academic/static/js/complete_my_procedure.js
@@ -15,13 +15,13 @@ formmodal.addBtnForm = function(instance) {
                 "X-CSRFToken": getCookie("csrftoken"),
             },
             success: (success) => {
-                datatableelement.ajax.reload();
                 instance.hidemodal();
                 Swal.fire({
                     icon: 'success',
                     title: gettext('Success'),
                     text: add_translation['successfull'],
-                    timer: 1500
+                }).then(function() {
+                    datatableelement.ajax.reload();
                 });
             },
         });
@@ -43,3 +43,187 @@ function add_comment() {
         formmodal.showmodal();
     }
 }
+
+function saveForm(state) {
+   Swal.fire({
+        title: message['title'],
+        text: message['text'],
+        icon: message['icon'],
+        confirmButtonText: message['confirm'],
+        denyButtonText: message['deny'],
+        showDenyButton: true,
+        showCloseButton: true
+
+        }).then((result) => {
+
+        if (result.isConfirmed) {
+            $.ajax({
+                url: urls['edit'],
+                type: "POST",
+                dataType: "json",
+                data: $('#procedure_form').serialize()+"&status="+state,
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                    "X-CSRFToken": getCookie("csrftoken"),
+                },
+                success: (success) => {
+                    Swal.fire(
+                        "",
+                        saved,
+                        "success"
+                        )
+                    location.href =urls['list'];
+                },
+            });
+
+        } else if (result.isDenied){
+        }
+        })
+}
+
+function Delete_Inform(id) {
+    Swal.fire({
+        title: message['title'],
+        text: message['text'],
+        icon: message['icon'],
+        confirmButtonText: message['confirm'],
+        denyButtonText: message['deny'],
+        showDenyButton: true,
+        showCloseButton: true
+
+        }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: urls['edit'],
+                type: "POST",
+                dataType: "json",
+                data: {'id':id},
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                    "X-CSRFToken": getCookie("csrftoken"),
+                },
+                success: (success) => {
+                    Swal.fire(
+                        '',
+                        saved,
+                        'success'
+                        )
+                    location.href =urls['list'];
+                },
+            });
+
+        } else if (result.isDenied){
+        }
+        })
+}
+
+function edit_comment(pk){
+    localStorage.setItem('comment', pk);
+    let url = urls['get_comment']+pk;
+    $.ajax({
+    url: url,
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRFToken": getCookie("csrftoken"),
+        },
+        success: (success) => {
+            Swal.fire({
+                title: update_translation['title'],
+                input: 'textarea',
+                confirmButtonText: update_translation['accept'],
+                denyButtonText: update_translation['deny'],
+                showDenyButton: true,
+                showCloseButton: true,
+                inputValue:success.comment
+            }).then(function(result) {
+                if (result.isConfirmed) {
+                    update_comment(result.value)
+                }
+            })
+        }
+    });
+}
+
+function update_comment(comment){
+     let url = urls['delete_update_comment'].replace('0',localStorage.getItem('comment'));
+     localStorage.removeItem('comment');
+     $.ajax({
+            url: url,
+            type: "PUT",
+            dataType: "json",
+            data: {'comment':comment},
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRFToken": getCookie("csrftoken"),
+            },
+            success: (success) => {
+                Swal.fire(
+                    '',
+                    update_translation['successfull'],
+                    'success'
+                ).then(function(result) {
+                    datatableelement.ajax.reload();
+                })
+            },
+        });
+}
+function delete_comment(comment){
+    let url = urls['delete_update_comment'].replace('0',comment);
+    Swal.fire({
+    title: remove_translation["title"],
+    text: remove_translation['text'],
+    icon: "error",
+    confirmButtonText: remove_translation['accept'],
+    denyButtonText: remove_translation['deny'],
+    showDenyButton: true,
+    showCloseButton: true
+    }).then((result) => {
+    if (result.isConfirmed) {
+        $.ajax({
+            url: url,
+            type: "DELETE",
+            dataType: "json",
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+                "X-CSRFToken": getCookie("csrftoken"),
+            },
+            success: (success) => {
+                Swal.fire(
+                   remove_translation['successfull'],
+                    ).then(function(result) {
+                    datatableelement.ajax.reload();
+                })
+            },
+        });
+    }
+    });
+}
+
+$('#datatableelement tbody').on( 'click', 'i', function () {
+
+    var action = this.className;
+    var data = datatableelement.row( $(this).parents('tr') ).data();
+
+    if (action==="fa fa-edit beditbtn") {
+        edit_comment(data.id);
+    } else if (action=="fa fa-trash deletebtn") {
+        delete_comment(data.id);
+    }
+
+});
+
+$(document).ready(function() {
+    $('.dataTables_filter').addClass('w-100');
+    $('.dataTables_filter input').addClass('filter-input').css('width', '96%');
+    $('.dataTables_filter label').addClass('filter-label').css('width', '100%');
+    $('.dataTables_paginate').removeClass('paging_full_numbers');
+    $('.dataTables_paginate').addClass('paging_simple_numbers');
+    $('#notificationdatatable').removeClass('dtr-inline');
+});
+
+
+$('.stepradio').on('change', function(e) {
+    datatableelement.ajax.reload();
+});
