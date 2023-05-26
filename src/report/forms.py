7581973c521cc86df69f579cde108f38654b0f2b
Marcela González Zúñiga <marcegz31@gmail.com>
2023-05-26 11:09:41 -0600

    Removiendo formulario de validación no necesario

--- src/report/forms.py
+++ src/report/forms.py
@@ -70,12 +70,6 @@ class ReportObjectForm(ReportObjectsBaseForm):
 
         return list(laboratory.values_list('pk',flat=True))
 
-class RelOrganizationForm(GTForm):
-    organization = forms.IntegerField()
-    laboratory = forms.IntegerField()
-    lab_room = forms.ModelMultipleChoiceField(queryset=LaboratoryRoom.objects.all(), required=False)
-    all_labs_org = forms.BooleanField(required=False)
-
 class LaboratoryRoomReportForm(ReportBase):
     all_labs_org = forms.BooleanField(help_text=_("This option allows to expand this query to all laboratories of current organization"),
         widget=genwidgets.YesNoInput, label=_("All laboratories"), required=False)
