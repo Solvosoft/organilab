ae1e2ec4a43cddf14c1657962626ccbb55e2ae53
Marcela González Zúñiga <marcegz31@gmail.com>
2023-05-29 15:20:35 -0600

    Agregando observaciones del objeto al incrementar y decrementar

--- src/laboratory/shelfobject/utils.py
+++ src/laboratory/shelfobject/utils.py
@@ -1,6 +1,8 @@
 from django.contrib.admin.models import CHANGE
 from laboratory.logsustances import log_object_add_change, log_object_change
+from laboratory.models import ShelfObjectObservation
 from laboratory.utils import organilab_logentry
+from django.utils.translation import gettext_lazy as _
 
 
 def save_increase_decrease_shelf_object(user, validated_data, laboratory, is_increase_process=False):
@@ -15,10 +17,13 @@ def save_increase_decrease_shelf_object(user, validated_data, laboratory, is_inc
 
     old = shelfobject.quantity
     new = old - amount
+    action_taken = _("Object was decreased")
 
     if is_increase_process:
         new = old + amount
+        action_taken = _("Object was increased")
         log_object_add_change(user, laboratory.pk, shelfobject, old, new, "Add", provider, bill, create=False)
+
     else:
         log_object_change(user, laboratory.pk, shelfobject, old, new, description, 2, "Substract", create=False)
 
@@ -26,4 +31,8 @@ def save_increase_decrease_shelf_object(user, validated_data, laboratory, is_inc
     shelfobject.save()
 
     changed_data = list(validated_data.keys())
-    organilab_logentry(user, shelfobject, CHANGE, 'shelfobject', changed_data=changed_data, relobj=[laboratory, shelfobject])
+    organilab_logentry(user, shelfobject, CHANGE, 'shelfobject', changed_data=changed_data,relobj=[laboratory, shelfobject])
+
+    if not description:
+        description = _("Current available objects: %(amount)d") % {'amount': new}
+    ShelfObjectObservation.objects.create(action_taken=action_taken, description=description, shelf_object=shelfobject, creator=user)
