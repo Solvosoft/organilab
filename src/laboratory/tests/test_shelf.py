285545c91fa2a83802e4f3953ed5f85a9fbd4bae
Marcela González Zúñiga <marcegz31@gmail.com>
2023-05-31 16:56:47 -0600

    Pruebas de disponibilidad de la información de un estante

--- src/laboratory/tests/test_shelf.py
+++ src/laboratory/tests/test_shelf.py
@@ -1,8 +1,11 @@
 from django.urls import reverse
 
 from laboratory.models import Shelf
-from laboratory.tests.utils import BaseLaboratorySetUpTest
+from laboratory.tests.utils import BaseLaboratorySetUpTest, ShelfObjectSetUp
 import json
+
+from laboratory.utils import check_user_access_kwargs_org_lab, get_laboratories_by_user_profile
+
 
 class ShelfViewTest(BaseLaboratorySetUpTest):
 
@@ -65,3 +68,57 @@ class ShelfViewTest(BaseLaboratorySetUpTest):
         url = reverse("laboratory:add_shelf_type_catalog")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+
+class ShelfAvailabilityInformationViewTest(ShelfObjectSetUp):
+
+    def test_shelf_availability_information_user_with_permissions(self):
+        """
+        #EXPECTED CASE
+
+        This test does an availability information shelf request by get method and action 'shelf_availability_information'
+        located in laboratory/api/shelfobject.py --> ShelfObjectViewSet generic view set class.
+
+        CHECK TESTS
+        1) Check response status code equal to 200.
+        2) Check if user has access permission to request and view this shelf information.
+        3) Check if shelf name is equal to returned name in response content.
+        4) Check if pk laboratory related to this shelf is equal to declared pk laboratory in url.
+        """
+
+        shelf = Shelf.objects.get(pk=1)
+        url = reverse("laboratory:api-shelfobject-shelf-availability-information", kwargs={"org_pk": self.org1.pk, "lab_pk": self.lab1_org1.pk})
+        data = {
+            "shelf": shelf.pk
+        }
+        response = self.client1_org1.get(url, data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(check_user_access_kwargs_org_lab(self.org1.pk, self.lab1_org1.pk, self.user1_org1))
+        self.assertIn(shelf.name, json.loads(response.content)['name'])
+        self.assertEqual(shelf.furniture.labroom.laboratory.pk, self.lab1_org1.pk)
+
+
+    def test_get_shelf_availability_information_user_without_permissions(self):
+        """
+        #UNEXPECTED CASE, BUT POSSIBLE(User to other organization try to get shelf information)
+
+        This test does an availability information shelf request by get method and action 'shelf_availability_information'
+        located in laboratory/api/shelfobject.py --> ShelfObjectViewSet generic view set class.
+
+        CHECK TESTS
+        1) Check response status code equal to 403.
+        2) Check if user has access permission to request and view this shelf information.
+        3) Check if response content return an error detail.
+        4) Check if pk laboratory related to this shelf is in laboratories list by user.
+        """
+        shelf = Shelf.objects.get(pk=1)
+        url = reverse("laboratory:api-shelfobject-shelf-availability-information",
+                      kwargs={"org_pk": self.org1.pk, "lab_pk": self.lab1_org1.pk})
+        data = {
+            "shelf": shelf.pk
+        }
+        response = self.client2_org2.get(url, data=data)
+        self.assertEqual(response.status_code, 403)
+        self.assertFalse(check_user_access_kwargs_org_lab(self.org1.pk, self.lab1_org1.pk, self.user2_org2))
+        self.assertIn('detail', json.loads(response.content))
+        self.assertNotIn(shelf.furniture.labroom.laboratory.pk, get_laboratories_by_user_profile(self.user2_org2, self.org1.pk))
