63b5bfbadcbe60d0253dadbe8b7b21557695793e
Marcela González Zúñiga <marcegz31@gmail.com>
2023-06-07 14:01:46 -0600

    Updating setup function from decrease and move shelfobject tests

--- src/laboratory/tests/shelfobject/test_shelfobject_decrease.py
+++ src/laboratory/tests/shelfobject/test_shelfobject_decrease.py
@@ -14,6 +14,11 @@ class ShelfObjectDecreaseViewTest(ShelfObjectSetUp):
         super().setUp()
         self.shelf_object = ShelfObject.objects.get(pk=1)
         self.old_quantity = self.shelf_object.quantity
+        self.data = {
+            "amount": 2,
+            "description": "Caso de estudio",
+            "shelf_object": self.shelf_object.pk
+        }
         self.url = reverse("laboratory:api-shelfobject-fill-decrease-shelfobject", kwargs={"org_pk": self.org1.pk, "lab_pk": self.lab1_org1.pk})
 
     def test_shelfobject_decrease_case1(self):
@@ -26,18 +31,13 @@ class ShelfObjectDecreaseViewTest(ShelfObjectSetUp):
         3) Check if new quantity is equal to (old quantity - amount)
         4) Check if new quantity is not equal to old quantity
         """
-        data = {
-            "amount": 2,
-            "description": "Caso de estudio",
-            "shelf_object": self.shelf_object.pk
-        }
-        response = self.client1_org1.post(self.url, data=data)
+        response = self.client1_org1.post(self.url, data=self.data)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(check_user_access_kwargs_org_lab(self.org1.pk, self.lab1_org1.pk, self.user1_org1))
 
-        shelf_object = ShelfObject.objects.get(pk=data["shelf_object"])
+        shelf_object = ShelfObject.objects.get(pk=self.data["shelf_object"])
         new_quantity = shelf_object.quantity
-        self.assertEqual(new_quantity, self.old_quantity - data["amount"])
+        self.assertEqual(new_quantity, self.old_quantity - self.data["amount"])
         self.assertNotEqual(new_quantity, self.old_quantity)
 
     def test_shelfobject_decrease_case2(self):
@@ -50,18 +50,11 @@ class ShelfObjectDecreaseViewTest(ShelfObjectSetUp):
         3) Check if new quantity is not equal to (old quantity - amount)
         4) Check if new quantity is equal to old quantity
         """
-        url = reverse("laboratory:api-shelfobject-fill-decrease-shelfobject",
-                      kwargs={"org_pk": self.org1.pk, "lab_pk": self.lab1_org1.pk})
-        data = {
-            "amount": 2,
-            "description": "Caso de estudio",
-            "shelf_object": self.shelf_object.pk
-        }
-        response = self.client2_org2.post(url, data=data)
+        response = self.client2_org2.post(self.url, data=self.data)
         self.assertEqual(response.status_code, 403)
         self.assertFalse(check_user_access_kwargs_org_lab(self.org1.pk, self.lab1_org1.pk, self.user2_org2))
 
-        shelf_object = ShelfObject.objects.get(pk=data["shelf_object"])
+        shelf_object = ShelfObject.objects.get(pk=self.data["shelf_object"])
         new_quantity = shelf_object.quantity
-        self.assertNotEqual(new_quantity, self.old_quantity - data["amount"])
+        self.assertNotEqual(new_quantity, self.old_quantity - self.data["amount"])
         self.assertEqual(new_quantity, self.old_quantity)
