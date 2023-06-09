71ddf1d2dc228edd44686642548bd603aa8abd28
Marcela González Zúñiga <marcegz31@gmail.com>
2023-06-09 14:30:54 -0600

    Agrega prefijos a formulario de reserva, incremento y decremento del objeto en el estante, además actualiza texto en el mensaje de éxito para el usuario

--- src/laboratory/api/shelfobject.py
+++ src/laboratory/api/shelfobject.py
@@ -383,7 +383,7 @@ class ShelfObjectViewSet(viewsets.GenericViewSet):
         if errors:
             return JsonResponse({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        return JsonResponse({"detail": _("Shelf object substract was performed successfully.")},
+        return JsonResponse({"detail": _("Shelf object was increased successfully.")},
                             status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['post'])
@@ -412,7 +412,7 @@ class ShelfObjectViewSet(viewsets.GenericViewSet):
         if errors:
             return JsonResponse({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
-        return JsonResponse({"detail": _("Shelf object substract was performed successfully.")}, status=status.HTTP_200_OK)
+        return JsonResponse({"detail": _("Shelf object was decrease successfully.")}, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['post'])
     def reserve(self, request, org_pk, lab_pk, **kwargs):
