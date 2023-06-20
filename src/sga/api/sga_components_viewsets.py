e54106fcaf0b973bef497c987358aa2730e6caff
AlpGregory <gregory.alce2001@gmail.com>
2023-06-20 17:56:07 -0600

    Create PrudenceAdvice viewsets

--- src/sga/api/sga_components_viewsets.py
+++ src/sga/api/sga_components_viewsets.py
@@ -296,3 +296,140 @@ class DangerIndicationAPI(mixins.ListModelMixin, viewsets.GenericViewSet):
             return Response({'data': template}, status=status.HTTP_200_OK)
 
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class PrudenceAdviceTableView(mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    serializer_class = PrudenceAdviceDataTableSerializer
+    queryset = PrudenceAdvice.objects.all()
+    pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['code', 'name', 'prudence_advice_help']
+    filterset_fields = ['code', 'name', 'prudence_advice_help']
+    ordering_fields = ['code']
+
+    def list(self, request, *args, **kwargs):
+        recordsTotal = self.get_queryset().count()
+        queryset = self.filter_queryset(self.get_queryset())
+        data = self.paginate_queryset(queryset)
+        response = {'data': data,
+                    'recordsTotal': recordsTotal,
+                    'recordsFiltered': queryset.count(),
+                    'draw': self.request.GET.get('draw', 1)}
+
+        return Response(self.get_serializer(response).data)
+
+
+class PrudenceAdviceAPI(mixins.ListModelMixin, viewsets.GenericViewSet):
+    authentication_classes = [SessionAuthentication, BaseAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = PrudenceAdvice.objects.all()
+    serializer_class = PrudenceAdviceSerializer
+    permissions_by_endpoint = {}
+
+    def _check_permission_on_organization(self, request, org_pk, method_name):
+        if request.user.has_perms(self.permissions_by_endpoint[method_name]):
+            self.organization = get_object_or_404(
+                OrganizationStructure.objects.using(settings.READONLY_DATABASE),
+                pk=org_pk
+            )
+            user_is_allowed_on_organization(request.user, self.organization)
+        else:
+            raise PermissionDenied()
+
+    @action(detail=False, methods=['post'])
+    def add_prudence_advice(self, request, org_pk):
+        self._check_permission_on_organization(request, org_pk, 'add_prudence_advice')
+        organization = get_object_or_404(
+            OrganizationStructure.objects.using(settings.READONLY_DATABASE),
+            pk=org_pk
+        )
+        serializer = PrudenceAdviceSerializer(data=request.data)
+
+        if serializer.is_valid():
+            prudence_advice = PrudenceAdvice.objects.create(
+                code=serializer.data['code'],
+                name=serializer.data['name'],
+                prudence_advice_help=serializer.data['prudence_advice_help']
+            )
+            prudence_advice.save()
+            organilab_logentry(request.user, prudence_advice, ADDITION,
+                               'prudenceadvice', relobj=[organization])
+
+            prudence_advices = self.get_queryset().order_by('code')
+            template = render_to_string('sga/prudence_advice.html',
+                                        {'prudence_advices': prudence_advices},
+                                        request)
+
+            return Response({'data': template}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'])
+    def list_prudence_advices(self, request, org_pk):
+        self._check_permission_on_organization(request, org_pk, 'list_prudence_advices')
+        prudence_advices = self.get_queryset().order_by('code')
+        template = render_to_string('sga/prudence_advice.html',
+                                    {'prudence_advices': prudence_advices},
+                                    request)
+
+        return Response({'data': template})
+
+    @action(detail=True, methods=['put'])
+    def update_prudence_advice(self, request, org_pk, pk=None):
+        self._check_permission_on_organization(request, org_pk, 'update_prudence_advice')
+        organization = get_object_or_404(
+            OrganizationStructure.objects.using(settings.READONLY_DATABASE),
+            pk=org_pk
+        )
+        prudence_advice = None
+
+        if pk:
+            serializer = PrudenceAdviceSerializer(data=request.data)
+
+            if serializer.is_valid():
+                prudence_advice = get_object_or_404(PrudenceAdvice, pk=pk)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            if prudence_advice:
+                prudence_advice.code = request.data['code']
+                prudence_advice.name = request.data['name']
+                prudence_advice.prudence_advice_help = request.data['prudence_advice_help']
+                prudence_advice.save()
+                organilab_logentry(request.user, prudence_advice, CHANGE,
+                                   'prudenceadvice', relobj=[organization])
+
+                prudence_advices = self.get_queryset().order_by('code')
+
+                template = render_to_string('sga/prudence_advice.html',
+                                            {'prudence_advices': prudence_advices},
+                                            request)
+
+                return Response({'data': template}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['delete'])
+    def delete_prudence_advice(self, request, org_pk, pk=None):
+        self._check_permission_on_organization(request, org_pk, 'delete_prudence_advice')
+        organization = get_object_or_404(
+            OrganizationStructure.objects.using(settings.READONLY_DATABASE),
+            pk=org_pk
+        )
+
+        if pk:
+            prudence_advice = get_object_or_404(PrudenceAdvice, pk=pk)
+            organilab_logentry(request.user, prudence_advice, DELETION,
+                               'prudenceadvice', relobj=[organization])
+            prudence_advice.delete()
+
+            prudence_advices = self.get_queryset().order_by('code')
+            template = render_to_string('sga/prudence_advice.html',
+                                        {'prudence_advices': prudence_advices},
+                                        request)
+
+            return Response({'data': template}, status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
