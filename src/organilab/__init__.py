a7ddd4e18090c1621a10b5c2c4a2137186cd485b
Luis Zárate Montero <luis.zarate@solvosoft.com>
2023-07-08 10:16:29 -0600

    Documented Make actions

--- src/organilab/__init__.py
+++ src/organilab/__init__.py
@@ -2,4 +2,7 @@ from __future__ import absolute_import
 
 __version__ = '1.0.0'
 
-from .celery import app as celery_app
+if __name__ == '__main__':
+    print(__version__)
+else:
+    from .celery import app as celery_app
