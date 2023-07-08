20fb484f44a73c8d57ad3ff39044829c98cc226d
Kendrick <kenjen041@gmail.com>
2023-07-07 23:22:19 -0600

    Cambios en el reporte de movimientos de objectos

--- src/report/views/object_changes.py
+++ src/report/views/object_changes.py
@@ -1,29 +1,88 @@
 from django.conf import settings
 from django.db.models import Sum
 from django.utils.translation import gettext as _
-from laboratory.models import Object, ObjectLogChange, Laboratory
+from laboratory.models import Object, ObjectLogChange, Laboratory, Catalog
 from laboratory.utils import get_user_laboratories
+from report.models import ObjectChangeLogReport, ObjectChangeLogReportBuilder
 from report.utils import filter_period
 
 
-def resume_queryset(queryset):
+def resume_queryset(report,queryset):
 
     laboratories = set(queryset.values_list('laboratory__pk', flat=True))
-    list_obj = []
-    result = []
-    total_record=0
-    doc_list= []
-    doc_elements=[]
+    builder = []
+    total = 0
+    doc_list=[]
+
     for laboratory in laboratories:
-        object_diff = 0
         lab = Laboratory.objects.get(pk=laboratory)
         query = queryset.filter(laboratory=lab)
         objects = set(query.values_list('object', 'measurement_unit'))
 
         for obj in objects:
-            obj_using= Object.objects.filter(pk=obj[0]).first()
+            object_log = None
+            obj_using = Object.objects.filter(pk=obj[0]).first()
+            catalog = Catalog.objects.get(pk=obj[1])
             query_values = query.filter(object=obj_using, measurement_unit__pk=obj[1])
-            object_diff = queryset.filter(laboratory=lab,object=obj_using, measurement_unit__pk=obj[1]).aggregate(total=Sum('diff_value'))['total']
+            object_diff = queryset.filter(laboratory=lab,
+                                          object=obj_using,
+                                          measurement_unit__pk=obj[1]
+                                          ).aggregate(total=Sum('diff_value'))['total']
+            if query_values:
+
+                object_log = ObjectChangeLogReport.objects.create(
+                    task_report=report,
+                    laboratory=lab,
+                    object=obj_using,
+                    unit=catalog,
+                    diff_value=object_diff)
+
+            for values in query_values:
+
+                object_builder = ObjectChangeLogReportBuilder(
+                    report=object_log,
+                    user=values.user,
+                    old_value=values.old_value,
+                    new_value=values.new_value,
+                    diff_value=values.diff_value,
+                    update_time=values.update_time
+                )
+
+                builder.append(object_builder)
+
+                total += 1
+            if object_log:
+                ObjectChangeLogReportBuilder.objects.bulk_create(
+                    builder
+                )
+            builder.clear()
+    return total
+
+def resume_queryset_doc(report,queryset):
+
+    laboratories = set(queryset.values_list('laboratory__pk', flat=True))
+    builder = []
+    total = 0
+    doc_list=[]
+
+    for laboratory in laboratories:
+        object_log = None
+        lab = Laboratory.objects.get(pk=laboratory)
+        query = queryset.filter(laboratory=lab)
+        objects = set(query.values_list('object', 'measurement_unit'))
+
+        for obj in objects:
+
+            object_log = None
+            obj_using = Object.objects.filter(pk=obj[0]).first()
+            catalog = Catalog.objects.get(pk=obj[1])
+            query_values = query.filter(object=obj_using, measurement_unit__pk=obj[1])
+            object_diff = queryset.filter(laboratory=lab,
+                                          object=obj_using,
+                                          measurement_unit__pk=obj[1]
+                                          ).aggregate(total=Sum('diff_value'))['total']
+            if query_values:
+                object_log=True
             for values in query_values:
 
                 try:
@@ -32,32 +91,21 @@ def resume_queryset(queryset):
                         user = values.user.username
                 except Exception as e:
                     user = ""
+                builder.append([user, values.update_time.strftime("%m/%d/%Y, %H:%M:%S"),
+                                values.old_value, values.new_value, values.diff_value])
+                total += 1
 
-                object_list =\
-                    {'user': user,
-                     'update_time': values.update_time.strftime("%m/%d/%Y, %H:%M:%S"),
-                     'old_value': values.old_value,
-                     'new_value': values.new_value,
-                     'diff_value': values.diff_value,
-                     'measurement_unit': str(values.measurement_unit),
-                     }
-                total_record+=1
-
-                list_obj.append(list(object_list.values()))
-                doc_elements.append(list(object_list.values()))
-            if obj_using:
-                doc_list.append([f'{lab.name} | {str(obj_using)} {object_diff}'])
-                doc_list.append([_("User"), _("Day"), _('Old'), _('New'), _("Difference"), _("Unit")])
-                doc_list.extend(doc_elements)
+            if object_log:
+                doc_list.append([f'{lab.name} | {obj_using.name}'])
+                doc_list.append([_('Difference')+f': {object_diff} {catalog.description}'])
+                doc_list.append(
+                        [_("User"), _("Day"), _('Old'), _('New'), _("Difference")])
+                doc_list.extend(builder)
                 doc_list.append([])
                 doc_list.append([])
-                result.append({'lab': lab.name,'lab_id': lab.id,'obj': str(obj_using),
-                               'obj_id': obj_using.id, 'unit': obj[1],
-                               'values': list_obj,'diff': object_diff})
-            list_obj = []
-            doc_elements=[]
-    return {'record': total_record, 'content': result,'doc_elements':doc_list}
+            builder = []
 
+    return {'record': total, 'content': doc_list}
 
 def get_queryset(report):
     query = ObjectLogChange.objects.all().using(settings.READONLY_DATABASE).order_by('update_time')
@@ -78,7 +126,11 @@ def get_queryset(report):
     return query
 
 
-def get_dataset_objectlogchanges(report,column_list=None):
+def get_dataset_objectlogchanges(report,is_doc=False,column_list=None):
     queryset = get_queryset(report)
-    object_list = resume_queryset(queryset)
+    object_list = None
+    if is_doc:
+        object_list = resume_queryset_doc(report,queryset)
+    else:
+        object_list = resume_queryset(report,queryset)
     return object_list
