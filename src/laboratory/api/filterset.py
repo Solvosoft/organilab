71bc9001eb1a2f4b57ea6848e0f9b1c70b5febca
Kendrick <kenjen041@gmail.com>
2023-07-19 17:00:13 -0600

    Correccion de filtros de mis sustancias

--- src/laboratory/api/filterset.py
+++ src/laboratory/api/filterset.py
@@ -1,10 +1,26 @@
+from django.db.models import Q
 from django_filters import FilterSet
-
+from django.db.models.functions import Concat
 from sga.models import Substance
+from django.db.models.expressions import Value
 
 
 class SubstanceFilterSet(FilterSet):
 
+    def filter_queryset(self, queryset):
+        search = self.request.GET.get('search', '')
+        if search:
+            queryset = queryset.annotate(
+                fullname=Concat('created_by__first_name', Value(' '),
+                                'created_by__last_name'))
+            queryset = queryset.filter(
+                Q(created_by__username__icontains=search) |
+                Q(fullname__icontains=search) |
+                Q(comercial_name__icontains=search) |
+                Q(uipa_name__icontains=search)).distinct()
+        return queryset
+
+
     class Meta:
         model = Substance
-        fields = ['comercial_name', 'uipa_name']
+        fields = ['created_by','comercial_name', 'uipa_name']
