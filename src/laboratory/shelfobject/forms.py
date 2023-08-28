0ea0ad68e1befbe3e29ef542014ee36539e803ca
Luis Zárate Montero <luis.zarate@solvosoft.com>
2023-08-28 12:10:33 -0600

    Fixed forms and migrations

--- src/laboratory/shelfobject/forms.py
+++ src/laboratory/shelfobject/forms.py
@@ -89,6 +89,51 @@ class MoveShelfObjectForm(GTForm):
                                    }), help_text=_("This select only shows shelves with same measurement unit than current object")
                                    )
 
+    def __init__(self, *args, **kwargs):
+        group_name = kwargs.pop('group_name')
+        super(MoveShelfObjectForm, self).__init__(*args, **kwargs)
+
+        self.fields["lab_room"] = forms.ModelChoiceField(
+            queryset=LaboratoryRoom.objects.all(), label=_("Laboratory Room"),
+            widget=AutocompleteSelect("lab_room", attrs={
+                'data-related': 'true',
+                'data-pos': 0,
+                'data-groupname': group_name,
+                'data-s2filter-shelfobject': '#id_shelfobject',
+                'data-s2filter-organization': '#id_organization',
+                'data-s2filter-laboratory': '#id_laboratory'
+            })
+        )
+
+        self.fields["furniture"] = forms.ModelChoiceField(
+            queryset=Furniture.objects.all(), label=_("Furniture"),
+            widget=AutocompleteSelect("furniture", attrs={
+                'data-related': 'true',
+                'data-pos': 1,
+                'data-groupname': group_name,
+                'data-s2filter-shelfobject': '#id_shelfobject',
+                'data-s2filter-organization': '#id_organization',
+                'data-s2filter-laboratory': '#id_laboratory'
+            })
+        )
+
+        self.fields["shelf"] = forms.ModelChoiceField(
+            queryset=Shelf.objects.all(), label=_("Shelf"),
+            widget=AutocompleteSelect("shelf", attrs={
+                'data-related': 'true',
+                'data-pos': 2,
+                'data-groupname': group_name,
+                'data-s2filter-shelfobject': '#id_shelfobject',
+                'data-s2filter-organization': '#id_organization',
+                'data-s2filter-laboratory': '#id_laboratory'
+            }), help_text=_(
+                "This select only shows shelves with some of the following features: "
+                "<br>1) Infinity quantity capacity and not defined measurement unit. "
+                "<br>2) Infinity quantity capacity and same measurement unit than selected shelf object. "
+                "<br>3) Same measurement unit than selected shelf object and available capacity in the shelf.")
+        )
+
+
 class ShelfObjectExtraFields(GTForm, forms.Form):
     objecttype = forms.IntegerField(widget=genwidgets.HiddenInput, min_value=0, max_value=3, required=True)
     without_limit = forms.BooleanField(widget=genwidgets.CheckboxInput(attrs={'class': 'check_limit'}),
