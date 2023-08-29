692a97e33f12bb48bee85918de8ed2ffceb8b770
Marta Solano <marsoguti@gmail.com>
2023-08-29 15:16:03 -0600

    Fix some code that was not required anymore but was merged.

--- src/laboratory/shelfobject/utils.py
+++ src/laboratory/shelfobject/utils.py
@@ -67,17 +67,13 @@ def clone_shelfobject_limits(shelfobject, user):
         organilab_logentry(user, new_limits, ADDITION, changed_data=['minimum_limit', 'maximum_limit', 'expiration_date'])
     return new_limits
 
-def get_available_containers_for_selection(laboratory_id, exclude_used_as_container=True):
+def get_available_containers_for_selection(laboratory_id):
     # all containers that belong to a laboratory that are not in use
-    filters = {
-        'in_where_laboratory_id': laboratory_id,
-        'object__type':  Object.MATERIAL
-    }
-    # it's not used as container - query the reverse relationship
-    if exclude_used_as_container:
-        filters['containershelfobject']  = None
-
-    containers = ShelfObject.objects.filter(**filters)
+    containers = ShelfObject.objects.filter(
+        in_where_laboratory_id=laboratory_id,
+        object__type=Object.MATERIAL,
+        containershelfobject=None  # it's not used as container - query the reverse relationship
+    )
     return containers
 
 def get_containers_for_cloning(organization_id):
