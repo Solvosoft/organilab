2bdda5a5227ede016b2e2a8cced7bea3be73098c
Marcela González Zúñiga <marcegz31@gmail.com>
2023-09-19 16:53:08 -0600

    Updating shelf tests

--- src/laboratory/tests/gtapi/base.py
+++ src/laboratory/tests/gtapi/base.py
@@ -16,22 +16,25 @@ class TestCaseBase(TestCase):
         self.org2 = OrganizationStructure.objects.filter(name="Organization 2").first()
 
         # USER
-        self.user1_org1 = get_user_model().objects.filter(username="user1org1").first()
-        self.user2_org2 = get_user_model().objects.filter(username="user2org2").first()
+        self.user1 = get_user_model().objects.filter(username="user1").first()
+        self.user2 = get_user_model().objects.filter(username="user2").first()
+        self.user3 = get_user_model().objects.filter(username="user3").first()
+        self.user4 = get_user_model().objects.filter(username="user4").first()
 
         # PROFILE
-        self.profile1_org1 = self.user1_org1.profile
-        self.profile2_org2 = self.user2_org2.profile
+        self.profile1 = self.user1.profile
+        self.profile2 = self.user2.profile
 
         # ORGS BY USER
         self.user1_org1_list = OrganizationStructure.os_manager.filter_organization_by_user(
-            self.user1_org1).distinct()
+            self.user1).distinct()
         self.user2_org2_list = OrganizationStructure.os_manager.filter_organization_by_user(
-            self.user2_org2).distinct()
+            self.user2).distinct()
 
         # LABS
         self.lab1_org1 = Laboratory.objects.get(name="Lab 1")
-        self.lab2_org1 = Laboratory.objects.get(name="Lab 2")
+        self.lab2_org2 = Laboratory.objects.get(name="Lab 2")
+        self.lab3_org2 = Laboratory.objects.get(name="Lab 3")
 
         # INITIAL DATA
 
@@ -40,7 +43,11 @@ class TestCaseBase(TestCase):
         self.org_contenttype = ContentType.objects.filter(app_label='laboratory',
                                                           model='organizationstructure').first()
 
-        self.client1_org1 = Client()
-        self.client2_org2 = Client()
-        self.client1_org1.force_login(self.user1_org1)
-        self.client2_org2.force_login(self.user2_org2)
+        self.client1 = Client()
+        self.client2 = Client()
+        self.client3 = Client()
+        self.client4 = Client()
+        self.client1.force_login(self.user1)
+        self.client2.force_login(self.user2)
+        self.client3.force_login(self.user3)
+        self.client4.force_login(self.user4)
