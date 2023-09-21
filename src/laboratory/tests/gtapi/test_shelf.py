98d950630a7f57d76572388a4b6c9ad860f7ff05
Marcela González Zúñiga <marcegz31@gmail.com>
2023-09-21 14:36:20 -0600

    Including new shelf tests cases

--- src/laboratory/tests/gtapi/test_shelf.py
+++ src/laboratory/tests/gtapi/test_shelf.py
@@ -1,3 +1,4 @@
+from laboratory.models import ShelfObject
 from laboratory.tests.gtapi.base import ShelfViewTestOrgCanManageLab, ShelfViewTestOrgCannotManageLab
 
 class ShelfViewTest1(ShelfViewTestOrgCanManageLab):
@@ -65,7 +66,63 @@ class ShelfViewTest3(ShelfViewTestOrgCanManageLab):
     def test_get_shelf_by_shelfobject_case4(self):
         self.get_shelf_by_shelfobject(self.user4, self.client4)
 
-class ShelfViewTest4(ShelfViewTestOrgCannotManageLab):
+class ShelfViewTest4(ShelfViewTestOrgCanManageLab):
+    """
+        * Organization can manage this laboratory
+        * With required data
+        * Shelf Object is located to other laboratory in this same organization
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.org = self.org2
+        self.lab = self.lab2_org2
+        self.shelfobject = ShelfObject.objects.get(pk=3)
+        self.data.update({
+            "organization": self.org.pk,
+            "laboratory": self.lab.pk,
+            "shelfobject": self.shelfobject.pk
+        })
+
+    def test_get_shelf_by_shelfobject_case1(self):
+        self.get_shelf_by_shelfobject(user_access=True, status_code=400)
+
+    def test_get_shelf_by_shelfobject_case2(self):
+        self.get_shelf_by_shelfobject(self.user2, self.client2)
+
+    def test_get_shelf_by_shelfobject_case3(self):
+        self.get_shelf_by_shelfobject(self.user3, self.client3)
+
+    def test_get_shelf_by_shelfobject_case4(self):
+        self.get_shelf_by_shelfobject(self.user4, self.client4)
+
+class ShelfViewTest5(ShelfViewTestOrgCanManageLab):
+    """
+        * Organization can manage this laboratory
+        * With required data
+        * Shelf Object is located to other laboratory in other organization
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.shelfobject = ShelfObject.objects.get(pk=3)
+        self.data.update({
+            "shelfobject": self.shelfobject.pk
+        })
+
+    def test_get_shelf_by_shelfobject_case1(self):
+        self.get_shelf_by_shelfobject(user_access=True, status_code=400)
+
+    def test_get_shelf_by_shelfobject_case2(self):
+        self.get_shelf_by_shelfobject(self.user2, self.client2)
+
+    def test_get_shelf_by_shelfobject_case3(self):
+        self.get_shelf_by_shelfobject(self.user3, self.client3)
+
+    def test_get_shelf_by_shelfobject_case4(self):
+        self.get_shelf_by_shelfobject(self.user4, self.client4)
+
+class ShelfViewTest6(ShelfViewTestOrgCannotManageLab):
     """
         * Organization cannot manage this laboratory
         * With required data
@@ -83,7 +140,7 @@ class ShelfViewTest4(ShelfViewTestOrgCannotManageLab):
     def test_get_shelf_by_shelfobject_case4(self):
         self.get_shelf_by_shelfobject(self.user4, self.client4)
 
-class ShelfViewTest5(ShelfViewTestOrgCannotManageLab):
+class ShelfViewTest7(ShelfViewTestOrgCannotManageLab):
     """
         * Organization cannot manage this laboratory
         * Without required data
@@ -105,7 +162,7 @@ class ShelfViewTest5(ShelfViewTestOrgCannotManageLab):
     def test_get_shelf_by_shelfobject_case4(self):
         self.get_shelf_by_shelfobject(self.user4, self.client4)
 
-class ShelfViewTest6(ShelfViewTestOrgCannotManageLab):
+class ShelfViewTest8(ShelfViewTestOrgCannotManageLab):
     """
         * Organization cannot manage this laboratory
         * Without related furniture(relfield)
@@ -114,6 +171,34 @@ class ShelfViewTest6(ShelfViewTestOrgCannotManageLab):
     def setUp(self):
         super().setUp()
         del self.data["relfield"]
+
+    def test_get_shelf_by_shelfobject_case1(self):
+        self.get_shelf_by_shelfobject()
+
+    def test_get_shelf_by_shelfobject_case2(self):
+        self.get_shelf_by_shelfobject(self.user2, self.client2)
+
+    def test_get_shelf_by_shelfobject_case3(self):
+        self.get_shelf_by_shelfobject(self.user3, self.client3)
+
+    def test_get_shelf_by_shelfobject_case4(self):
+        self.get_shelf_by_shelfobject(self.user4, self.client4)
+
+class ShelfViewTest9(ShelfViewTestOrgCannotManageLab):
+    """
+        * Organization cannot manage this laboratory
+        * With required data
+        * Shelf Object is located to other laboratory in other organization
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.lab = self.lab2_org2
+        self.shelfobject = ShelfObject.objects.get(pk=3)
+        self.data.update({
+            "laboratory": self.lab.pk,
+            "shelfobject": self.shelfobject.pk
+        })
 
     def test_get_shelf_by_shelfobject_case1(self):
         self.get_shelf_by_shelfobject()
