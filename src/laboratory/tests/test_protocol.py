1cd13b76eb7ad6c082fb6a72886f3dce945ed697
Kejebo <Kenjen041@gmail.com>
2023-10-18 11:42:19 -0600

    Revert "Fixed protocols tests"
    This reverts commit eeb059603a04c725a2160b8a0e6ddc6b9a40ba3b.

--- src/laboratory/tests/test_protocol.py
+++ src/laboratory/tests/test_protocol.py
@@ -1,41 +1,19 @@
-from django.contrib.auth.models import Permission
 from django.urls import reverse
 
-from auth_and_perms.models import ProfilePermission, Rol
 from laboratory.models import Protocol
 from laboratory.tests.utils import BaseLaboratorySetUpTest
 import json
 
 class ProtocolViewTest(BaseLaboratorySetUpTest):
-    def setUp(self):
-        super().setUp()
-
-        self.permissions =ProfilePermission.objects.filter(content_type__app_label='laboratory',object_id=self.lab.pk,
-                                               profile=self.user.profile).first()
-        self.permissions.rol.clear()
-        self.rol = Rol.objects.create(name="Protocol user")
-        self.permissions.rol.add(self.rol)
-        self.add_permission = Permission.objects.get(pk=167)
-        self.update_permission = Permission.objects.get(pk=168)
-        self.delete_permission = Permission.objects.get(pk=169)
-        self.view_permission = Permission.objects.get(pk=170)
-        self.data_file = json.dumps({
-            'token': self.chfile.upload_id,
-            'name': self.chfile.filename,
-            'display_text': "Protocol_text.pdf"
-        })
 
     def test_protocol_list(self):
-        self.rol.permissions.add(self.view_permission)
         #This view is just a container(table), protocol table loads by "laboratory:api-protocol-list"
         url = reverse("laboratory:protocol_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name="laboratory/protocol/protocol_list.html")
-        self.rol.permissions.remove(self.view_permission)
 
     def test_update_protocol(self):
-        self.rol.permissions.add(self.update_permission)
         protocol = Protocol.objects.get(name="Lavado de manos")
         url = reverse("laboratory:protocol_update", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": protocol.pk})
 
@@ -48,32 +26,26 @@ class ProtocolViewTest(BaseLaboratorySetUpTest):
         data = {
             "name": "Lavado de manos",
             "short_description": "Higienización de las manos previa al ingreso del laboratorio y manipulación de los instrumentos del mismo.",
-            "file": self.data_file,
+            "file": self.chfile.upload_id,
             "laboratory": self.lab
         }
         response_post = self.client.post(url, data=data)
         success_url = reverse("laboratory:protocol_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         self.assertRedirects(response_post, success_url)
-        self.rol.permissions.remove(self.update_permission)
-
 
     def test_create_protocol(self):
-        self.rol.permissions.add(self.add_permission)
         data = {
             "name": "Manejo de desechos",
             "short_description": "Manipulación de desechos ordinarios y reciclables y su destino.",
-            "file": self.data_file
+            "file": self.chfile.upload_id
         }
-
         url = reverse("laboratory:protocol_create", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         response = self.client.post(url, data=data)
         success_url = reverse("laboratory:protocol_list", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk})
         self.assertRedirects(response, success_url)
         self.assertIn("Manejo de desechos", list(Protocol.objects.values_list("name", flat=True)))
-        self.rol.permissions.remove(self.add_permission)
 
     def test_delete_protocol(self):
-        self.rol.permissions.add(self.delete_permission)
         protocol = Protocol.objects.get(name="Manipulación de instrumentos de laboratorio")
         url = reverse("laboratory:protocol_delete", kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": protocol.pk})
         response = self.client.post(url)
@@ -81,7 +53,6 @@ class ProtocolViewTest(BaseLaboratorySetUpTest):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, success_url)
         self.assertNotIn("Manipulación de instrumentos de laboratorio", list(Protocol.objects.values_list("name", flat=True)))
-        self.rol.permissions.add(self.delete_permission)
 
     def test_api_protocol_list_set_limit(self):
         url = reverse("laboratory:api-protocol-list")
