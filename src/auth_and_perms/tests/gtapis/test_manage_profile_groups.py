0e9eb5be28bbc7fb7d4fa81e89fff9891f800795
Kejebo <kenjen041@gmail.com>
2023-11-13 17:57:13 -0600

    Add Tests of profile groups

---
+++ src/auth_and_perms/tests/gtapis/test_manage_profile_groups.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.models import Group
+from django.urls import reverse
+
+from auth_and_perms.tests.base_manage_profile_groups import TestCaseBase
+import json
+
+class TestUserProfile(TestCaseBase):
+
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("groupsbyprofile-list")
+        groups = Group.objects.filter(pk__in=[1, 4])
+        self.user1.groups.add(*groups)
+
+    def test_get_profile_group(self):
+        response = self.client.get(f'{self.url}?profile={self.user.pk}')
+        result = [id['id'] for id in json.loads(response.content)['results']]
+        self.assertTrue(response.status_code==200)
+        total = json.loads(response.content)['total_count']
+        self.assertTrue(self.user.groups.filter(pk__in=result).count()==total)
+
+    def test_get_profile_group_no_login(self):
+        self.client.logout()
+        response = self.client.get(f'{self.url}?profile={self.user.pk}')
+        self.assertNotIn('result',json.loads(response.content))
+        self.assertTrue(response.status_code==403)
+        self.assertTrue(self.user.groups.count()==2)
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(msg=="Authentication credentials were not provided.")
+
+    def test_get_profile_group_no_profile(self):
+        response = self.client.get(f'{self.url}')
+        self.assertNotIn('result',json.loads(response.content))
+        self.assertTrue(response.status_code==400)
+        self.assertTrue(self.user.groups.count()==2)
+        msg = json.loads(response.content)['errors']
+        self.assertTrue(msg['profile'][0]=="This field is required.")
+
+    def test_get_profile_group_no_groups(self):
+        response = self.client.get(f'{self.url}?profile={self.user3.pk}')
+        self.assertTrue(json.loads(response.content)['results']==[])
+        self.assertTrue(json.loads(response.content)['total_count']==0)
+        self.assertTrue(response.status_code==200)
+        self.assertTrue(self.user3.groups.count()==0)
+
+
+class TestProfileSelect(TestCaseBase):
+
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("usersbyorg-list")
+        groups = Group.objects.filter(pk__in=[1, 4])
+        self.user1.groups.add(*groups)
+
+    def test_get_profile_group(self):
+        response = self.client.get(f'{self.url}?organization={self.org.pk}')
+        result = [id['id'] for id in json.loads(response.content)['results']]
+        self.assertTrue(response.status_code==200)
+        self.assertTrue(len(result)>0)
+        total = json.loads(response.content)['total_count']
+        self.assertTrue(self.org.users.count()==total)
+
+    def test_get_profile_group_no_org(self):
+        response = self.client.get(f'{self.url}')
+        self.assertTrue(response.status_code==400)
+        self.assertTrue(json.loads(response.content)['errors']['organization'][0]=='This field is required.')
+
+    def test_get_profile_group_no_login(self):
+        self.client.logout()
+        response = self.client.get(f'{self.url}?organization={self.org.pk}')
+        self.assertTrue(response.status_code==403)
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(msg == "Authentication credentials were not provided.")
--- src/auth_and_perms/tests/test_user_groups.py
+++ src/auth_and_perms/tests/test_user_groups.py
@@ -1,21 +1,10 @@
+from django.contrib.auth.models import Group
 from django.urls import reverse
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from laboratory.models import OrganizationStructure
+
+from auth_and_perms.tests.base_manage_profile_groups import TestCaseBase
 import json
-class TestCaseBase(TestCase):
-    fixtures = ["groups/group.json"]
-
-    def setUp(self):
-        super().setUp()
-        self.org1 = OrganizationStructure.objects.filter(name="Organization 1").first()
-        self.org2 = OrganizationStructure.objects.filter(name="Organization 2").first()
-        self.user1 = get_user_model().objects.filter(username="user1").first()
-        self.client = Client()
-        self.client.force_login(self.user1)
-        self.org = self.org1
-        self.user = self.user1
-
 class TestUserProfile(TestCaseBase):
 
     def setUp(self):
@@ -28,8 +17,6 @@ class TestUserProfile(TestCaseBase):
                 "organization":self.org.pk}
 
         response = self.client.post(self.url,data=data, content_type='application/json')
-        print(response.content)
-
         self.assertTrue(response.status_code==200)
         user = get_user_model().objects.filter(username="user1").first()
         msg = json.loads(response.content)['detail']
@@ -40,10 +27,11 @@ class TestUserProfile(TestCaseBase):
         data = {"groups":[1,2,3],
                 "organization":self.org.pk}
         response = self.client.post(self.url,data=data, content_type='application/json')
-        self.assertTrue(response.status_code==404)
+        self.assertTrue(response.status_code==400)
         user = get_user_model().objects.filter(username="user1").first()
+        msg = json.loads(response.content)['errors']
         self.assertTrue(user.groups.count()==0)
-        print(response.content)
+        self.assertTrue(msg['profile'][0]=="This field is required.")
 
     def test_update_profile_wrong_org(self):
         data = {"profile": 1,
@@ -51,8 +39,19 @@ class TestUserProfile(TestCaseBase):
                 "organization":2}
         response = self.client.post(self.url,data=data, content_type='application/json')
         self.assertTrue(response.status_code==403)
-        print(response.content)
         user = get_user_model().objects.filter(username="user1").first()
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(user.groups.count()==0)
+        self.assertTrue(msg == "You do not have permission to perform this action.")
+
+    def test_update_profile_no_org(self):
+        data = {"profile": 1,
+                "groups":[1,2,3]}
+        response = self.client.post(self.url,data=data, content_type='application/json')
+        self.assertTrue(response.status_code==404)
+        user = get_user_model().objects.filter(username="user1").first()
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(msg == "Not found.")
         self.assertTrue(user.groups.count()==0)
 
     def test_update_profile_no_org(self):
@@ -61,13 +60,74 @@ class TestUserProfile(TestCaseBase):
         response = self.client.post(self.url,data=data, content_type='application/json')
         self.assertTrue(response.status_code==404)
         user = get_user_model().objects.filter(username="user1").first()
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(msg == "Not found.")
         self.assertTrue(user.groups.count()==0)
 
-    def test_update_profile_no_org(self):
+    def test_update_profile_no_login(self):
+        self.client.logout()
         data = {"profile": 1,
-                "groups":[1,2,3]}
+                "groups":[1,2,3],
+                "organization":2}
         response = self.client.post(self.url,data=data, content_type='application/json')
-        self.assertTrue(response.status_code==404)
+        self.assertTrue(response.status_code==403)
         user = get_user_model().objects.filter(username="user1").first()
         self.assertTrue(user.groups.count()==0)
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(user.groups.count()==0)
+        self.assertTrue(msg == "You do not have permission to perform this action.")
 
+    def test_update_profile_new_group(self):
+        data = {"profile": 1,
+                "groups":[1,2,3,5],
+                "organization":1}
+        groups = Group.objects.filter(pk__in=[1,4])
+        self.user1.groups.add(*groups)
+        response = self.client.post(self.url,data=data, content_type='application/json')
+        self.assertTrue(response.status_code==200)
+        user = get_user_model().objects.filter(username="user1").first()
+        self.assertFalse(user.groups.filter(pk=4).exists())
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(user.groups.count()==4)
+        self.assertTrue(msg == "Profile was updated successfully.")
+
+
+    def test_profile_user_no_profile(self):
+        self.client.logout()
+        self.client.force_login(self.user3)
+        data = {"profile": 1,
+                "groups": [1, 2, 3, 5],
+                "organization": 1}
+        groups = Group.objects.filter(pk__in=[1, 4])
+        self.user1.groups.add(*groups)
+        response = self.client.post(self.url, data=data,
+                                    content_type='application/json')
+
+        self.assertTrue(response.status_code == 404)
+
+    def test_update_profile_empty_group(self):
+        data = {"profile": 1,
+                "groups":[],
+                "organization":1}
+        groups = Group.objects.filter(pk__in=[1,4])
+        self.user1.groups.add(*groups)
+        response = self.client.post(self.url,data=data, content_type='application/json')
+        self.assertTrue(response.status_code==200)
+        user = get_user_model().objects.filter(username="user1").first()
+        self.assertFalse(user.groups.filter(pk=4).exists())
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(user.groups.count()==0)
+        self.assertTrue(msg == "Profile was updated successfully.")
+
+    def test_update_profile_no_group(self):
+        data = {"profile": 1,
+                "organization":1}
+        groups = Group.objects.filter(pk__in=[1,4])
+        self.user1.groups.add(*groups)
+        response = self.client.post(self.url,data=data, content_type='application/json')
+        self.assertTrue(response.status_code==200)
+        user = get_user_model().objects.filter(username="user1").first()
+        self.assertFalse(user.groups.filter(pk=4).exists())
+        msg = json.loads(response.content)['detail']
+        self.assertTrue(user.groups.count()==0)
+        self.assertTrue(msg == "Profile was updated successfully.")
