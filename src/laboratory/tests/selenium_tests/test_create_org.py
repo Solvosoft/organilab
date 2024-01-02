892095eec7b36b0928224321a05d765a3f44bd98
Marce <marcegz31@gmail.com>
2024-01-02 16:31:51 -0600

    Path list from create organization test

--- src/laboratory/tests/selenium_tests/test_create_org.py
+++ src/laboratory/tests/selenium_tests/test_create_org.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
-from django.urls import reverse
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from organilab_test.tests.base import SeleniumBase
-from django.utils.translation import gettext_lazy as _
 from django.test import tag
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+from organilab_test.tests.base import SeleniumBase
+
 
 @tag('selenium')
 class OrganizationSeleniumTest(SeleniumBase):
@@ -19,37 +19,15 @@ class OrganizationSeleniumTest(SeleniumBase):
         self.create_directory_path(folder_name=self.folder_name)
 
     def test_create_organization(self):
-        self.screenShots("1_select_org", time_out=3)
-        action = ActionChains(self.selenium)
 
-        org_and_perms = self.selenium.find_element(By.XPATH, ".//ul[@class='nav side-menu']/li[2]/a")
-        action.move_to_element(org_and_perms).perform()
-        org_and_perms.click()
-        self.screenShots("2_org_and_perms", time_out=3)
+        path_list = [
+            {"path": ".//ul[@class='nav side-menu']/li[2]/a"},
+            {"path": ".//ul[@class='nav side-menu']/li[2]/ul/li/a"},
+            {"path": ".//span[@class='addOrgStructureEmpty']"},
+            {"path": ".//div[@id='addOrganizationmodal']/div/div[@class='modal-content']/form/div[@class='modal-body']/div/div/input[@id='id_name']"},
+            {"path": ".//div[@id='addOrganizationmodal']/div/div[@class='modal-content']/form/div[@class='modal-body']/div/div/input[@id='id_name']",
+             "extra_action": "setvalue", "value": _("Organization Name")},
+            {"path": ".//div[@id='addOrganizationmodal']/div/div[@class='modal-content']/form/div[@class='modal-footer']/button[@type='submit']"},
+        ]
 
-        manage_orgs = self.selenium.find_element(By.XPATH, ".//ul[@class='nav side-menu']/li[2]/ul/li/a")
-        action.move_to_element(manage_orgs).perform()
-        manage_orgs.click()
-        self.screenShots("3_manage_orgs", time_out=3)
-
-        btn_add_org = self.selenium.find_element(By.XPATH, ".//span[@class='addOrgStructureEmpty']")
-        action.move_to_element(btn_add_org).perform()
-        btn_add_org.click()
-        self.screenShots("4_btn_add_org", time_out=3)
-
-        name_input = self.selenium.find_element(By.XPATH, ".//div[@id='addOrganizationmodal']/div/div[@class='modal-content']/form/div[@class='modal-body']/div/div/input[@id='id_name']")
-
-        self.screenShots("5_add_org_form", time_out=3)
-        action.move_to_element(name_input).perform()
-        name_input.click()
-        self.screenShots("6_name_input_click", time_out=3)
-
-        name_input.send_keys(_("Organization Name"))
-        self.screenShots("7_name_input_set_value", time_out=3)
-
-        btn_create_org = self.selenium.find_element(By.XPATH, ".//div[@id='addOrganizationmodal']/div/div[@class='modal-content']/form/div[@class='modal-footer']/button[@type='submit']")
-        action.move_to_element(btn_create_org).perform()
-        btn_create_org.click()
-        self.screenShots("8_btn_create_org_click", time_out=3)
-
-        self.create_gifs(self.dir, self.folder_name)
+        self.create_gif_process(path_list, self.folder_name)
