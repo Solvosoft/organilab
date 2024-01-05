7c6d3a8c59df9b87c0fc8c53d497f9bab14e37f4
Marce <marcegz31@gmail.com>
2024-01-05 10:45:31 -0600

    Create and delete organization tests

--- src/laboratory/tests/selenium_tests/test_create_org.py
+++ src/laboratory/tests/selenium_tests/test_create_org.py
@@ -3,6 +3,7 @@ from django.test import tag
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
+from laboratory.models import OrganizationStructure
 from organilab_test.tests.base import SeleniumBase
 
 
@@ -13,14 +14,13 @@ class OrganizationSeleniumTest(SeleniumBase):
     def setUp(self):
         super().setUp()
         self.user = User.objects.get(pk=1)
+        self.org = OrganizationStructure.objects.get(pk=1)
         self.select_org_url = self.live_server_url + str(reverse('auth_and_perms:select_organization_by_user'))
         self.force_login(user=self.user, driver=self.selenium, base_url=self.live_server_url)
-        self.folder_name = "create_org"
-        self.create_directory_path(folder_name=self.folder_name)
 
     def test_create_organization(self):
-
         path_list = [
+
             {"path": ".//ul[@class='nav side-menu']/li[2]/a"},
             {"path": ".//ul[@class='nav side-menu']/li[2]/ul/li/a"},
             {"path": ".//span[@class='addOrgStructureEmpty']"},
@@ -29,5 +29,13 @@ class OrganizationSeleniumTest(SeleniumBase):
              "extra_action": "setvalue", "value": _("Organization Name")},
             {"path": ".//div[@id='addOrganizationmodal']/div/div[@class='modal-content']/form/div[@class='modal-footer']/button[@type='submit']"},
         ]
+        self.create_gif_process(path_list, "create_org")
 
-        self.create_gif_process(path_list, self.folder_name)
+
+    def test_delete_organization(self):
+        path_list = [
+            {"path": ".//ul[@class='nav side-menu']/li[2]/a"},
+            {"path": ".//ul[@class='nav side-menu']/li[2]/ul/li/a"},
+            {"path": ".//div[@class='right_col']/div[@class='card']/div[@class='card-body']/div[@class='row']/div[1]/div[2]/div/div/div[2]/ul/li[6]/a"}
+        ]
+        self.create_gif_process(path_list, "delete_org")
