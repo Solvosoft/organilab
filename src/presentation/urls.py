553f283bcadbc910de1d1d45f2403dcb299d9e72
Luis Zárate Montero <luis.zarate@solvosoft.com>
2024-01-27 14:33:15 -0600

    View for check production (now check emails with errores going well)

--- src/presentation/urls.py
+++ src/presentation/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .check_system import do_checks
 
 urlpatterns = [
     path('', views.index_organilab, name='index'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('donate', views.donate, name='donate'),
     path('donate_success', views.donate_success, name='donate_success'),
     path('feedback', views.FeedbackView.as_view(), name='feedback'),
+    path('check_ok', do_checks, name="check_ok")
 ]
