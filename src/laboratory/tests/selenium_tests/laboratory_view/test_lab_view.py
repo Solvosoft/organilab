6ba7d69cc4f355e2af7db20df6f27dca84c1cbd7
Marce <marcegz31@gmail.com>
2024-02-06 17:01:18 -0600

    Shefobject actions column

--- src/laboratory/tests/selenium_tests/laboratory_view/test_lab_view.py
+++ src/laboratory/tests/selenium_tests/laboratory_view/test_lab_view.py
@@ -6,6 +6,41 @@ from laboratory.tests.selenium_tests.laboratory_view.base import LaboratoryViewS
 @tag('selenium')
 class CreateShelfObject(LaboratoryViewSeleniumTest):
 
+    def setUp(self):
+        super().setUp()
+
+        self.create_shelfobject_reative_start = self.path_base + [
+            {"path": "//*[@id='labroom_2']"},
+            {"path": "//*[@id='furniture_3']"},
+            {"path": "//*[@id='shelf_2']"},
+            {"path": "//*[@id='shelfobjecttable_wrapper']/div/div[2]/div/button[3]",
+             "scroll": "window.scrollTo(0, 250)"},
+            {"path": "//*[@id='reactive_form']/div/div/span/span/span"},
+            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"},
+            {"path": "//*[@id='reactive_form']/div[2]/div/span/span/span"},
+            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"},
+            {"path": "//*[@id='reactive_form']/div[3]/div/input"},
+            {"path": "//*[@id='reactive_form']/div[3]/div/input",
+             "extra_action": "setvalue", "value": "3"},
+            {"path": "//*[@id='reactive_form']/div[4]/div/span/span/span"},
+            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"}
+        ]
+
+        self.create_shelfobject_reative_end = [
+            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"},
+            {"path": "//*[@id='reactive_form']/div[8]/div/textarea",
+             "scroll": "$('#reactive_modal').scrollTop(400);"},
+            {"path": "//*[@id='reactive_form']/div[8]/div/textarea",
+             "extra_action": "setvalue", "value": "El cloroformo es un líquido incoloro"
+             " de olor dulce y agradable. Se utiliza como disolvente y en la elaboración"
+             " de refrigerantes, resinas y plásticos."},
+            {"path": "//*[@id='reactive_form']/div[10]/div/input"},
+            {"path": "//*[@id='reactive_form']/div[10]/div/input",
+             "extra_action": "clearinput"},
+            {"path": "//*[@id='reactive_form']/div[10]/div/input",
+             "extra_action": "setvalue", "value": "3092"}
+        ]
+
     def get_save_button_modal(self, id_modal):
         return "//*[@id='%s']/div/div/div[3]/button[2]" % id_modal
 
