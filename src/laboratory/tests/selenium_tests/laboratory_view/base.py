1e1df9e70ff4e91847d3105c9bbd531e9363abba
Marce <marcegz31@gmail.com>
2024-02-06 17:01:18 -0600

    Shefobject actions column

--- src/laboratory/tests/selenium_tests/laboratory_view/base.py
+++ src/laboratory/tests/selenium_tests/laboratory_view/base.py
@@ -19,38 +19,6 @@ class LaboratoryViewSeleniumTest(SeleniumBase):
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div[1]/div[2]/ul/li[1]/a"}
         ]
 
-        self.create_shelfobject_reative_start = self.path_base + [
-            {"path": "//*[@id='labroom_2']"},
-            {"path": "//*[@id='furniture_3']"},
-            {"path": "//*[@id='shelf_2']"},
-            {"path": "//*[@id='shelfobjecttable_wrapper']/div/div[2]/div/button[3]",
-             "scroll": "window.scrollTo(0, 250)"},
-            {"path": "//*[@id='reactive_form']/div/div/span/span/span"},
-            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"},
-            {"path": "//*[@id='reactive_form']/div[2]/div/span/span/span"},
-            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"},
-            {"path": "//*[@id='reactive_form']/div[3]/div/input"},
-            {"path": "//*[@id='reactive_form']/div[3]/div/input",
-             "extra_action": "setvalue", "value": "3"},
-            {"path": "//*[@id='reactive_form']/div[4]/div/span/span/span"},
-            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"}
-        ]
-
-        self.create_shelfobject_reative_end = [
-            {"path": "//*[@id='reactive_form']/span/span/span[2]/ul/li"},
-            {"path": "//*[@id='reactive_form']/div[8]/div/textarea",
-             "scroll": "$('#reactive_modal').scrollTop(400);"},
-            {"path": "//*[@id='reactive_form']/div[8]/div/textarea",
-             "extra_action": "setvalue", "value": "El cloroformo es un líquido incoloro"
-             " de olor dulce y agradable. Se utiliza como disolvente y en la elaboración"
-             " de refrigerantes, resinas y plásticos."},
-            {"path": "//*[@id='reactive_form']/div[10]/div/input"},
-            {"path": "//*[@id='reactive_form']/div[10]/div/input",
-             "extra_action": "clearinput"},
-            {"path": "//*[@id='reactive_form']/div[10]/div/input",
-             "extra_action": "setvalue", "value": "3092"}
-        ]
-
 @tag('selenium')
 class LabViewTest(SeleniumBase):
     fixtures = ["selenium/laboratory_view.json"]
--- src/organilab_test/tests/base.py
+++ src/organilab_test/tests/base.py
@@ -225,8 +225,6 @@ class SeleniumBase(StaticLiveServerTestCase):
         """
         if obj["extra_action"] == "setvalue":
             self.set_value_action(obj, element)
-        elif obj["extra_action"] == "submit":
-            self.do_submit_action(obj,element)
         elif obj["extra_action"] == "script":
             self.selenium.execute_script(obj['value'])
         elif obj["extra_action"] == "sweetalert_comfirm":
@@ -262,9 +260,6 @@ class SeleniumBase(StaticLiveServerTestCase):
     def apply_utils(self,obj):
 
         if 'scroll' in obj:
-            self.selenium.execute_script(obj['scroll'])
-
-        if 'modalscroll' in obj:
             self.selenium.execute_script(obj['scroll'])
 
         if 'sleep' in obj:
