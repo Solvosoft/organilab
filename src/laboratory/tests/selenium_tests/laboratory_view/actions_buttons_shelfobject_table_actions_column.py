1ee06f1976c9c1d374b43c98167f555fb9774b8d
Marce <marcegz31@gmail.com>
2024-02-09 17:47:16 -0600

    Move and transfer out tests

--- src/laboratory/tests/selenium_tests/laboratory_view/actions_buttons_shelfobject_table_actions_column.py
+++ src/laboratory/tests/selenium_tests/laboratory_view/actions_buttons_shelfobject_table_actions_column.py
@@ -29,112 +29,177 @@ class ButtonsActionsTableColumnBase(LaboratoryViewSeleniumTest):
 @tag('selenium')
 class ButtonsActionsTableColumn(ButtonsActionsTableColumnBase):
 
-    def test_view_shelfobject_detail(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a"},
-            {"path": "//*[@id='shelfobject_detail_modal_body']",
-             "extra_action": "script",
-             "value": "$('#shelfobject_detail_modal_body').scrollTop(150);"},
-            {"path": "//*[@id='shelfobject_detail_modal_body']",
-             "extra_action": "script",
-             "value": "$('#shelfobject_detail_modal_body').scrollTop(300);"},
-            {"path": "//*[@id='shelfobject_detail_modal_body']",
-             "extra_action": "script",
-             "value": "$('#shelfobject_detail_modal_body').scrollTop(450);"},
-            {"path": "//*[@id='detail_modal_container']/div/div/div[3]/button"}
-        ]
-        self.create_gif_process(path_list, "view_shelfobject_detail")
+    def setUp(self):
+        super().setUp()
 
-    def test_reserve_shelfobject(self):
-        initial_date, initial_date_strftime = self.get_format_increase_decrease_date(now(), 1)
-        final_date, final_date_strftime = self.get_format_increase_decrease_date(initial_date, 5)
-
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[2]"},
-            {"path": "//*[@id='reservesoform']/div/div/input"},
-            {"path": "//*[@id='reservesoform']/div/div/input",
-             "extra_action": "setvalue", "value": "2"},
-            {"path": "//*[@id='reservesoform']/div[2]/div/div/input"},
-            {"path": "//*[@data-day='%s']" % initial_date_strftime},
-            {"path": "//*[@id='reservesoform']/div[3]/div/div/input"},
-            {"path": "//*[@data-day='%s']" % final_date_strftime},
-            {"path": self.get_save_button_modal("reservesomodal")}
-        ]
-        self.create_gif_process(path_list, "reserve_shelfobject")
-
-    def test_increase_shelfobject(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[3]"},
-            {"path": "//*[@id='increasesoform']/div/div/input"},
-            {"path": "//*[@id='increasesoform']/div/div/input",
-             "extra_action": "setvalue", "value": "5"},
-            {"path": "//*[@id='increasesoform']/div[2]/div/input"},
-            {"path": "//*[@id='increasesoform']/div[2]/div/input",
-             "extra_action": "setvalue", "value": "R78509"},
-            {"path": "//*[@id='increasesoform']/div[3]/div/span/span/span"},
-            {"path": "/html/body/span/span/span[2]/ul/li"},
-            {"path": self.get_save_button_modal("increasesomodal")},
-        ]
-        self.create_gif_process(path_list, "increase_shelfobject")
-
-    def test_transfer_out_shelfobject(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[4]"},
-            {"path": self.get_save_button_modal("reservesomodal")}
-        ]
-        self.create_gif_process(path_list, "transfer_out_obj_id_modal")
-
-    def test_decrease_shelfobject(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[5]"},
-            {"path": "//*[@id='decreasesoform']/div/div/input"},
-            {"path": "//*[@id='decreasesoform']/div/div/input",
-             "extra_action": "setvalue", "value": "3"},
-            {"path": "//*[@id='decreasesoform']/div[2]/div/input"},
-            {"path": "//*[@id='decreasesoform']/div[2]/div/input",
-             "extra_action": "setvalue", "value": "Práctica de laboratorio"},
-            {"path": self.get_save_button_modal("decreasesomodal")}
-        ]
-        self.create_gif_process(path_list, "decrease_shelfobject")
-
-    def test_view_shelfobject_logs(self):
-        path_list1 = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[6]"}
-        ]
-        general_path_list = [path_list1, self.scroll_shelfobject_info]
-        self.create_gif_by_change_focus_tab(general_path_list, ["shelfobjectlog"],"view_shelfobject_logs")
-
-    def test_manage_shelfobject_container(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[7]"},
-            {"path": self.get_save_button_modal("managecontainermodal")}
-        ]
-        self.create_gif_process(path_list, "manage_shelfobject_container")
-
-    def test_move_shelfobject(self):
-        path_list = self.buttons_actions_path + [
+        self.move_shelfobject_form_path = self.buttons_actions_path + [
             {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[8]"},
-            {"path": self.get_save_button_modal("managecontainermodal")}
+            {"path": "//*[@id='movesocontainerform']/div/div/span/span/span",
+             "scroll": "$('#movesocontainermodal').scrollTop(150);"},
+            {"path": "/html/body/span/span/span[2]/ul/li[1]"},
+            {"path": "//*[@id='movesocontainerform']/div[2]/div/span/span/span"},
+            {"path": "/html/body/span/span/span[2]/ul/li[1]"},
+            {"path": "//*[@id='movesocontainerform']/div[3]/div/span/span/span"},
+            {"path": "/html/body/span/span/span[2]/ul/li[1]"},
         ]
-        self.create_gif_process(path_list, "move_shelfobject")
 
-    def test_download_shelfobject_info(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[9]"}
-        ]
-        self.create_gif_process(path_list, "download_shelfobject_info")
+    # def test_view_shelfobject_detail(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a"},
+    #         {"path": "//*[@id='shelfobject_detail_modal_body']",
+    #          "extra_action": "script",
+    #          "value": "$('#shelfobject_detail_modal_body').scrollTop(150);"},
+    #         {"path": "//*[@id='shelfobject_detail_modal_body']",
+    #          "extra_action": "script",
+    #          "value": "$('#shelfobject_detail_modal_body').scrollTop(300);"},
+    #         {"path": "//*[@id='shelfobject_detail_modal_body']",
+    #          "extra_action": "script",
+    #          "value": "$('#shelfobject_detail_modal_body').scrollTop(450);"},
+    #         {"path": "//*[@id='detail_modal_container']/div/div/div[3]/button"}
+    #     ]
+    #     self.create_gif_process(path_list, "view_shelfobject_detail")
+    #
+    # def test_reserve_shelfobject(self):
+    #     initial_date, initial_date_strftime = self.get_format_increase_decrease_date(now(), 1)
+    #     final_date, final_date_strftime = self.get_format_increase_decrease_date(initial_date, 5)
+    #
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[2]"},
+    #         {"path": "//*[@id='reservesoform']/div/div/input"},
+    #         {"path": "//*[@id='reservesoform']/div/div/input",
+    #          "extra_action": "setvalue", "value": "2"},
+    #         {"path": "//*[@id='reservesoform']/div[2]/div/div/input"},
+    #         {"path": "//*[@data-day='%s']" % initial_date_strftime},
+    #         {"path": "//*[@id='reservesoform']/div[3]/div/div/input"},
+    #         {"path": "//*[@data-day='%s']" % final_date_strftime},
+    #         {"path": self.get_save_button_modal("reservesomodal")}
+    #     ]
+    #     self.create_gif_process(path_list, "reserve_shelfobject")
+    #
+    # def test_increase_shelfobject(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[3]"},
+    #         {"path": "//*[@id='increasesoform']/div/div/input"},
+    #         {"path": "//*[@id='increasesoform']/div/div/input",
+    #          "extra_action": "setvalue", "value": "5"},
+    #         {"path": "//*[@id='increasesoform']/div[2]/div/input"},
+    #         {"path": "//*[@id='increasesoform']/div[2]/div/input",
+    #          "extra_action": "setvalue", "value": "R78509"},
+    #         {"path": "//*[@id='increasesoform']/div[3]/div/span/span/span"},
+    #         {"path": "/html/body/span/span/span[2]/ul/li"},
+    #         {"path": self.get_save_button_modal("increasesomodal")},
+    #     ]
+    #     self.create_gif_process(path_list, "increase_shelfobject")
+    #
+    # def test_transfer_out_shelfobject(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[4]"},
+    #         {"path": "//*[@id='transfer_out_form']/div/div/input"},
+    #         {"path": "//*[@id='transfer_out_form']/div/div/input",
+    #          "extra_action": "setvalue", "value": "2"},
+    #         {"path": "//*[@id='transfer_out_form']/div[2]/div/span/span/span"},
+    #         {"path": "/html/body/span/span/span[2]/ul/li[2]"},
+    #         {"path": self.get_save_button_modal("transfer_out_obj_id_modal")}
+    #     ]
+    #     self.create_gif_process(path_list, "transfer_out_shelfobject")
+    #
+    # def test_transfer_out_shelfobject_refuse(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[4]"},
+    #         {"path": "//*[@id='transfer_out_form']/div/div/input"},
+    #         {"path": "//*[@id='transfer_out_form']/div/div/input",
+    #          "extra_action": "setvalue", "value": "2"},
+    #         {"path": "//*[@id='transfer_out_form']/div[2]/div/span/span/span"},
+    #         {"path": "/html/body/span/span/span[2]/ul/li[2]"},
+    #         {"path": "//*[@id='transfer_out_form']/div[3]/div/span"},
+    #         {"path": self.get_save_button_modal("transfer_out_obj_id_modal")}
+    #     ]
+    #     self.create_gif_process(path_list, "transfer_out_shelfobject_refuse")
+    #
+    # def test_decrease_shelfobject(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[5]"},
+    #         {"path": "//*[@id='decreasesoform']/div/div/input"},
+    #         {"path": "//*[@id='decreasesoform']/div/div/input",
+    #          "extra_action": "setvalue", "value": "3"},
+    #         {"path": "//*[@id='decreasesoform']/div[2]/div/input"},
+    #         {"path": "//*[@id='decreasesoform']/div[2]/div/input",
+    #          "extra_action": "setvalue", "value": "Práctica de laboratorio"},
+    #         {"path": self.get_save_button_modal("decreasesomodal")}
+    #     ]
+    #     self.create_gif_process(path_list, "decrease_shelfobject")
+    #
+    # def test_view_shelfobject_logs(self):
+    #     path_list1 = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[6]"}
+    #     ]
+    #     general_path_list = [path_list1, self.scroll_shelfobject_info]
+    #     self.create_gif_by_change_focus_tab(general_path_list, ["shelfobjectlog"],"view_shelfobject_logs")
+    #
+    # def test_manage_shelfobject_container(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[7]"},
+    #         {"path": self.get_save_button_modal("managecontainermodal")}
+    #     ]
+    #     self.create_gif_process(path_list, "manage_shelfobject_container")
 
-    def test_delete_shelfobject(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[10]"},
-            {"path": self.get_save_button_modal("delete_shelfobject_modal")}
-        ]
-        self.create_gif_process(path_list, "delete_shelfobject")
+    # def test_move_shelfobject_with_clone_container(self):
+    #     path_list = self.move_shelfobject_form_path + [
+    #         {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div/ins"},
+    #         {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div/ins", "extra_action": "script",
+    #          "value": "$('#movesocontainermodal').scrollTop(300);"},
+    #         {"path": "//*[@id='movesocontainerform']/div[6]/div/span/span/span"},
+    #         {"path": "//*[@id='movesocontainerform']/span/span/span[2]/ul/li"},
+    #         {"path": self.get_save_button_modal("movesocontainermodal")}
+    #     ]
+    #     self.create_gif_process(path_list, "move_shelfobject_with_clone_container")
 
-    def test_delete_shelfobject_and_its_container(self):
-        path_list = self.buttons_actions_path + [
-            {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[10]"},
-            {"path": "//*[@id='divcontainer']/span"},
-            {"path": self.get_save_button_modal("delete_shelfobject_modal")}
+    def test_move_shelfobject_with_available_container(self):
+        path_list = self.move_shelfobject_form_path + [
+            {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div[2]/ins"},
+            {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div[2]/ins",
+             "extra_action": "script", "value": "$('#movesocontainermodal').scrollTop(300);"},
+            {"path": "//*[@id='movesocontainerform']/div[7]/div/span/span/span"},
+            {"path": "//*[@id='movesocontainerform']/span/span/span[2]/ul/li"},
+            {"path": self.get_save_button_modal("movesocontainermodal")}
         ]
-        self.create_gif_process(path_list, "delete_shelfobject_and_its_container")
+        self.create_gif_process(path_list, "move_shelfobject_with_available_container")
+
+    def test_move_shelfobject_with_use_source_container(self):
+        path_list = self.move_shelfobject_form_path + [
+            {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div[3]/ins"},
+            {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div[3]/ins",
+             "extra_action": "script", "value": "$('#movesocontainermodal').scrollTop(300);"},
+            {"path": self.get_save_button_modal("movesocontainermodal")}
+        ]
+        self.create_gif_process(path_list, "move_shelfobject_with_use_source_container")
+
+    def test_move_shelfobject_with_new_based_source_container(self):
+        path_list = self.move_shelfobject_form_path + [
+            {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div[4]/ins"},
+            {"path": "//*[@id='movesocontainerform']/div[5]/div/div/div[4]/ins",
+             "extra_action": "script", "value": "$('#movesocontainermodal').scrollTop(300);"},
+            {"path": self.get_save_button_modal("movesocontainermodal")}
+        ]
+        self.create_gif_process(path_list, "move_shelfobject_with_new_based_source_container")
+
+    # def test_download_shelfobject_info(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[9]"}
+    #     ]
+    #     self.create_gif_process(path_list, "download_shelfobject_info")
+    #
+    # def test_delete_shelfobject(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[10]"},
+    #         {"path": self.get_save_button_modal("delete_shelfobject_modal")}
+    #     ]
+    #     self.create_gif_process(path_list, "delete_shelfobject")
+    #
+    # def test_delete_shelfobject_and_its_container(self):
+    #     path_list = self.buttons_actions_path + [
+    #         {"path": "//*[@id='shelfobjecttable']/tbody/tr/td[7]/a[10]"},
+    #         {"path": "//*[@id='divcontainer']/span"},
+    #         {"path": self.get_save_button_modal("delete_shelfobject_modal")}
+    #     ]
+    #     self.create_gif_process(path_list, "delete_shelfobject_and_its_container")
