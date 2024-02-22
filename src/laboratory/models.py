c6ea71b174741e75d72f5154fc6ea7407e1a70aa
Kejebo <kenjen041@gmail.com>
2024-02-22 10:59:26 -0600

    Fixed get_measurement_unit_display method, because returning None when dont has a unit the shelves and shelfobjects

--- src/laboratory/models.py
+++ src/laboratory/models.py
@@ -238,7 +238,7 @@ class ShelfObject(models.Model):
         return self.quantity < self.limit_quantity
 
     def get_measurement_unit_display(self):
-        return str(self.measurement_unit)
+        return str(self.measurement_unit) if self.measurement_unit else _("Unknown")
 
     class Meta:
         verbose_name = _('Shelf object')
@@ -367,7 +367,7 @@ class Shelf(BaseCreationObj):
         return result
 
     def get_measurement_unit_display(self):
-        return str(self.measurement_unit) if self.measurement_unit else ""
+        return str(self.measurement_unit) if self.measurement_unit else _("Unknown")
 
     def __str__(self):
         return '%s %s %s' % (self.furniture, str(self.type), self.name)
