c8f912d15439177162d6365acd1a37290858fee5
Kejebo <kenjen041@gmail.com>
2024-04-01 15:25:38 -0600

    fixed object selenium tests

--- src/laboratory/tests/selenium_tests/manage_laboratory/test_objects.py
+++ src/laboratory/tests/selenium_tests/manage_laboratory/test_objects.py
@@ -162,35 +162,26 @@ class ObjectSeleniumTest(ObjectSeleniumBase):
         path_list = [
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[2]/a[1]","screenshot_name":"reactive_object"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div[1]/a"},
-            {"path": "//*[@id='id_name']", "extra_action": "clearInput"},
             {"path": "//*[@id='id_name']", "extra_action": "setvalue", "value": "BEA143 Beakers 50 mL"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[3]/div[1]/span[2]"},
             {"path": "/html/body/span/span/span[2]/ul/li[2]"},
-            {"path": "//*[@id='id_synonym']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_synonym']", "extra_action": "setvalue", "value":"ss454"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[3]/div[2]/span[2]/span[1]/span"},
             {"path": "/html/body/span/span/span[2]/ul/li[2]"},
-            {"path": "//*[@id='id_code']", "extra_action": "clearIvput"},
             {"path": "//*[@id='id_code']", "extra_action": "setvalue", "value": "CE-456"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[3]/div[3]/span[2]/span[1]/span"},
             {"path": "/html/body/span/span/span/ul/li[2]"},
-            {"path": "//*[@id='id_description']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_description']", "extra_action": "setvalue", "value":
                 "Un vaso de precipitado es un recipiente cilíndrico de vidrio borosilicatado fino que se utiliza muy comúnmente ,",
                 "scroll":"window.scrollTo(0, 400)"
             },
-            {"path": "//*[@id='id_molecular_formula']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_molecular_formula']", "extra_action": "setvalue", "value": "AE2"},
-            {"path": "//*[@id='id_cas_id_number']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_cas_id_number']", "extra_action": "setvalue", "value": "12633468"},
-            {"path": "//*[@id='id_model']", "extra_action": "clearinput",
-             "scroll": "window.scrollTo(0, 400)"},
+            {"path": "//*[@id='id_model']", "scroll": "window.scrollTo(0, 400)"},
             {"path": "//*[@id='id_model']", "extra_action": "setvalue",
              "value": "CA-546"},
-            {"path": "//*[@id='id_serie']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_serie']", "extra_action": "setvalue",
              "value": "B54897"},
-            {"path": "//*[@id='id_plaque']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_plaque']", "extra_action": "setvalue", "value": "5634646465"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[2]/div[9]/span[2]/span[1]/span"},
             {"path": "/html/body/span/span/span/ul/li[2]"},
@@ -214,8 +205,8 @@ class ObjectSeleniumTest(ObjectSeleniumBase):
         self.selenium.get(url= self.live_server_url + str(reverse('laboratory:labindex',kwargs={"org_pk":1,"lab_pk":1})))
         path_list = [
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[2]/a[1]"},
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[4]/a[2]"},
-            {"path": "//*[@id='id_name']", "extra_action": "clearInput"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div[2]/div[2]/div/table/tbody/tr[2]/td[4]/a[1]"},
+            {"path": "//*[@id='id_name']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_name']", "extra_action": "setvalue", "value": "BEA143 Beakers 50 mL"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[3]/div[1]/span[2]"},
             {"path": "/html/body/span/span/span[2]/ul/li[2]"},
@@ -223,7 +214,7 @@ class ObjectSeleniumTest(ObjectSeleniumBase):
             {"path": "//*[@id='id_synonym']", "extra_action": "setvalue", "value":"ss454"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[3]/div[2]/span[2]/span[1]/span"},
             {"path": "/html/body/span/span/span[2]/ul/li[2]"},
-            {"path": "//*[@id='id_code']", "extra_action": "clearIvput"},
+            {"path": "//*[@id='id_code']", "extra_action": "clearinput"},
             {"path": "//*[@id='id_code']", "extra_action": "setvalue", "value": "CE-456"},
             {"path": "/html/body/div[1]/div[1]/div[3]/div/div/div/form/div[1]/div[3]/div[3]/span[2]/span[1]/span"},
             {"path": "/html/body/span/span/span/ul/li[2]"},
@@ -254,7 +245,7 @@ class ObjectSeleniumTest(ObjectSeleniumBase):
         path_list = [
 
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div[2]/div[2]/ul/li[2]/a[1]"},
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[4]/a[3]"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[4]/a[2]"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/form/input[3]"},
 
             ]
--- src/organilab_test/tests/base.py
+++ src/organilab_test/tests/base.py
@@ -27,17 +27,22 @@ class SeleniumBase(StaticLiveServerTestCase):
 
         cls.ob = Screenshot.Screenshot()
         cls.options = webdriver.FirefoxOptions()
+        is_docker = os.getenv("DOCKER_ACTIVE","false").lower() == "true"
 
-        driverpath = os.getenv("CHROMEDRIVER_DIR", '/usr/bin/chromedriver')
-        options = webdriver.ChromeOptions()
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--remote-debugging-port=9222")
-        service = Service(executable_path=driverpath)
+        if is_docker:
 
-        cls.selenium = webdriver.Chrome(options=options, service=service)
+            driverpath = os.getenv("CHROMEDRIVER_DIR", '/usr/bin/chromedriver')
+            options = webdriver.ChromeOptions()
+            options.add_argument("--no-sandbox")
+            options.add_argument("--headless")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--remote-debugging-port=9222")
+            service = Service(executable_path=driverpath)
+
+            cls.selenium = webdriver.Chrome(options=options, service=service)
+        else:
+            cls.selenium = webdriver.Chrome()
         cls.selenium.set_window_size(1280, 720)
 
         cls.tmp = Path(settings.BASE_DIR) / 'tmp'
