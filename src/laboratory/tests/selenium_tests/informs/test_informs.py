d993cc21e603ba4b649c2fbec619bbe68b1d8cc4
Kejebo <kenjen041@gmail.com>
2024-04-04 14:57:16 -0600

    fixed informs selenium tests

--- src/laboratory/tests/selenium_tests/informs/test_informs.py
+++ src/laboratory/tests/selenium_tests/informs/test_informs.py
@@ -29,7 +29,7 @@ class InformSeleniumTest(InformSeleniumBase):
     def test_add_inform(self):
 
         path_list = self.path_base+[
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div/div[2]/form/div[1]/div/input", "extra_action":"clearinput"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[1]/a"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div/div[2]/form/div[1]/div/input", "extra_action":"setvalue", "value":"Prime Informe"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[2]/div/div/div[2]/form/div[2]/div/span/span[1]/span/span[1]"},
             {"path": "/html/body/span/span/span[2]/ul/li[3]"},
@@ -38,10 +38,10 @@ class InformSeleniumTest(InformSeleniumBase):
         self.create_gif_process(path_list, "add_inform")
     def test_review_inform(self):
         path_list = self.path_base+[
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/a[1]"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr/td[5]/a[1]"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/div/div/div[1]/div[1]/div[1]/input","extra_action":"clearinput"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/div/div/div[1]/div[1]/div[1]/input","extra_action":"setvalue","value":"Primer Laboratorio"},
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/div/div/div[1]/div[2]/div[1]/textarea", "sleep":30,"extra_action":"clearinput"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/div/div/div[1]/div[2]/div[1]/textarea", "extra_action":"clearinput"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/div/div/div[1]/div[2]/div[1]/textarea","extra_action":"setvalue","value":"Caída de estante de reactivos"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/button"},
             {"path": "/html/body/div[3]/div/div[3]/button[1]"},
@@ -50,13 +50,13 @@ class InformSeleniumTest(InformSeleniumBase):
         self.finalize_inform()
     def test_remove_inform(self):
         path_list = self.path_base+[
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/a[2]"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr/td[5]/a[2]"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/h3/span"},
         ]
         self.create_gif_process(path_list, "remove_inform")
     def test_crud_inform_observation(self):
         path_list = self.path_base+[
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/a[1]"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr/td[5]/a[2]"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/button"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[2]/div/div/div/div[1]/button"},
             {"path": "/html/body/div[3]/div/div[2]/textarea", "extra_action": "clearinput"},
@@ -91,7 +91,7 @@ class InformSeleniumTest(InformSeleniumBase):
 
     def finalize_inform(self):
         path_list =[
-            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/a[1]"},
+            {"path": "/html/body/div[1]/div/div[3]/div/div/div[1]/div/div/div/div[2]/div/div[2]/div/table/tbody/tr/td[5]/a[1]"},
             {"path": "/html/body/div[1]/div/div[3]/div/div/div/div[2]/div[1]/form/button"},
             {"path": "/html/body/div[3]/div/div[3]/button[1]"},
 
