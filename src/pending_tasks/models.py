81030dd81f26b11e7d06176ca6a986ef767705ed
Johan Muñoz <125044661+JohanMunozMendez@users.noreply.github.com>
2024-09-05 21:09:32 -0600

    Some adjustments in urls, model, admin

--- src/pending_tasks/models.py
+++ src/pending_tasks/models.py
@@ -1,19 +1,24 @@
 from django.db import models
+from django.db.models import Q
+
 from auth_and_perms.models import Profile, Rol
+from presentation.models import AbstractOrganizationRef
+
 
 # Create your models here.
-class PendingTask(models.Model):
-    STATUSES = (
+class PendingTask(AbstractOrganizationRef):
+    STATUS = (
         (0, "Pending"),
         (1, "In process"),
         (2, "Finished")
     )
-    creation_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField(null=True, blank=True)
-    status = models.IntegerField(choices=STATUSES, default=0)
-    validate_profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, null=True, blank=True)
-    rols = models.ManyToManyField(Rol, related_name='pending_tasks')
+    status = models.IntegerField(choices=STATUS, default=0)
+    profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING,
+                                null=True, blank=True)
+    rols = models.ManyToManyField(Rol)
     link = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return f'{self.description} - {self.validate_profile.user.username}'
+        return f'{self.description} - {self.profile}'
+
