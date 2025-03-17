43f358aa8c7a1d346d74a6d16c16f9f09f4e0ecd
Kejebo <kenjen041@gmail.com>
2025-03-17 14:40:58 -0600

    Remove the code was use PayPal dependency

--- src/presentation/views.py
+++ src/presentation/views.py
@@ -7,8 +7,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import CreateView
-from paypal.standard.forms import PayPalPaymentsForm
-
 from presentation.models import Donation, FeedbackEntry
 from presentation.forms import DonateForm, FeedbackEntryForm
 from django.conf import settings
@@ -16,45 +14,6 @@ from django.conf import settings
 
 def index_tutorial(request, org_pk):
     return render(request, 'tutorial.html', context={'org_pk': org_pk})
-
-
-@login_required
-def donate(request):
-    pay = False
-
-    if request.method == "POST":
-        form = DonateForm(request.POST)
-        if form.is_valid():
-            donation = Donation(
-                name=form.cleaned_data['name'], email=form.cleaned_data['email'],
-                amount=form.cleaned_data['amount'], is_donator=form.cleaned_data['is_donator'],
-                is_paid=False)
-            donation.save()
-            paypal_dict = {
-                'business': settings.PAYPAL_RECEIVER_EMAIL,
-                'amount': form.cleaned_data['amount'],
-                'item_name': _('Donate Organilab'),
-                'invoice': str(donation.pk),
-                'currency_code': 'USD',
-                'notify_url': settings.MY_PAYPAL_HOST + reverse('paypal-ipn'),
-                'return_url': settings.MY_PAYPAL_HOST + reverse('donate_success'),
-                'cancel_return': settings.MY_PAYPAL_HOST + reverse('index'),
-            }
-            pay = True
-            paypal_form = PayPalPaymentsForm(initial=paypal_dict)
-            paypal_form.button_type = "donate"
-        return render(
-            request, 'presentation/donate_organilab.html', {'paypal_form': paypal_form, 'pay': pay, 'form':form})
-    else:
-        form = DonateForm()
-        return render(
-            request, 'presentation/donate_organilab.html', {'form': form, 'pay': pay})
-
-
-def donate_success(request):
-    messages.success(request, _("Your donation was completed successfully, thank you for support this project!"))
-    return HttpResponseRedirect(reverse('donate'))
-
 
 @method_decorator(login_required(), name='dispatch')
 class FeedbackView(CreateView):
