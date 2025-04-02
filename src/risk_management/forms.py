2c977709a8d170c8e38ccb08e1525597a85f9f87
Kejebo <kenjen041@gmail.com>
2025-04-02 16:54:20 -0600

    Fixed forms

--- src/risk_management/forms.py
+++ src/risk_management/forms.py
@@ -3,20 +3,22 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 from djgentelella.forms.forms import GTForm
+from djgentelella.widgets.files import FileChunkedUpload
 from djgentelella.widgets.wysiwyg import TextareaWysiwyg
 
-from .models import Regent
+from .models import Regent, Structure
 from laboratory.models import OrganizationStructureRelations, Laboratory
 from laboratory.utils import get_user_laboratories, get_users_from_organization
 from risk_management.models import RiskZone, IncidentReport, ZoneType, Buildings
 from djgentelella.widgets import core as djgentelella
 from djgentelella.widgets import core as genwidgets
 from urllib.parse import quote
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 from .utils import get_regents_from_organization
 
+from djgentelella.widgets.selects import AutocompleteSelect, AutocompleteSelectMultiple
 
 class RiskZoneCreateForm(forms.ModelForm,GTForm):
     def __init__(self, *args, **kwargs):
@@ -138,30 +140,34 @@ class BuildingsForm(GTForm, forms.ModelForm):
 
     class Meta:
         model = Buildings
-        fields = ['name', 'laboratories', 'is_asociaty_buildings',
-                  'nearby_buildings',  'geolocation', 'phone', 'manager',
-                  'area', 'plans', 'security_plan', 'has_water_resources',
-                  'has_nearby_sites', 'regents', 'regulatory_plans']
+        fields = "__all__"
+        exclude = ['organization']
         widgets = {
             'name': djgentelella.TextInput,
-            'laboratories': genwidgets.SelectMultiple(),
+            'laboratories': djgentelella.SelectMultiple,
             'is_asociaty_buildings': djgentelella.YesNoInput(
-                shparent='.asgrid',
+                shparent='.mb-3',
                 attrs={
                     "rel": ["#id_nearby_buildings"]
                 },
             ),
-            'nearby_buildings': genwidgets.SelectMultiple(),
+            'nearby_buildings': djgentelella.SelectMultiple,
+            #AutocompleteSelectMultiple(
+            #   'risk_building',
+            #   attrs={
+            #    'data-s2filter-org_pk': '#org',
+
+        #}),
             'geolocation': djgentelella.TextInput,
             'phone': djgentelella.TextInput,
             'manager': djgentelella.Select,
-            'regents': genwidgets.SelectMultiple(),
+            'regents': djgentelella.SelectMultiple,
             'has_water_resources': djgentelella.YesNoInput,
-            'has_nearby_sites': djgentelella.FileInput,
+            'has_nearby_sites': FileChunkedUpload,
             'area': djgentelella.FloatInput,
-            'plans': djgentelella.FileInput,
-            'security_plan': djgentelella.FileInput,
-            'regulatory_plans': djgentelella.FileInput,
+            'plans': FileChunkedUpload,
+            'security_plan': FileChunkedUpload,
+            'regulatory_plans': FileChunkedUpload,
 
         }
 
@@ -184,4 +190,60 @@ class RegentForm(GTForm, forms.ModelForm):
         fields = ["user", "buildings"]
         widgets = {
             'user': djgentelella.Select,
+        }
+
+
+class StructureForm(GTForm, forms.ModelForm):
+    default_render_type = "as_grid"
+    grid_representation = [
+        [
+            ["name",
+             "type_building",
+             "buildings",
+             "manager",
+             ],
+            [
+             "area",
+             "measuerement_unit",
+             "geolocation"
+             ]
+
+        ],
+
+
+    ]
+    def __init__(self, *args, **kwargs):
+        org_pk = kwargs.pop('org_pk', None)
+        instance = kwargs.get('instance', None)
+        super(StructureForm, self).__init__(*args, **kwargs)
+        self.fields['geolocation'].widget.attrs['class'] = 'form-control'
+        self.fields['geolocation'].label = _('Geolocation')
+        self.fields['type_building'].widget.attrs['add_url'] = reverse_lazy('laboratory:add_structure_type_catalog')
+
+
+        if org_pk:
+            if instance:
+                self.fields['buildings'].queryset = (
+                    Buildings.objects.filter(organization__pk=org_pk).
+                    exclude(pk=instance.pk))
+            else:
+                self.fields['buildings'].queryset = (
+                Buildings.objects.filter(organization__pk=org_pk))
+            self.fields['manager'].queryset = (
+                User.objects.filter(pk__in=get_users_from_organization(org_pk)))
+
+    class Meta:
+        model = Structure
+        fields = "__all__"
+        exclude = ['organization']
+        widgets = {
+            'name': djgentelella.TextInput,
+            'buildings': djgentelella.SelectMultiple,
+            'geolocation': djgentelella.TextInput,
+            'manager': djgentelella.Select,
+            'area': djgentelella.FloatInput,
+            'measuerement_unit': djgentelella.Select,
+            'area': djgentelella.FloatInput,
+            'type_building': djgentelella.SelectWithAdd(attrs={'add_url': "#", 'data-otrono': 1}),
+
         }
