41365095de1f0aee4fb8fdca06405cc37525548c
Kejebo <kenjen041@gmail.com>
2025-04-21 16:49:20 -0600

    Fixed urls to use buildings in incident modules

--- src/risk_management/urls.py
+++ src/risk_management/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('riskzone/<int:pk>/update/', zoneviews.ZoneEdit.as_view(), name='riskzone_update'),
     path('riskzone/<int:pk>/delete/', zoneviews.ZoneDelete.as_view(), name='riskzone_delete'),
 
-    path('incident/<int:lab_pk>/list/', incidents.IncidentReportList.as_view(), name='incident_list'),
-    path('incident/<int:lab_pk>/create/', incidents.IncidentReportCreate.as_view(), name='incident_create'),
+    path('incident/<int:building_pk>/list/', incidents.IncidentReportList.as_view(), name='incident_list'),
+    path('incident/<int:building_pk>/create/', incidents.IncidentReportCreate.as_view(), name='incident_create'),
     path('incident/<int:lab_pk>/<int:pk>/update/', incidents.IncidentReportEdit.as_view(), name='incident_update'),
     path('incident/<int:lab_pk>/<int:pk>/detail/', incidents.IncidentReportDetail.as_view(), name='incident_detail'),
     path('incident/<int:lab_pk>/<int:pk>/delete/', incidents.IncidentReportDelete.as_view(), name='incident_delete'),
