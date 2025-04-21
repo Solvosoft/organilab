9907e86e7b766c5b650e0c7494845691859162e1
Kejebo <kenjen041@gmail.com>
2025-04-21 16:47:28 -0600

    Validated when choice the option all in labroom report

--- src/report/views/lab_room.py
+++ src/report/views/lab_room.py
@@ -11,10 +11,11 @@ def get_dataset(report, column_list=None):
     furniture_list = get_furniture_queryset_by_filters(report)
     attrs = {}
     if "object_type" in report.data:
-        attrs['object__type'] = report.data["object_type"]
-        if "is_precursor" in report.data and report.data["object_type"] == '0':
-            attrs['object__sustancecharacteristics__is_precursor'] = (
-                report.data)["is_precursor"]
+        if report.data["object_type"]:
+            attrs['object__type'] = report.data["object_type"]
+            if "is_precursor" in report.data and report.data["object_type"] == '0':
+                attrs['object__sustancecharacteristics__is_precursor'] = (
+                    report.data)["is_precursor"]
     for furniture in furniture_list:
         objects = furniture.get_objects().filter(**attrs)
         for shelfobject in objects:
