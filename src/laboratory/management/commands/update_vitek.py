0a63088e39c8127b14be5a13c534a89d0b346ae3
Kejebo <kenjen041@gmail.com>
2025-10-02 10:55:42 -0600

    New command to update vitek containers name

---
+++ src/laboratory/management/commands/update_vitek.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from laboratory.models import ShelfObject, ObjectLogChange
+
+
+
+class Command(BaseCommand):
+
+    help = "Add Shelf Object Id in ObjectLogChange"
+
+    def update_shelf_object(self):
+        queryset = ShelfObject.objects.filter(in_where_laboratory__pk=137,
+                                              object__type=0,
+                                              object__name__startswith="Vitek")
+        for query in queryset:
+            obj = query.container.object
+            obj.name = f"Caja Vitek {obj.materialcapacity.capacity} {obj.materialcapacity.capacity_measurement_unit.description}"
+            obj.save()
+        containers = queryset.filter(container__object__materialcapacity__capacity=20.0)
+        container = containers.first()
+        container_sixty = containers.filter(container__object__materialcapacity__capacity=60.0).first()
+        containers_delete = []
+        for shelf_obj in containers:
+            shelf_obj.container = container
+            if shelf_obj.quantity> 40 and shelf_obj.quantity < 60:
+                shelf_obj.container = container_sixty
+            shelf_obj.save()
+            if container.pk != shelf_obj.container.pk:
+                containers_delete.append(shelf_obj.container.pk)
+
+        container.save()
+        ShelfObject.objects.filter(pk__in=containers_delete).delete()
+
+
+    def handle(self, *args, **options):
+        self.update_shelf_object()
