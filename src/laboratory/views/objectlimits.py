351626adc69126220a461f53e973bee7084e120d
Kejebo <kenjen041@gmail.com>
2025-11-11 17:53:58 -0600

    Validate is user can access to the view

--- src/laboratory/views/objectlimits.py
+++ src/laboratory/views/objectlimits.py
@@ -1,13 +1,20 @@
+from django.contrib.auth.decorators import permission_required
+from django.http import Http404
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 from laboratory.forms import ReactiveStockForm
 from laboratory.models import Object
+from laboratory.utils import check_user_access_kwargs_org_lab
 
-
+@method_decorator(permission_required("laboratory.view_object"), name="dispatch")
 class ReactiveStockDashboard(TemplateView):
     template_name = "laboratory/objectlimit/dashboard.html"
-
+    def get(self, request, *args, **kwargs):
+        if not check_user_access_kwargs_org_lab(self.kwargs.get("org_pk", None), self.kwargs.get("lab_pk", None), request.user):
+            raise Http404()
+        return super().get(request, *args, **kwargs)
     def get_context_data(self, **kwargs):
         context = super(ReactiveStockDashboard, self).get_context_data()
         context["org_pk"] = self.kwargs["org_pk"]
