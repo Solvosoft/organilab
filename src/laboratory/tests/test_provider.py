e7929aa0140baa14031e64ff42fe043df5981177
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-02-09 17:40:15 -0600

    fixed test provider

--- src/laboratory/tests/test_provider.py
+++ src/laboratory/tests/test_provider.py
@@ -8,58 +8,52 @@ class ProviderViewTest(BaseLaboratorySetUpTest):
 
     def test_provider_list(self):
         url = reverse(
-            "laboratory:list_provider",
+            "laboratory:provider_view",
             kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk},
         )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "LP Company")
-        self.assertTemplateUsed(response, template_name="laboratory/provider_list.html")
 
     def test_udpate_provider(self):
-        provider = Provider.objects.first()
         url = reverse(
-            "laboratory:update_lab_provider",
-            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": provider.pk},
+            "laboratory:api-provider-list",
+            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk},
         )
-        response_get = self.client.get(url)
-        self.assertEqual(response_get.status_code, 200)
-        self.assertContains(response_get, "Karine Products")
         data = {
             "name": "Karine Chemicals Products",
             "phone_number": "(506)2209-2209",
             "email": "karinechemicalsproductos@cr.com",
             "legal_identity": "3-8764-8354",
-            "laboratory": 1,
+            "laboratory": self.lab.pk,
         }
-        response_post = self.client.post(url, data=data)
-        self.assertEqual(response_post.status_code, 302)
-        success_url = reverse(
-            "laboratory:list_provider",
-            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk},
+
+        response = self.client.post(url, data=data, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(Provider.objects.filter(name=data["name"]).exists())
+
+    def test_delete_provider(self):
+        url = reverse(
+            "laboratory:api-provider-detail",
+            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk, "pk": 2},
         )
-        self.assertRedirects(response_post, success_url)
-        self.assertIn(
-            "Karine Chemicals Products",
-            list(Provider.objects.values_list("name", flat=True)),
-        )
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Provider.objects.filter(pk=2).exists())
 
     def test_add_provider(self):
+        url = reverse(
+            "laboratory:api-provider-list",
+            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk},
+        )
         data = {
             "name": "NatVou",
             "phone_number": "(506)2240-8035",
             "email": "natvou@example.com",
             "legal_identity": "3-876-763",
-            "laboratory": self.lab,
+            "laboratory_id": self.lab.pk,
         }
-        url = reverse(
-            "laboratory:add_provider",
-            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk},
-        )
-        response = self.client.post(url, data=data)
-        success_url = reverse(
-            "laboratory:list_provider",
-            kwargs={"org_pk": self.org.pk, "lab_pk": self.lab.pk},
-        )
-        self.assertRedirects(response, success_url)
-        self.assertIn("NatVou", list(Provider.objects.values_list("name", flat=True)))
+
+        response = self.client.post(url, data=data, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(Provider.objects.filter(name=data["name"]).exists())
