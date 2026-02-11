687ce22a6459880b99dd29bf09ba280aef1ec896
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-02-11 09:52:34 -0600

    eliminacion de registro y de seccion de donaciones

--- src/auth_and_perms/middleware.py
+++ src/auth_and_perms/middleware.py
@@ -13,40 +13,40 @@ from django.utils.deprecation import MiddlewareMixin
 from auth_and_perms.utils import get_ip_address
 
 
-class ProfileLanguageMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        return response
-
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        user = request.user
-        if not user.is_authenticated or not user.is_active:
-            return
-        if not hasattr(user, "profile"):
-            reguser = RegistrationUser.objects.filter(user=user).first()
-            if reguser:
-                auth_logout(request)
-                if reguser.registration_method == 1:
-                    return redirect(
-                        reverse("auth_and_perms:user_org_creation_totp", args=[user.pk])
-                    )
-                if reguser.registration_method == 2:
-                    return redirect(
-                        reverse(
-                            "auth_and_perms:create_profile_by_digital_signature",
-                            args=[user.pk],
-                        )
-                    )
-            raise Http404("User has not profile")
-
-        profile = user.profile
-        default_profile_lang = profile.language
-        languages = list(dict(settings.LANGUAGES).keys())
-        if default_profile_lang in languages:
-            translation.activate(default_profile_lang)
+# class ProfileLanguageMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+#
+#     def __call__(self, request):
+#         response = self.get_response(request)
+#         return response
+#
+#     def process_view(self, request, view_func, view_args, view_kwargs):
+#         user = request.user
+#         if not user.is_authenticated or not user.is_active:
+#             return
+#         if not hasattr(user, "profile"):
+#             reguser = RegistrationUser.objects.filter(user=user).first()
+#             if reguser:
+#                 auth_logout(request)
+#                 if reguser.registration_method == 1:
+#                     return redirect(
+#                         reverse("auth_and_perms:user_org_creation_totp", args=[user.pk])
+#                     )
+#                 if reguser.registration_method == 2:
+#                     return redirect(
+#                         reverse(
+#                             "auth_and_perms:create_profile_by_digital_signature",
+#                             args=[user.pk],
+#                         )
+#                     )
+#             raise Http404("User has not profile")
+#
+#         profile = user.profile
+#         default_profile_lang = profile.language
+#         languages = list(dict(settings.LANGUAGES).keys())
+#         if default_profile_lang in languages:
+#             translation.activate(default_profile_lang)
 
 
 class ImpostorMiddleware(MiddlewareMixin):
