9f969a2d3ad50e92bf86433b8a973a23f5f03d92
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-02-12 16:35:44 -0600

    Update views.py

--- src/sga/views/danger_substance/views.py
+++ src/sga/views/danger_substance/views.py
@@ -1,11 +1,20 @@
+from django.conf import settings
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
 
+from auth_and_perms.organization_utils import user_is_allowed_on_organization
+from laboratory.models import OrganizationStructure
 from sga.forms import DangerSubstanceForm
 
 
 @permission_required("sga.view_dangersubstance")
 def danger_substance_view(request, org_pk):
+    organization = get_object_or_404(
+        OrganizationStructure.objects.using(settings.READONLY_DATABASE),
+        pk=org_pk,
+    )
+    user_is_allowed_on_organization(request.user, organization)
     return render(
         request,
         "danger_substance/danger_substance.html",
