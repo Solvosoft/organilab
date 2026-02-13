607f5abd9bc9ccf75333bcf1c275e78df219d90f
Kejebo <kenjen041@gmail.com>
2026-02-13 09:59:31 -0600

    avance en captura de inventario

--- src/report/views/regency.py
+++ src/report/views/regency.py
@@ -32,9 +32,108 @@ def get_dataset_report(report, column_list=None):
     )
     objs = Object.objects.filter(
         pk__in=logs.values_list("object__pk", flat=True),
-        has_threshold=True,
+        sustancecharacteristics__isnull=False,
+    ).distinct()
+    units = Catalog.objects.filter(
+        pk__in=logs.values_list("measurement_unit", flat=True)
+    )
+    fourth_list = []
+    organization = OrganizationStructure.objects.filter(
+        pk=report.data["organization"]
+    ).first()
+    data = {
+        "organization": organization,
+        "year": report.data["years"],
+        "task_report": report,
+    }
+
+    report_regency = RegencyReport.objects.create(**data)
+    reactive_list = []
+
+    for reactive in objs:
+        third_square = False
+        total_shelfobjects = 0
+        density = getattr(reactive.sustancecharacteristics, "density", None)
+
+        for unit in units:
+            quantity = (
+                logs.filter(object=reactive, measurement_unit=unit)
+                .distinct()
+                .aggregate(Sum("diff_value", default=0))["diff_value__sum"]
+            )
+            if quantity > 0:
+                total_shelfobjects += get_conversion_units_to_kilograms(
+                    unit, quantity, density
+                )  # list 3
+
+        reactive_list.append(
+            RegencyReportBuilder(
+                report=report_regency,
+                substance=reactive,
+                danger_category=3,
+                total=total_shelfobjects,
+                break_threshold=(reactive.threshold * 1000) < total_shelfobjects,
+            )
+        )
+
+        if not third_square and hasattr(reactive, "sustancecharacteristics"):
+            # list 4
+            danger_categories_list = get_danger_categories(
+                reactive,
+                total_shelfobjects,
+            )
+            if len(danger_categories_list) > 0:
+                if danger_categories_list[-1].threshold * 1000 < total_shelfobjects:
+                    danger_categories_list.append(_("Yes Exceeds Threshold"))
+                else:
+                    danger_categories_list.append(_("No Exceeds Threshold"))
+                fourth_list.extend(danger_categories_list)
+
+    total_fifth_list = {
+        "enviroment": {"substance": 0, "category_threshold": 0, "total": 0},
+        "health": {"substance": 0, "category_threshold": 0, "total": 0},
+        "physical": {"substance": 0, "category_threshold": 0, "total": 0},
+    }
+    if len(fourth_list) > 0:
+        for danger in fourth_list:
+            total_fifth_list[danger[2]]["substance"] += danger[3]
+            total_fifth_list[danger[2]]["category_threshold"] += danger[4]
+            try:
+                total_fifth_list[danger[2]]["total"] += danger[3] / danger[4]
+            except ZeroDivisionError:
+                total_fifth_list[danger[2]]["total"] = 0
+
+            reactive_list.append(
+                RegencyReportBuilder(
+                    report=report_regency,
+                    substance=reactive,
+                    danger_category=4,
+                    total=total_fifth_list[danger[2]]["total"],
+                )
+            )
+
+        for danger in total_fifth_list:
+            total_fifth_list[danger]["category_threshold"] /= 1000
+
+    totals = 0
+    if reactive_list:
+        RegencyReportBuilder.objects.bulk_create(reactive_list)
+        totals = len(reactive_list)
+
+    return totals
+
+
+def get_dataset_report_x(report, column_list=None):
+    dataset = []
+    filters = {"object__type": 0}
+    logs = ObjectLogChange.objects.filter(
+        organization_where_action_taken__pk=report.data["organization"],
+        update_time__year=report.data["years"],
+        laboratory__pk__in=report.data["laboratory"],
+    )
+    objs = Object.objects.filter(
+        pk__in=logs.values_list("object__pk", flat=True),
         is_dangerous=True,
-        threshold__isnull=False,
     ).distinct()
     units = Catalog.objects.filter(
         pk__in=logs.values_list("measurement_unit", flat=True)
