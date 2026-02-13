7daa2b0a62c177cd05ee16caaae7036ef4fb8b3f
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-02-13 14:24:06 -0600

    Update models.py

--- src/risk_management/models.py
+++ src/risk_management/models.py
@@ -329,11 +329,14 @@ class EstablishmentLogs(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
-
+    physical = models.BooleanField(default=False)
+    health = models.BooleanField(default=False)
+    environmental = models.BooleanField(default=False)
     establishment_status = models.CharField(
         max_length=50, null=True, blank=True, verbose_name=_("Establishment Status")
     )
     table_content = models.JSONField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.establishment_status
