3cdec0f92a244cdf515f9e5abd55dfa48444ee98
Luis Zárate Montero <luis.zarate@solvosoft.com>
2026-02-22 14:05:25 -0600

    Matriz de compatibilidad

--- src/report/views/reports_org.py
+++ src/report/views/reports_org.py
@@ -31,6 +31,7 @@ from report.forms import (
     LaboratoryRoomReportForm,
     ValidateFurnitureForm,
     PrecursorFilterForm,
+    CompatibilityReportForm,
 )
 from risk_management.models import RiskZone
 
@@ -427,6 +428,34 @@ class FurnitureReportView(ListView):
                 "title_view": title,
                 "report_urlnames": ["reports_furniture_detail"],
                 "form": LaboratoryRoomReportForm(initial=initial_data),
+            }
+        )
+        return context
+
+
+@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+class CompatibilityReport(ListView):
+    model = RiskZone
+    template_name = "report/base_report_form_view.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(CompatibilityReport, self).get_context_data(**kwargs)
+        title = _("SGA Compatibility Table")
+        context.update(
+            {
+                "title_view": title,
+                "report_urlnames": ["compatibility_report"],
+                "form": CompatibilityReportForm(
+                    initial={
+                        "name": slugify(
+                            title + " " + now().strftime("%x").replace("/", "-")
+                        ),
+                        "title": title,
+                        "organization": self.org,
+                        "report_name": "compatibility_report",
+                    },
+                    org_pk=self.org,
+                ),
             }
         )
         return context
