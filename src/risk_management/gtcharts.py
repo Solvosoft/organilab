0f6ab2fdf0a8414da5a49b40ab7eabb8e7f082de
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-02-23 08:19:26 -0600

    fix name zone_pk to extraParams

--- src/risk_management/gtcharts.py
+++ src/risk_management/gtcharts.py
@@ -807,7 +807,7 @@ class EstablishmentLogsClassChart(BaseChart, HorizontalBarChart):
     def retrieve(self, request, pk):
         self.request = request
         self.organization = get_object_or_404(OrganizationStructure, pk=pk)
-        self.pk = request.GET.get('zone_pk')
+        self.pk = request.GET.get('extraParams')
         data = self.get_graph_data()
         serializer = self.serializer_class(data)
         return Response(serializer.data)
