916a2f0cb110d4bedae2012c1354d3c8c6539a1b
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-02-27 13:45:52 -0600

    updated views perms in reports

--- src/report/views/reports_org.py
+++ src/report/views/reports_org.py
@@ -39,7 +39,7 @@ from laboratory.models import Laboratory
 from risk_management.models import RiskZone
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class ObjectList(ListView):
     model = Object
     template_name = "report/base_report_form_view.html"
@@ -87,7 +87,7 @@ class ObjectList(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class LimitedShelfObjectList(ListView):
     model = ShelfObject
     template_name = "report/base_report_form_view.html"
@@ -112,7 +112,7 @@ class LimitedShelfObjectList(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class ReactivePrecursorObjectList(ListView):
     model = Object
     template_name = "report/base_report_form_view.html"
@@ -142,7 +142,7 @@ class ReactivePrecursorObjectList(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class LogObjectView(ReportListView):
     model = ObjectLogChange
     template_name = "report/base_report_form_view.html"
@@ -169,7 +169,7 @@ class LogObjectView(ReportListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class PrecursorsView(ReportListView):
     model = PrecursorReport
     template_name = "report/precursor_report.html"
@@ -313,7 +313,7 @@ class DiscardShelfReportView(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class ReactiveReport(ListView):
     model = ShelfObject
     template_name = "report/base_report_form_view.html"
@@ -340,7 +340,7 @@ class ReactiveReport(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class RiskZoneReport(ListView):
     model = RiskZone
     template_name = "report/base_report_form_view.html"
@@ -368,7 +368,7 @@ class RiskZoneReport(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class ReactiveStockReport(ListView):
     model = ShelfObject
     template_name = "report/base_report_form_view.html"
@@ -436,7 +436,7 @@ class FurnitureReportView(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class CompatibilityReport(ListView):
     model = RiskZone
     template_name = "report/base_report_form_view.html"
@@ -464,7 +464,7 @@ class CompatibilityReport(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class HazardMapReport(ListView):
     model = Laboratory
     template_name = "report/base_report_form_view.html"
@@ -492,7 +492,7 @@ class HazardMapReport(ListView):
         return context
 
 
-@method_decorator(permission_required("laboratory.view_report"), name="dispatch")
+@method_decorator(permission_required("laboratory.do_report"), name="dispatch")
 class PrecursorReportValuesView(ListView):
     model = PrecursorReportValues
     template_name = "report/precursor_report_values_view.html"
