c47f9ee06be533505e1e11d1c4b932792b46f89d
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-03-06 13:04:29 -0600

    arreglos en reservaciones

--- src/reservations_management/views.py
+++ src/reservations_management/views.py
@@ -2,8 +2,9 @@ from django.contrib.admin.models import CHANGE
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse
 
+from auth_and_perms.models import ProfilePermission
 from laboratory.models import OrganizationStructure
-from laboratory.utils import organilab_logentry
+from laboratory.utils import organilab_logentry, get_lab_ids
 from laboratory.views.djgeneric import ListView, UpdateView
 from .forms import ReservationsForm, ProductForm
 from .models import Reservations, ReservedProducts
@@ -16,13 +17,15 @@ class ReservationsListView(PermissionRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        reservation_list = list(
-            ReservedProducts.objects.filter(
-                laboratory__profile__user_id=self.request.user.id,
-                organization__pk=self.org,
-                reservation__isnull=False,
-            ).values_list("reservation__pk", flat=True)
-        )
+        org = OrganizationStructure.objects.filter(pk=self.kwargs["org_pk"]).first()
+        user = self.request.user
+        profile = getattr(user, "profile", None)
+        lab_ids = get_lab_ids(org, profile)
+        reservation_list = ReservedProducts.objects.filter(
+            reservation__isnull=False,
+            laboratory__id__in=lab_ids,
+        ).values_list("reservation__pk", flat=True).distinct()
+
         context["reservations"] = Reservations.objects.filter(
             pk__in=reservation_list, status=self.kwargs["status"]
         )
