f904b6a58e07556da3a98333a44c7d6f91d42dfa
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-03-11 12:34:49 -0600

    ultimas correcciones

---
+++ copias/copias.py
@@ -0,0 +1,133 @@
+copia = {"labindex": [
+    {
+        "name": "Add Report",
+        "category": "Report",
+        "permission": "laboratory.do_report",
+    },
+    {
+        "name": "View Report",
+        "category": "Report",
+        "permission": "laboratory.view_report",
+    },
+    {
+        "name": "Delete Laboratory",
+        "category": "Laboratory",
+        "permission": "laboratory.delete_laboratory",
+    },
+    {
+        "name": "View Provider Laboratory",
+        "category": "Provider Laboratory",
+        "permission": "laboratory.view_provider",
+    },
+    {
+        "name": "Add Provider Laboratory",
+        "category": "Provider Laboratory",
+        "permission": "laboratory.add_provider",
+    },
+    {
+        "name": "View Laboratory",
+        "category": "Laboratory",
+        "permission": "laboratory.view_laboratory",
+    },
+    {
+        "name": "Change Laboratory",
+        "category": "Laboratory",
+        "permission": "laboratory.change_laboratory",
+    },
+    {
+        "name": "View Procedure",
+        "category": "Procedure",
+        "permission": "academic.view_procedure",
+    },
+    {
+        "name": "View My Procedure",
+        "category": "My Procedure",
+        "permission": "academic.view_myprocedure",
+    },
+    {
+        "name": "View Profile",
+        "category": "Profile",
+        "permission": "auth_and_perms.view_profile",
+    },
+    {
+        "name": "Add Laboratory Room",
+        "category": "Laboratory Room",
+        "permission": "laboratory.add_laboratoryroom",
+    },
+    {
+        "name": "View Laboratory Room",
+        "category": "Laboratory Room",
+        "permission": "laboratory.view_laboratoryroom",
+    },
+    {
+        "name": "View Furniture",
+        "category": "Furniture",
+        "permission": "laboratory.view_furniture",
+    },
+    {
+        "name": "View Object",
+        "category": "Object",
+        "permission": "laboratory.view_object",
+    },
+    {
+        "name": "Add Object",
+        "category": "Object",
+        "permission": "laboratory.add_object",
+    },
+    {
+        "name": "View Object Features",
+        "category": "Object Features",
+        "permission": "laboratory.view_objectfeatures",
+    },
+    {
+        "name": "Add Object Features",
+        "category": "Object Features",
+        "permission": "laboratory.add_objectfeatures",
+    },
+    {
+        "name": "View Shelf Object",
+        "category": "Shelf Object",
+        "permission": "laboratory.view_shelfobject",
+    },
+    {
+        "name": "Add Shelf Object",
+        "category": "Shelf Object",
+        "permission": "laboratory.add_shelfobject",
+    },
+    {
+        "name": "Update Shelf Object",
+        "category": "Shelf Object",
+        "permission": "laboratory.change_shelfobject",
+    },
+    {
+        "name": "Delete Shelf Object",
+        "category": "Shelf Object",
+        "permission": "laboratory.delete_shelfobject",
+    },
+    {
+        "name": "List reservations",
+        "category": "Reservations",
+        "permission": "djreservation.add_reservation",
+    },
+    {
+        "name": "View Inform",
+        "category": "Inform",
+        "permission": "laboratory.view_inform",
+    },
+    {
+        "name": "View Laboratory Process",
+        "category": "Laboratory Process",
+        "permission": "laboratory.view_laboratoryprocess",
+    },
+    {
+        "name": "View Protocol",
+        "category": "Protocol",
+        "permission": "laboratory.view_protocol",
+    },
+    {
+        "name": "View Register User",
+        "category": "Register User",
+        "permission": "auth_and_perms.view_registeruserqr"
+    },
+
+], }
