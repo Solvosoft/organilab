9d2ded591e71bbf197cd3811e67afd906cd20491
Kejebo <kenjen041@gmail.com>
2026-03-18 17:57:47 -0600

    Added new command to create new riskzones

--- src/risk_management/models.py
+++ src/risk_management/models.py
@@ -92,7 +92,7 @@ class RiskZone(AbstractOrganizationRef):
     zone_type = models.ForeignKey(
         ZoneType, on_delete=models.CASCADE, verbose_name=_("Zone Type")
     )
-    priority = models.SmallIntegerField(verbose_name=_("Priority"))
+    priority = models.SmallIntegerField(verbose_name=_("Priority"), default=0)
 
     def __str__(self):
         return self.name
@@ -233,6 +233,8 @@ class Buildings(AbstractOrganizationRef):
         verbose_name=_("Responsible"),
         related_name="manager",
         on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
     )
     geolocation = PlainLocationField(
         default="9.895804362670006,-84.1552734375", zoom=15
