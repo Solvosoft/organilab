bb117b182998e93d75914f9a4dc1b11a81a54eb4
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-03-18 17:29:03 -0600

    generacion de xls

--- src/organilab/settings.py
+++ src/organilab/settings.py
@@ -149,7 +149,7 @@ WSGI_APPLICATION = "organilab.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DBNAME", "organilab"),
+        "NAME": os.getenv("DBNAME", "organilab_pro"),
         "USER": os.getenv("DBUSER", "organilab_user"),
         "PASSWORD": os.getenv("DBPASSWORD", "0rg4n1l4b"),
         "HOST": os.getenv("DBHOST", "127.0.0.1"),
