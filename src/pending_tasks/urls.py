55bbe17582b77dfd03b41417efa44a3dac9457a9
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-03-24 15:42:58 -0600

    updated pending task urls

--- src/pending_tasks/urls.py
+++ src/pending_tasks/urls.py
@@ -3,12 +3,14 @@ from rest_framework.routers import DefaultRouter
 from pending_tasks import views
 from pending_tasks.api.views import PendingTaskViewSet
 
-router = DefaultRouter()
-router.register("pending-tasks", PendingTaskViewSet, basename="pending_tasks")
+router_pending_tasks = DefaultRouter()
+router_pending_tasks.register(
+    "api_pending_tasks", PendingTaskViewSet, basename="api-pending_tasks"
+)
 
 app_name = "pending_tasks"
 
 urlpatterns = [
-    path("api/", include(router.urls)),
-    path("view-task", views.view_task, name="view_task"),
+    path("<int:org_pk>/api/", include(router_pending_tasks.urls)),
+    path("<int:org_pk>/view-tasks", views.view_task, name="view_task"),
 ]
