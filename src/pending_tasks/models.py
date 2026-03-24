9f930f8588f3f36cb951fec0ee11f9ea9b8cf337
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-03-24 15:42:10 -0600

    updated model pending task

--- src/pending_tasks/models.py
+++ src/pending_tasks/models.py
@@ -13,19 +13,26 @@ class PendingTask(AbstractOrganizationRef):
     STATUS = (
         (PENDING, _("Pending")),
         (IN_PROCESS, _("In process")),
-        (FINISHED, _("Finished"))
+        (FINISHED, _("Finished")),
     )
-    description = models.TextField(_('Description'), null=True, blank=True)
-    status = models.IntegerField(_('Status'), choices=STATUS, default=PENDING)
-    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL,
-                                null=True, blank=True, verbose_name=_('Profile'))
-    rols = models.ManyToManyField(Rol, verbose_name=_('Roles'), blank=True)
-    link = models.URLField(_('Link'), null=True, blank=True)
+    name = models.CharField(_("Name"), max_length=255)
+    description = models.TextField(_("Description"))
+    status = models.IntegerField(_("Status"), choices=STATUS, default=PENDING)
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name=_("Profile"),
+    )
+    rols = models.ManyToManyField(Rol, verbose_name=_("Roles"), blank=True)
+    link = models.URLField(_("Link"), null=True, blank=True)
+    is_archived = models.BooleanField(_("Is archived"), default=False)
 
     class Meta:
-        verbose_name = _('Pending task')
-        verbose_name_plural = _('Pending tasks')
-        ordering = ['-creation_date']
+        verbose_name = _("Pending task")
+        verbose_name_plural = _("Pending tasks")
+        ordering = ["-creation_date"]
 
     def __str__(self):
-        return f'{self.description} - {self.profile}'
+        return f"{self.name}"
