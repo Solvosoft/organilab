5b1d3a19801016f8c0e4ad31d1915661e5e3686d
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-03-24 15:42:58 -0600

    updated pending task urls

--- src/pending_tasks/views.py
+++ src/pending_tasks/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 
+from pending_tasks.forms import PendingTaskForm
+
 
 @login_required
 @permission_required("pending_tasks.view_pendingtask", raise_exception=True)
-def view_task(request):
-    context = {}
-
+def view_task(request, org_pk=None):
+    context = {
+        "org_pk": org_pk,
+        "create_form": PendingTaskForm(prefix="create", org_pk=org_pk),
+        "update_form": PendingTaskForm(prefix="update", org_pk=org_pk),
+    }
     return render(request, "tasks/tasks-view.html", context=context)
