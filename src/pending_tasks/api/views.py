c50a1b29edfea9794b17f07cf9bb5a46c92e89af
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-03-27 07:35:39 -0600

    added create pending task in utils

--- src/pending_tasks/api/views.py
+++ src/pending_tasks/api/views.py
@@ -1,11 +1,8 @@
 import logging
 
-from django.contrib.admin.models import CHANGE, ADDITION
-from django.contrib.auth import get_user_model
+from django.contrib.admin.models import ADDITION
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
 
 from django_filters.rest_framework import DjangoFilterBackend
 from djgentelella.permission_management import AllPermissionByAction
@@ -22,7 +19,7 @@ from laboratory.models import OrganizationStructure
 from laboratory.utils import organilab_logentry
 from pending_tasks.api import filterset
 from pending_tasks.models import PendingTask
-from report.utils import create_notification
+from pending_tasks.utils import notify_task_created
 from pending_tasks.api.serializers import (
     PendingTaskSerializer,
     PendingTaskListSerializer,
@@ -103,26 +100,7 @@ class PendingTaskViewSet(
             ADDITION,
             changed_data=["name", "description", "status"],
         )
-        self.notify_task_created(instance, org)
-
-    def notify_task_created(self, instance, org):
-        url = reverse("pending_tasks:view_task", kwargs={"org_pk": org.pk})
-        message = _("New pending task: %s") % instance.name
-
-        users_to_notify = set()
-
-        if instance.profile_id and instance.profile.user_id:
-            users_to_notify.add(instance.profile.user_id)
-
-        role_user_ids = Profile.objects.filter(
-            profilepermission__rol__in=instance.rols.all()
-        ).values_list("user_id", flat=True)
-        users_to_notify.update(role_user_ids)
-        users_to_notify.discard(self.request.user.pk)
-
-        User = get_user_model()
-        for user in User.objects.filter(pk__in=users_to_notify):
-            create_notification(user, message, url)
+        notify_task_created(instance, self.request.user, org)
 
     def filter_queryset(self, queryset):
         queryset = super().filter_queryset(queryset)
