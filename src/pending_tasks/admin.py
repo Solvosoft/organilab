9b4b134ebe198f09f8b9be32f125328289f40aeb
Kejebo <kenjen041@gmail.com>
2026-04-01 15:57:46 -0600

    Added new model

--- src/pending_tasks/admin.py
+++ src/pending_tasks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from pending_tasks.models import PendingTask
+from pending_tasks.models import PendingTask, PendingTaskManager
 
 
 @admin.register(PendingTask)
@@ -21,3 +21,12 @@ class PendingTaskAdmin(admin.ModelAdmin):
     readonly_fields = ["creation_date", "last_update"]
     list_editable = ["status", "is_archived"]
     date_hierarchy = "creation_date"
+
+
+class PendingTaskManagerAdmin(admin.ModelAdmin):
+    list_display = ["id", "task", "content_type", "object_id"]
+    list_filter = ["task", "content_type", "object_id"]
+    search_fields = ["task", "content_type", "object_id"]
+
+
+admin.site.register(PendingTaskManager, PendingTaskManagerAdmin)
