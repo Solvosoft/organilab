0df2e3c628fd0c96c5e2122c49decad677ea1058
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2026-04-10 10:32:49 -0600

    cambios en reportes

--- src/report/views/discard_objects.py
+++ src/report/views/discard_objects.py
@@ -28,6 +28,8 @@ def get_dataset_report_discard_objects(report, laboratory, column_list=None):
             "shelf__name",
             "object__name",
             "quantity",
+            "is_box",
+            "quantity_units",
             "creation_date",
             "created_by__username",
             "created_by__last_name",
@@ -37,10 +39,11 @@ def get_dataset_report_discard_objects(report, laboratory, column_list=None):
     )
 
     for obj in shelfobjects.distinct():
+        total = obj["quantity"] * obj["quantity_units"] if obj["is_box"] and obj["quantity_units"] else obj["quantity"]
         data_column = {
             "shelf": obj["shelf__name"] if obj["shelf__name"] else "",
             "object": obj["object__name"] if obj["object__name"] else "",
-            "amount": str(round(obj["quantity"], 3)),
+            "amount": str(round(total, 3)),
             "unit": (
                 obj["measurement_unit__description"]
                 if obj["measurement_unit__description"]
@@ -116,6 +119,8 @@ def get_dataset_report_discard_objects_html(report, column_list=None):
                 "shelf__name",
                 "object__name",
                 "quantity",
+                "is_box",
+                "quantity_units",
                 "creation_date",
                 "in_where_laboratory__name",
                 "created_by__username",
@@ -125,11 +130,12 @@ def get_dataset_report_discard_objects_html(report, column_list=None):
         )
 
         for obj in shelfobjects.distinct():
+            total = obj["quantity"] * obj["quantity_units"] if obj["is_box"] and obj["quantity_units"] else obj["quantity"]
             data_column = {
                 "in_where_laboratory__name": obj["in_where_laboratory__name"],
                 "shelf__name": obj["shelf__name"],
                 "object__name": obj["object__name"],
-                "quantity": str(round(obj["quantity"], 3)),
+                "quantity": str(round(total, 3)),
                 "measurement_unit__description": obj["measurement_unit__description"],
                 "created_by": (
                     obj["created_by__username"]
