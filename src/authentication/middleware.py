950424ee4d1effed7a2e5686689074566c4dbdec
Wilfredo Barquero Herrera <wilfredo.barquero.herrera@est.una.ac.cr>
2026-04-17 12:54:00 -0600

    fixed image capacitation

--- src/authentication/middleware.py
+++ src/authentication/middleware.py
@@ -170,6 +170,9 @@ class HandleErrorMiddleware:
             if "application/json" in content_type:
                 return response
 
+            if request.path.startswith("/docs/"):
+                return response
+
             error_path = reverse("error_view")
 
             if request.path != error_path:
