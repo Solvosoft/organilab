eef78c62e62ba47be73d9a81408a18c9f8757fd9
Kejebo <kenjen041@gmail.com>
2026-04-27 12:03:00 -0600

    Update sds module

--- src/msds/api/serializer.py
+++ src/msds/api/serializer.py
@@ -1,7 +1,8 @@
 from djgentelella.serializers import GTDateField
 from rest_framework import serializers
 
-from laboratory.models import SDSTraceability
+from laboratory.models import SDSTraceability, SustanceCharacteristics
+from django.utils.translation import gettext_lazy as _
 
 
 class SDSTraceabilitySerializer(serializers.ModelSerializer):
@@ -11,6 +12,7 @@ class SDSTraceabilitySerializer(serializers.ModelSerializer):
     verified_by_name = serializers.SerializerMethodField()
     security_sheet_url = serializers.SerializerMethodField()
     verified_date = GTDateField()
+    hcodes = serializers.SerializerMethodField()
 
     def get_actions(self, obj):
         user = self.context["request"].user
@@ -39,8 +41,16 @@ class SDSTraceabilitySerializer(serializers.ModelSerializer):
         return ""
 
     def get_security_sheet_url(self, obj):
-        if obj.security_sheet:
-            return obj.security_sheet.url
+        if obj.sustance_characteristics.security_sheet:
+            return obj.sustance_characteristics.security_sheet.url
+        return ""
+
+    def get_hcodes(self, obj):
+        if obj.sustance_characteristics.h_code.all():
+            codes = set(
+                [hc.code.__str__() for hc in obj.sustance_characteristics.h_code.all()]
+            )
+            return ", ".join(codes) if len(codes) > 0 else ""
         return ""
 
     class Meta:
@@ -55,6 +65,7 @@ class SDSTraceabilitySerializer(serializers.ModelSerializer):
             "is_verified",
             "security_sheet",
             "security_sheet_url",
+            "hcodes",
             "sustance_characteristics_cas_id_number",
             "verified_date",
             "actions",
@@ -75,4 +86,92 @@ class SDSTraceabilityValidateSerializer(serializers.ModelSerializer):
         model = SDSTraceability
         fields = [
             "is_verified",
+        ]
+
+
+class SustanceCharacteristicsSerializer(serializers.ModelSerializer):
+    obj_name = serializers.SerializerMethodField()
+    iarc_name = serializers.SerializerMethodField()
+    imdg_name = serializers.SerializerMethodField()
+    precursor_type_name = serializers.SerializerMethodField()
+    white_organ_list = serializers.SerializerMethodField()
+    h_code_list = serializers.SerializerMethodField()
+    ue_code_list = serializers.SerializerMethodField()
+    nfpa_list = serializers.SerializerMethodField()
+    storage_class_list = serializers.SerializerMethodField()
+    security_sheet_url = serializers.SerializerMethodField()
+    is_precursor = serializers.SerializerMethodField()
+    molecular_formula = serializers.SerializerMethodField()
+    cas_id_number = serializers.SerializerMethodField()
+
+    def get_is_precursor(self, obj):
+        if obj.is_precursor:
+            return _("Yes")
+        return _("No")
+
+    def get_molecular_formula(self, obj):
+        if obj.valid_molecular_formula:
+            return obj.molecular_formula
+        return ""
+
+    def get_cas_id_number(self, obj):
+        if obj.cas_id_number:
+            return obj.cas_id_number
+        return ""
+
+    def get_obj_name(self, obj):
+        return obj.obj.name if obj.obj else ""
+
+    def get_iarc_name(self, obj):
+        return str(obj.iarc) if obj.iarc else ""
+
+    def get_imdg_name(self, obj):
+        return str(obj.imdg) if obj.imdg else ""
+
+    def get_precursor_type_name(self, obj):
+        return str(obj.precursor_type) if obj.precursor_type else ""
+
+    def get_white_organ_list(self, obj):
+        return [str(wo) for wo in obj.white_organ.all()]
+
+    def get_h_code_list(self, obj):
+        return [hc.__str__() for hc in obj.h_code.all()]
+
+    def get_ue_code_list(self, obj):
+        return [str(ue) for ue in obj.ue_code.all()]
+
+    def get_nfpa_list(self, obj):
+        return [str(nfpa) for nfpa in obj.nfpa.all()]
+
+    def get_storage_class_list(self, obj):
+        return [str(sc) for sc in obj.storage_class.all()]
+
+    def get_security_sheet_url(self, obj):
+        if obj.security_sheet:
+            return obj.security_sheet.url
+        return ""
+
+    class Meta:
+        model = SustanceCharacteristics
+        fields = [
+            "id",
+            "obj_name",
+            "cas_id_number",
+            "molecular_formula",
+            "valid_molecular_formula",
+            "is_precursor",
+            "precursor_type",
+            "precursor_type_name",
+            "iarc",
+            "iarc_name",
+            "imdg",
+            "imdg_name",
+            "bioaccumulable",
+            "seveso_list",
+            "security_sheet_url",
+            "white_organ_list",
+            "h_code_list",
+            "ue_code_list",
+            "nfpa_list",
+            "storage_class_list",
         ]
