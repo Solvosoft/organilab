04a8fd4f4b3d3ff5153c2965a25ce7c4649f0e19
Kejebo <kenjen041@gmail.com>
2026-04-27 15:32:31 -0600

    updated shelfobject detail modal

--- src/laboratory/static/laboratory/js/detail_modal_creation.js
+++ src/laboratory/static/laboratory/js/detail_modal_creation.js
@@ -210,10 +210,13 @@ O: None
 function load_array_data(array_list, title, inst) {
     let html_object = ""
     let nested_key = Object.keys(array_list[0])[0]
+        console.log(title);
     array_list.forEach((item) => {
         let value = item
-        if (nested_key) {
+        if (nested_key && !["Códigos H", "H-codes"].includes(title)) {
             value = value[nested_key]
+        }else{
+            value = value
         }
         html_object += `<li>${value}</li>`
     })
